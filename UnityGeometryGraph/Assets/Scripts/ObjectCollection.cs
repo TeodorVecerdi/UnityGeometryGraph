67d12956d7d9791af401e1cea79d8318c5b6f4a1
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-06 16:34:38 +0300

    Add ObjectCollection type

---
+++ UnityGeometryGraph/Assets/Scripts/ObjectCollection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GraphFramework.Runtime {
+    public abstract class ObjectCollection<TChild, TProcessed> : MonoBehaviour {
+        [SerializeField, HideInInspector] private int childrenHashCode = 0;
+        [SerializeField, HideInInspector] private TProcessed processed;
+
+        public void Process() {
+            var newHashCode = ComputeChildrenHashCode(transform);
+            
+            if (childrenHashCode != newHashCode || processed == null) {
+                childrenHashCode = newHashCode;
+                processed = Process(CollectChildren());
+            }
+        }
+        
+        protected abstract TProcessed Process(List<TChild> children);
+        protected abstract TChild CollectChild(Transform childTransform);
+
+        private List<TChild> CollectChildren() {
+            var list = new List<TChild>();
+            for (var i = 0; i < transform.childCount; i++) {
+                list.Add(CollectChild(transform.GetChild(i)));
+            }
+
+            return list;
+        }
+
+        private int ComputeChildrenHashCode(Transform transform) {
+            var hashCode = transform.GetHashCode();
+            if (transform.childCount == 0) return hashCode;
+            
+            for (var i = 0; i < transform.childCount; i++) {
+                hashCode = hashCode * 37 + ComputeChildrenHashCode(transform.GetChild(i));
+            }
+
+            return hashCode;
+        }
+
+        private void Reset() {
+            Process();
+        }
+    }
+}
