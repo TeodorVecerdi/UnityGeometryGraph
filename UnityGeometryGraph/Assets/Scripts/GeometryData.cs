9ea0238206cc2839dee9d090b14305a7a2f3e214
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-07 12:55:41 +0300

    Add prototype of mesh conversion

---
+++ UnityGeometryGraph/Assets/Scripts/GeometryData.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class GeometryData {
+    [SerializeField] public List<Vector3> Vertices;
+    [SerializeField] public List<Vector3> Normals;
+    [SerializeField] public List<int> Triangles;
+    [SerializeField] public GeometryMetadata Metadata;
+
+    public GeometryData(Mesh mesh) {
+        Vertices = new List<Vector3>();
+        Normals = new List<Vector3>();
+        Triangles = new List<int>();
+        
+        mesh.GetVertices(Vertices);
+        mesh.GetNormals(Normals);
+        mesh.GetTriangles(Triangles, 0);
+
+        BuildMetadata();
+    }
+
+    private void BuildMetadata() {
+        Metadata = new GeometryMetadata(this);
+
+        for (var i = 0; i < Triangles.Count; i+=3) {
+            var idxA = Triangles[i];
+            var idxB = Triangles[i + 1];
+            var idxC = Triangles[i + 2];
+
+            var faceNormal = Normals[idxA] + Normals[idxB] + Normals[idxC];
+            
+            var face = new Face(idxA, idxB, idxC, faceNormal / 3.0f);
+            var edgeA = face.EdgeA = new Edge(idxA, idxB);
+            var edgeB = face.EdgeB = new Edge(idxB, idxC);
+            var edgeC = face.EdgeC = new Edge(idxC, idxA);
+            
+            Metadata.Faces.Add(face);
+            Metadata.Edges.Add(edgeA);
+            Metadata.Edges.Add(edgeB);
+            Metadata.Edges.Add(edgeC);
+        }
+    }
+    
+    
+    [Serializable]
+    public class GeometryMetadata {
+        [SerializeReference, HideInInspector] private GeometryData owner;
+
+        public List<Edge> Edges;
+        public List<Face> Faces;
+
+        public GeometryMetadata(GeometryData owner) {
+            this.owner = owner;
+            Edges = new List<Edge>();
+            Faces = new List<Face>();
+        }
+    }
+    
+    [Serializable]
+    public class Edge {
+        public int VertA;
+        public int VertB;
+
+        public Edge(int vertA, int vertB) {
+            VertA = vertA;
+            VertB = vertB;
+        }
+    }
+
+    [Serializable]
+    public class Face {
+        public int VertA;
+        public int VertB;
+        public int VertC;
+        public Vector3 FaceNormal;
+
+        [SerializeReference] public Edge EdgeA;
+        [SerializeReference] public Edge EdgeB;
+        [SerializeReference] public Edge EdgeC;
+
+        public Face(int vertA, int vertB, int vertC, Vector3 faceNormal) {
+            VertA = vertA;
+            VertB = vertB;
+            VertC = vertC;
+            FaceNormal = faceNormal;
+        }
+    }
+}
+
+
---
+++ UnityGeometryGraph/Assets/Scripts/MeshImporter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.XR;
+using Random = UnityEngine.Random;
+
+public class MeshImporter : MonoBehaviour {
+    [SerializeField] private MeshFilter source;
+    [SerializeField] private ElementGizmoType gizmoType;
+    [SerializeField] private float handleSize = 0.1f;
+    
+    [SerializeField] private GeometryData data;
+
+
+    [Button]
+    private void Load() {
+        if (source == null) {
+            Debug.LogError("Source MeshFilter is null");
+            return;
+        }
+        
+        data = new GeometryData(source.sharedMesh);
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (gizmoType == ElementGizmoType.None) return;
+        
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Handles.matrix = transform.localToWorldMatrix;
+        var zTest = Handles.zTest;
+        Handles.zTest = CompareFunction.Less;
+        Random.InitState(0);
+        
+        
+        if (gizmoType == ElementGizmoType.Vertices) {
+            foreach (var vertex in data.Vertices) {
+                var size = handleSize * HandleUtility.GetHandleSize(vertex);
+                var color = Random.ColorHSV(0f, 1f, 0.5f, 1f, .75f, 1f);
+                Gizmos.color = Handles.color = color;
+                Gizmos.DrawSphere(vertex, size);
+            }
+        } else if (gizmoType == ElementGizmoType.Edges) {
+            foreach (var edge in data.Metadata.Edges) {
+                var v0 = data.Vertices[edge.VertA];
+                var v1 = data.Vertices[edge.VertB];
+                var color = Random.ColorHSV(0f, 1f, 0.5f, 1f, .75f, 1f);
+                Gizmos.color = Handles.color = color;
+                Handles.DrawAAPolyLine(4.0f, v0, v1);
+            }
+        } else if (gizmoType == ElementGizmoType.Faces) {
+            foreach (var face in data.Metadata.Faces) {
+                var normal = face.FaceNormal;
+                var v0 = data.Vertices[face.VertA] + normal * 0.007f;
+                var v1 = data.Vertices[face.VertB] + normal * 0.007f;
+                var v2 = data.Vertices[face.VertC] + normal * 0.007f;
+                var color = Random.ColorHSV(0f, 1f, 0.5f, 1f, .75f, 1f);
+                Gizmos.color = Handles.color = color;
+                Handles.DrawAAConvexPolygon(v0, v1, v2);
+            }
+            
+            // Normals
+            foreach (var face in data.Metadata.Faces) {
+                var normal = face.FaceNormal;
+                var v0 = data.Vertices[face.VertA] + normal * 0.007f;
+                var v1 = data.Vertices[face.VertB] + normal * 0.007f;
+                var v2 = data.Vertices[face.VertC] + normal * 0.007f;
+                var mid = (v0 + v1 + v2) / 3.0f;
+                Gizmos.color = Handles.color = Color.white;
+                Handles.DrawAAPolyLine(4.0f, mid, mid + normal * 0.15f);
+            }
+        }
+        Handles.zTest = zTest;
+    }
+}
+
+public enum ElementGizmoType {
+    None,
+    Vertices,
+    Edges,
+    Faces,
+}
