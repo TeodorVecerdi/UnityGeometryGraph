da4377331151ff6bbe90dde0ea115987c08a2879
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-08 11:50:26 +0300

    Fix namespace

--- UnityGeometryGraph/Assets/Scripts/ObjectCollection.cs
+++ UnityGeometryGraph/Assets/Scripts/ObjectCollection.cs
@@ -1,45 +1,43 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-namespace GraphFramework.Runtime {
-    public abstract class ObjectCollection<TChild, TProcessed> : MonoBehaviour {
-        [SerializeField, HideInInspector] private int childrenHashCode = 0;
-        [SerializeField, HideInInspector] private TProcessed processed;
+public abstract class ObjectCollection<TChild, TProcessed> : MonoBehaviour {
+    [SerializeField, HideInInspector] private int childrenHashCode = 0;
+    [SerializeField, HideInInspector] private TProcessed processed;
 
-        public void Process() {
-            var newHashCode = ComputeChildrenHashCode(transform);
-            
-            if (childrenHashCode != newHashCode || processed == null) {
-                childrenHashCode = newHashCode;
-                processed = Process(CollectChildren());
-            }
-        }
+    public void Process() {
+        var newHashCode = ComputeChildrenHashCode(transform);
         
-        protected abstract TProcessed Process(List<TChild> children);
-        protected abstract TChild CollectChild(Transform childTransform);
+        if (childrenHashCode != newHashCode || processed == null) {
+            childrenHashCode = newHashCode;
+            processed = Process(CollectChildren());
+        }
+    }
+    
+    protected abstract TProcessed Process(List<TChild> children);
+    protected abstract TChild CollectChild(Transform childTransform);
 
-        private List<TChild> CollectChildren() {
-            var list = new List<TChild>();
-            for (var i = 0; i < transform.childCount; i++) {
-                list.Add(CollectChild(transform.GetChild(i)));
-            }
-
-            return list;
+    private List<TChild> CollectChildren() {
+        var list = new List<TChild>();
+        for (var i = 0; i < transform.childCount; i++) {
+            list.Add(CollectChild(transform.GetChild(i)));
         }
 
-        private int ComputeChildrenHashCode(Transform transform) {
-            var hashCode = transform.GetHashCode();
-            if (transform.childCount == 0) return hashCode;
-            
-            for (var i = 0; i < transform.childCount; i++) {
-                hashCode = hashCode * 37 + ComputeChildrenHashCode(transform.GetChild(i));
-            }
+        return list;
+    }
 
-            return hashCode;
+    private int ComputeChildrenHashCode(Transform transform) {
+        var hashCode = transform.GetHashCode();
+        if (transform.childCount == 0) return hashCode;
+        
+        for (var i = 0; i < transform.childCount; i++) {
+            hashCode = hashCode * 37 + ComputeChildrenHashCode(transform.GetChild(i));
         }
 
-        private void Reset() {
-            Process();
-        }
+        return hashCode;
+    }
+
+    private void Reset() {
+        Process();
     }
 }
