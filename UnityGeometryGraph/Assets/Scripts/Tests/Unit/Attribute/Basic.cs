36b265ed9f295ab507f0be68913808c77cf6f4b5
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-13 15:49:21 +0300

    Add serialization test

--- UnityGeometryGraph/Assets/Scripts/Tests/Unit/Attribute/Basic.cs
+++ UnityGeometryGraph/Assets/Scripts/Tests/Unit/Attribute/Basic.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+﻿using System.Linq;
 using Attribute;
 using NUnit.Framework;
 using Unity.Mathematics;
@@ -25,6 +25,19 @@ namespace Tests.Unit.Attribute {
             var attr = Enumerable.Range(0, 100).Select(_ => new float3(Rand.Range(-2.0f, 2.0f), Rand.Range(-2.0f, 2.0f), Rand.Range(-2.0f, 2.0f)))
                                  .Into<Vector3Attribute>("attr", AttributeDomain.Vertex);
             Assert.True(attr.Into<ClampedFloatAttribute>("clamped", new AttributeDomain?(AttributeDomain.Vertex)).All(f => f >= 0.0f && f <= 1.0f));
+        }
+
+        [Test]
+        public void SerializationMaintainsData() {
+            var attr = Enumerable.Range(0, 100).Select(_ => new float3(Rand.Range(-2.0f, 2.0f), Rand.Range(-2.0f, 2.0f), Rand.Range(-2.0f, 2.0f)))
+                                 .Into<Vector3Attribute>("attr", AttributeDomain.Vertex);
+            var serialized = SerializedAttribute.Serialize(attr);
+            var deserialized = SerializedAttribute.Deserialize(serialized);
+            
+            Assert.AreEqual(attr.GetType(), deserialized.GetType(), "{0} == {1}", attr.GetType(), deserialized.GetType());
+            Assert.AreEqual(attr.Type, deserialized.Type, "{0} == {1}", attr.Type, deserialized.Type);
+            Assert.AreEqual(attr.Domain, deserialized.Domain, "{0} == {1}", attr.Domain, deserialized.Domain);
+            Assert.IsTrue(attr.Values.SequenceEqual(deserialized.Values), "original.Values.SequenceEqual(deserialized.Values)");
         }
     }
 }
