74cedd1701352deef3191ae3dab4d9f883e6e5e8
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-13 10:18:13 +0300

    Add script to generate quad with submeshes

--- UnityGeometryGraph/Assets/Scripts/DoubleQuadMeshGenerator.cs
+++ UnityGeometryGraph/Assets/Scripts/DoubleQuadMeshGenerator.cs
@@ -54,4 +54,45 @@ public class DoubleQuadMeshGenerator : MonoBehaviour {
         }
         target.sharedMesh = mesh;
     }
+
+    [Button]
+    private void GenerateSubmeshes() {
+        var v0 = Vector3.zero;
+        var v1 = Vector3.up;
+        var v2 = Vector3.right;
+        var v3 = Vector3.up + Vector3.right;
+
+        var vertices = new List<Vector3> { v0, v1, v2, v3 };
+        var t0 = new List<int> {0, 2, 1};
+        var t1 = new List<int> {2, 3, 1};
+        List<int> t2 = null;
+        List<int> t3 = null;
+        
+        if (doubleSided) {
+            vertices.Add(v0);
+            vertices.Add(v1);
+            vertices.Add(v2);
+            vertices.Add(v3);
+            t2 = new List<int> { 0 + 4, 1 + 4, 2 + 4 };
+            t3 = new List<int> { 2 + 4, 1 + 4, 3 + 4 };
+        }
+        
+        var mesh = new Mesh {name = "$$_Double Quad"};
+        mesh.subMeshCount = doubleSided ? 4 : 2;
+        mesh.SetVertices(vertices);
+        mesh.SetTriangles(t0, 0);
+        mesh.SetTriangles(t1, 1);
+        if (doubleSided) {
+            mesh.SetTriangles(t2, 2);
+            mesh.SetTriangles(t3, 3);
+        }
+        
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+
+        if (target.sharedMesh.name.StartsWith("$$_", StringComparison.InvariantCulture)) {
+            DestroyImmediate(target.sharedMesh);
+        }
+        target.sharedMesh = mesh;
+    }
 }
