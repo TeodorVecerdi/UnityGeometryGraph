a399dc1be99d28a55752154d180cec67455b02e9
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-13 14:59:52 +0300

    Add clamped float type conversion tests

--- UnityGeometryGraph/Assets/Scripts/Tests/Attribute/TypeConversion/ClamepedFloatConversion.cs
+++ UnityGeometryGraph/Assets/Scripts/Tests/Attribute/TypeConversion/ClamepedFloatConversion.cs
@@ -4,10 +4,10 @@ using NUnit.Framework;
 using UnityCommons;
 
 namespace Tests.Attribute.TypeConversion {
-    public class FloatConversion {
+    public class ClampedFloatConversion {
         [Test]
-        public void FloatToBoolean() {
-            var floatAttribute = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<FloatAttribute>("src", AttributeDomain.Vertex);
+        public void ClampedFloatToBoolean() {
+            var floatAttribute = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<ClampedFloatAttribute>("src", AttributeDomain.Vertex);
             var boolAttribute = floatAttribute.Into<BoolAttribute>("dst", AttributeDomain.Vertex as AttributeDomain?);
 
             Assert.AreEqual(floatAttribute.Values.Count, boolAttribute.Values.Count, "Attribute length doesn't match: float:{0} == bool:{1}", floatAttribute.Values.Count, boolAttribute.Values.Count);
@@ -20,8 +20,8 @@ namespace Tests.Attribute.TypeConversion {
         }
         
         [Test]
-        public void FloatToInteger() {
-            var floatAttribute = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<FloatAttribute>("src", AttributeDomain.Vertex);
+        public void ClampedFloatToInteger() {
+            var floatAttribute = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<ClampedFloatAttribute>("src", AttributeDomain.Vertex);
             var intAttribute = floatAttribute.Into<IntAttribute>("dst", AttributeDomain.Vertex as AttributeDomain?);
 
             Assert.AreEqual(floatAttribute.Values.Count, intAttribute.Values.Count, "Attribute length doesn't match: float:{0} == int:{1}", floatAttribute.Values.Count, intAttribute.Values.Count);
@@ -34,22 +34,22 @@ namespace Tests.Attribute.TypeConversion {
         }
         
         [Test]
-        public void FloatToClampedFloat() {
-            var floatAttribute = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<FloatAttribute>("src", AttributeDomain.Vertex);
-            var clampedFloatAttribute = floatAttribute.Into<ClampedFloatAttribute>("dst", AttributeDomain.Vertex as AttributeDomain?);
+        public void ClampedFloatToFloat() {
+            var clampedFloatAttribute = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<ClampedFloatAttribute>("src", AttributeDomain.Vertex);
+            var floatAttribute = clampedFloatAttribute.Into<FloatAttribute>("dst", AttributeDomain.Vertex as AttributeDomain?);
 
-            Assert.AreEqual(floatAttribute.Values.Count, clampedFloatAttribute.Values.Count, "Attribute length doesn't match: float:{0} == clampedFloat:{1}", floatAttribute.Values.Count, clampedFloatAttribute.Values.Count);
+            Assert.AreEqual(clampedFloatAttribute.Values.Count, floatAttribute.Values.Count, "Attribute length doesn't match: float:{0} == clampedFloat:{1}", clampedFloatAttribute.Values.Count, floatAttribute.Values.Count);
             
             Assert.IsTrue(
-                clampedFloatAttribute.Zip(floatAttribute, (clampedFloat, @float) => (clampedFloat, @float))
-                                     .All(pair => pair.@float < 0.0f ? pair.clampedFloat == 0.0f : pair.@float > 1.0f ? pair.clampedFloat == 1.0f : pair.clampedFloat == pair.@float), 
-                "clampedFloatAttribute.Zip(floatAttribute, (@float, clampedFloat) => (@float, clampedFloat)).All(pair => pair.@float < 0.0f ? pair.clampedFloat == 0.0f : pair.@float > 1.0f ? pair.clampedFloat == 1.0f : pair.clampedFloat == pair.@float)"
+                floatAttribute.Zip(clampedFloatAttribute, (@float, clampedFloat) => (@float, clampedFloat))
+                                     .All(pair => pair.@float == pair.clampedFloat), 
+                "floatAttribute.Zip(clampedFloatAttribute, (@float, clampedFloat) => (@float, clampedFloat)).All(pair => pair.@float == pair.clampedFloat)"
             );
         }
         
         [Test]
-        public void FloatToVector2() {
-            var floatAttribute = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<FloatAttribute>("src", AttributeDomain.Vertex);
+        public void ClampedFloatToVector2() {
+            var floatAttribute = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<ClampedFloatAttribute>("src", AttributeDomain.Vertex);
             var vec2Attribute = floatAttribute.Into<Vector2Attribute>("dst", AttributeDomain.Vertex as AttributeDomain?);
 
             Assert.AreEqual(floatAttribute.Values.Count, vec2Attribute.Values.Count, "Attribute length doesn't match: float:{0} == vec2:{1}", floatAttribute.Values.Count, vec2Attribute.Values.Count);
@@ -62,8 +62,8 @@ namespace Tests.Attribute.TypeConversion {
         }
         
         [Test]
-        public void FloatToVector3() {
-            var floatAttribute = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<FloatAttribute>("src", AttributeDomain.Vertex);
+        public void ClampedFloatToVector3() {
+            var floatAttribute = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<ClampedFloatAttribute>("src", AttributeDomain.Vertex);
             var vec3Attribute = floatAttribute.Into<Vector3Attribute>("dst", AttributeDomain.Vertex as AttributeDomain?);
 
             Assert.AreEqual(floatAttribute.Values.Count, vec3Attribute.Values.Count, "Attribute length doesn't match: float:{0} == vec3:{1}", floatAttribute.Values.Count, vec3Attribute.Values.Count);
