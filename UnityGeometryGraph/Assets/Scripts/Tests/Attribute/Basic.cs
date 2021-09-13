1f5dce8b065d3538a231aa3b299c31083bf6396e
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-13 14:47:37 +0300

    Add basic tests

---
+++ UnityGeometryGraph/Assets/Scripts/Tests/Attribute/Basic.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Attribute;
+using NUnit.Framework;
+using UnityCommons;
+
+namespace Tests.Attribute {
+    public class Basic {
+        [Test]
+        public void ClampedFloatIsClampedOnCreation() {
+            var clampedAttribute = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<ClampedFloatAttribute>("clamped", AttributeDomain.Vertex);
+            Assert.True(clampedAttribute.All(f => f >= 0.0f && f <= 1.0f));
+        }
+
+        [Test]
+        public void ClampedFloatIsClampedAfterOperation() {
+            var clampedAttributeA = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<ClampedFloatAttribute>("clampedA", AttributeDomain.Vertex);
+            var clampedAttributeB = Enumerable.Range(0, 100).Select(_ => Rand.Range(-100f, 100f)).Into<ClampedFloatAttribute>("clampedB", AttributeDomain.Vertex);
+            var clampedAttribute = clampedAttributeA.YieldWithAttribute(clampedAttributeB, (f1, f2) => f1 + f2).Into<ClampedFloatAttribute>("clampedC", AttributeDomain.Vertex);
+            Assert.True(clampedAttribute.All(f => f >= 0.0f && f <= 1.0f));
+        }
+    }
+}
