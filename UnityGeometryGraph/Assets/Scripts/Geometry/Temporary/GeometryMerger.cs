c066991e4603f84382aa919b6978f21dc5884021
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-16 10:15:44 +0300

    Add GeometryMerger script

---
+++ UnityGeometryGraph/Assets/Scripts/Geometry/Temporary/GeometryMerger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Geometry {
+    
+    public class GeometryMerger : MonoBehaviour, IGeometryProvider {
+        [SerializeField] private List<GeometryImporter> importers;
+        [SerializeField] private GeometryData geometryData;
+
+        [Button]
+        private void Merge() {
+            geometryData = GeometryData.Empty;
+
+            foreach (var importer in importers) {
+                if(importer == null || importer.Geometry == null) continue; 
+                GeometryData.Merge(geometryData, importer.Geometry);
+            }
+        }
+
+        public GeometryData Geometry => geometryData;
+        public Matrix4x4 LocalToWorldMatrix => transform.localToWorldMatrix;
+    }
+}
