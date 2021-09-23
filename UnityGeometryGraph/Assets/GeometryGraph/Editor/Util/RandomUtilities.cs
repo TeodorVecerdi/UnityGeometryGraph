098acb49a86ff8a339e12095da571fb6608861bb
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-23 17:48:05 +0300

    Implement enum dropdown window & button

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Util/RandomUtilities.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace GeometryGraph.Editor {
+    public static class RandomUtilities {
+        /// <summary>
+        /// Splits <paramref name="value"/> into words where the case changes.
+        /// </summary>
+        /// <example><c>"someStringValue"</c> becomes <c>"Some String Value"</c></example>
+        public static string WordBreakString(string value) {
+            var result = new StringBuilder();
+            var currentWordStart = 0;
+            for (var i = 1; i < value.Length - 1; i++) {
+                if (char.IsUpper(value[i]) != char.IsUpper(value[i + 1])) {
+                    result.Append($"{Capitalize(value.Substring(currentWordStart, i - currentWordStart + 1))} ");
+                    currentWordStart = i + 1;
+                    i++;
+                }
+            }
+
+            result.Append(Capitalize(value.Substring(currentWordStart)));
+
+            return result.ToString();
+        }
+
+        private static string Capitalize(string value) {
+            return $"{char.ToUpper(value[0])}{value.Substring(1)}";
+        }
+    }
+}
