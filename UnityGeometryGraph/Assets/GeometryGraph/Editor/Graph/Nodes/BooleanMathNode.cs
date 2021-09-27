fa950a1cdec159eb7bcff012ac71515d9ef42d19
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-27 10:47:31 +0300

    Add boolean math node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/BooleanMathNode.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.Graph;
+using Newtonsoft.Json.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+using Which = GeometryGraph.Runtime.Graph.BooleanMathNode.BooleanMathNode_Which;
+using Operation = GeometryGraph.Runtime.Graph.BooleanMathNode.BooleanMathNode_Operation;
+
+namespace GeometryGraph.Editor {
+    [Title("Boolean", "Math")]
+    public class BooleanMathNode : AbstractNode<GeometryGraph.Runtime.Graph.BooleanMathNode> {
+        
+        private GraphFrameworkPort xPort;
+        private GraphFrameworkPort yPort;
+        private GraphFrameworkPort resultPort;
+
+        private EnumSelectionButton<Operation> operationButton;
+        private Toggle xField;
+        private Toggle yField;
+
+        private Operation operation;
+        private bool x;
+        private bool y;
+
+        private static readonly SelectionTree compareOperationTree = new SelectionTree(new List<object>(Enum.GetValues(typeof(Operation)).Convert(o => o))) {
+            new SelectionCategory("Operation", false, SelectionCategory.CategorySize.Large) {
+                new SelectionEntry("True if both x and y are true", 0, false),
+                new SelectionEntry("True if either x or y are true", 1, false),
+                new SelectionEntry("True if only one of x and y are true", 2, false),
+                new SelectionEntry("True if x is false", 3, false),
+            }
+        };
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Boolean Math", EditorView.DefaultNodePosition);
+
+            (xPort, xField) = GraphFrameworkPort.CreateWithBackingField<Toggle, bool>("X", Orientation.Horizontal, PortType.Boolean, edgeConnectorListener, this);
+            (yPort, yField) = GraphFrameworkPort.CreateWithBackingField<Toggle, bool>("Y", Orientation.Horizontal, PortType.Boolean, edgeConnectorListener, this);
+            resultPort = GraphFrameworkPort.Create("Result", Orientation.Horizontal, Direction.Output, Port.Capacity.Multi, PortType.Boolean, edgeConnectorListener, this);
+
+            operationButton = new EnumSelectionButton<Operation>(operation, compareOperationTree);
+            operationButton.RegisterCallback<ChangeEvent<Operation>>(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change operation");
+                operation = evt.newValue;
+                RuntimeNode.UpdateCompareOperation(operation);
+                OnOperationChanged();
+            });
+
+            xField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change value");
+                x = evt.newValue;
+                RuntimeNode.UpdateValue(x, Which.A);
+            });
+            
+            yField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change value");
+                y = evt.newValue;
+                RuntimeNode.UpdateValue(y, Which.B);
+            });
+
+            xPort.Add(xField);
+            yPort.Add(yField);
+            
+            inputContainer.Add(operationButton);
+            AddPort(xPort);
+            AddPort(yPort);
+            AddPort(resultPort);
+            
+            OnOperationChanged();
+
+            Refresh();
+        }
+
+        public override void BindPorts() {
+            BindPort(xPort, RuntimeNode.APort);
+            BindPort(yPort, RuntimeNode.BPort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+
+        public override JObject GetNodeData() {
+            var root = base.GetNodeData();
+
+            root["o"] = (int)operation;
+            root["a"] = x ? 1 : 0;
+            root["b"] = y ? 1 : 0;
+            
+            return root;
+        }
+
+        private void OnOperationChanged() {
+            if (operation == Operation.NOT) {
+                yPort.HideAndDisconnect();
+            } else {
+                yPort.Show();
+            }
+        }
+
+        public override void SetNodeData(JObject jsonData) {
+            operation = (Operation) jsonData.Value<int>("o");
+            x = jsonData.Value<int>("a") == 1;
+            y = jsonData.Value<int>("b") == 1;
+            
+            operationButton.SetValueWithoutNotify(operation, 1);
+            xField.SetValueWithoutNotify(x);
+            yField.SetValueWithoutNotify(y);
+            
+            RuntimeNode.UpdateCompareOperation(operation);
+            RuntimeNode.UpdateValue(x, Which.A);
+            RuntimeNode.UpdateValue(y, Which.B);
+
+            OnOperationChanged();
+
+            base.SetNodeData(jsonData);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/BooleanMathNode.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityCommons;
+
+namespace GeometryGraph.Runtime.Graph {
+    public class BooleanMathNode : RuntimeNode {
+        private BooleanMathNode_Operation operation;
+        private bool a;
+        private bool b;
+
+        public RuntimePort APort { get; private set; }
+        public RuntimePort BPort { get; private set; }
+        public RuntimePort ResultPort { get; private set; }
+
+        public BooleanMathNode(string guid) : base(guid) {
+            APort = RuntimePort.Create(PortType.Boolean, PortDirection.Input, this);
+            BPort = RuntimePort.Create(PortType.Boolean, PortDirection.Input, this);
+            ResultPort = RuntimePort.Create(PortType.Boolean, PortDirection.Input, this);
+        }
+
+        public void UpdateCompareOperation(BooleanMathNode_Operation newOperation) {
+            if (newOperation == operation) return;
+            
+            operation = newOperation;
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateValue(bool value, BooleanMathNode_Which which) {
+            switch (which) {
+                case BooleanMathNode_Which.A: a = value; break;
+                case BooleanMathNode_Which.B: b = value; break;
+                default: throw new ArgumentOutOfRangeException(nameof(which), which, null);
+            }
+
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public override object GetValueForPort(RuntimePort port) {
+            if (port != ResultPort) return null;
+            return operation switch {
+                BooleanMathNode_Operation.AND => a & b,
+                BooleanMathNode_Operation.OR => a | b,
+                BooleanMathNode_Operation.XOR => a ^ b,
+                BooleanMathNode_Operation.NOT => !a,
+                _ => throw new ArgumentOutOfRangeException(nameof(operation), operation, null)
+            };
+        }
+
+        protected override void OnPortValueChanged(Connection connection, RuntimePort port) {
+            if(port == ResultPort) return;
+            if (port == APort) {
+                var newValue = GetValue(APort, a);
+                if (newValue != a) {
+                    a = newValue;
+                    NotifyPortValueChanged(ResultPort);
+                } 
+            } else if (port == BPort) {
+                var newValue = GetValue(BPort, b);
+                if (newValue != b) {
+                    b = newValue;
+                    NotifyPortValueChanged(ResultPort);
+                } 
+            }
+        }
+        
+        public override void RebindPorts() {
+            APort = Ports[0];
+            BPort = Ports[1];
+            ResultPort = Ports[2];
+        }
+
+        public override string GetCustomData() {
+            var data = new JObject {
+                ["o"] = (int)operation,
+                ["a"] = a ? 1 : 0,
+                ["b"] = b ? 1 : 0
+            };
+            return data.ToString(Formatting.None);
+        }
+
+        public override void SetCustomData(string json) {
+            if(string.IsNullOrEmpty(json)) return;
+            
+            var data = JObject.Parse(json);
+            operation = (BooleanMathNode_Operation)data.Value<int>("o");
+            a = data.Value<int>("a") == 1;
+            b = data.Value<int>("b") == 1;
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public enum BooleanMathNode_Which {A = 0, B = 1}
+        public enum BooleanMathNode_Operation {AND = 0, OR = 1, XOR = 2, NOT = 3}
+    }
+}
