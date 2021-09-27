43c61771865f3ed8fb9e48729b55f3282ee83023
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-27 12:17:18 +0300

    Add support for underscores in Display strings

--- UnityGeometryGraph/Assets/GeometryGraph/Editor/Util/RandomUtilities.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Util/RandomUtilities.cs
@@ -10,10 +10,15 @@ namespace GeometryGraph.Editor {
             var result = new StringBuilder();
             var currentWordStart = 0;
             for (var i = 1; i < value.Length - 1; i++) {
+                if (value[i] == '_') {
+                    result.Append($"{Capitalize(value.Substring(currentWordStart, i - currentWordStart))} ");
+                    currentWordStart = i + 1;
+                    i += 2;
+                }
                 if (char.IsUpper(value[i]) != char.IsUpper(value[i + 1]) || char.IsLetter(value[i]) != char.IsLetter(value[i + 1])) {
                     result.Append($"{Capitalize(value.Substring(currentWordStart, i - currentWordStart + 1))} ");
-                    currentWordStart = i + 1;
                     i++;
+                    currentWordStart = i;
                 }
             }
 
