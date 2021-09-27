2bc69a1a747ac1058253e4377465fb2c6637fefb
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-27 12:16:30 +0300

    Add RotateVector node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Vector/RotateVectorNode.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.Graph;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Which = GeometryGraph.Runtime.Graph.RotateVectorNode.RotateVectorNode_Which;
+using RotationType = GeometryGraph.Runtime.Graph.RotateVectorNode.RotateVectorNode_Type;
+
+namespace GeometryGraph.Editor {
+    [Title("Vector", "Rotate")]
+    public class RotateVectorNode : AbstractNode<GeometryGraph.Runtime.Graph.RotateVectorNode> {
+        private GraphFrameworkPort vectorPort;
+        private GraphFrameworkPort centerPort;
+        private GraphFrameworkPort axisPort;
+        private GraphFrameworkPort eulerAnglesPort;
+        private GraphFrameworkPort anglePort;
+        private GraphFrameworkPort resultPort;
+
+        private EnumSelectionButton<RotationType> rotationTypeButton;
+        private Vector3Field vectorField;
+        private Vector3Field centerField;
+        private Vector3Field axisField;
+        private Vector3Field eulerAnglesField;
+        private FloatField angleField;
+
+        private RotationType rotationType;
+        private float3 vector;
+        private float3 center;
+        private float3 axis;
+        private float3 eulerAngles;
+        private float angle;
+
+        private static readonly SelectionTree compareOperationTree = new SelectionTree(new List<object>(Enum.GetValues(typeof(RotationType)).Convert(o => o))) {
+            new SelectionCategory("Rotation Type", false, SelectionCategory.CategorySize.Normal) {
+                new SelectionEntry("", 0, false),
+                new SelectionEntry("", 1, true),
+                new SelectionEntry("", 2, false),
+                new SelectionEntry("", 3, false),
+                new SelectionEntry("", 4, false),
+            }
+        };
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Rotate", EditorView.DefaultNodePosition);
+
+            (vectorPort, vectorField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("Vector", Orientation.Horizontal, PortType.Vector, edgeConnectorListener, this, showLabelOnField: false);
+            (centerPort, centerField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("Center", Orientation.Horizontal, PortType.Vector, edgeConnectorListener, this, showLabelOnField: false);
+            (axisPort, axisField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("Axis", Orientation.Horizontal, PortType.Vector, edgeConnectorListener, this, showLabelOnField: false);
+            (eulerAnglesPort, eulerAnglesField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("Euler", Orientation.Horizontal, PortType.Vector, edgeConnectorListener, this, showLabelOnField: false);
+            (anglePort, angleField) = GraphFrameworkPort.CreateWithBackingField<FloatField, float>("Angle", Orientation.Horizontal, PortType.Float, edgeConnectorListener, this);
+            resultPort = GraphFrameworkPort.Create("Result", Orientation.Horizontal, Direction.Output, Port.Capacity.Multi, PortType.Vector, edgeConnectorListener, this);
+
+            rotationTypeButton = new EnumSelectionButton<RotationType>(rotationType, compareOperationTree);
+            rotationTypeButton.RegisterCallback<ChangeEvent<RotationType>>(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change operation");
+                rotationType = evt.newValue;
+                RuntimeNode.UpdateType(rotationType);
+                OnRotationTypeChanged();
+            });
+
+            vectorField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change vector value");
+                vector = evt.newValue;
+                RuntimeNode.UpdateValue(vector, Which.Vector);
+            });
+            
+            centerField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change center value");
+                center = evt.newValue;
+                RuntimeNode.UpdateValue(center, Which.Center);
+            });
+            
+            axisField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change axis value");
+                axis = evt.newValue;
+                RuntimeNode.UpdateValue(axis, Which.Axis);
+            });
+            
+            eulerAnglesField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change euler angles value");
+                eulerAngles = evt.newValue;
+                RuntimeNode.UpdateValue(eulerAngles, Which.Euler);
+            });
+            
+            anglePort.Add(angleField);
+            
+            inputContainer.Add(rotationTypeButton);
+            AddPort(vectorPort);
+            inputContainer.Add(vectorField);
+            AddPort(centerPort);
+            inputContainer.Add(centerField);
+            AddPort(axisPort);
+            inputContainer.Add(axisField);
+            AddPort(eulerAnglesPort);
+            inputContainer.Add(eulerAnglesField);
+            AddPort(anglePort);
+            AddPort(resultPort);
+            
+            OnRotationTypeChanged();
+
+            Refresh();
+        }
+
+        public override void BindPorts() {
+            BindPort(vectorPort, RuntimeNode.VectorPort);
+            BindPort(centerPort, RuntimeNode.CenterPort);
+            BindPort(axisPort, RuntimeNode.AxisPort);
+            BindPort(eulerAnglesPort, RuntimeNode.EulerAnglesPort);
+            BindPort(anglePort, RuntimeNode.AnglePort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+
+        private void OnRotationTypeChanged() {
+            switch (rotationType) {
+                case RotationType.AxisAngle:
+                    axisPort.Show();
+                    anglePort.Show();
+                    eulerAnglesPort.HideAndDisconnect();
+                    break;
+                case RotationType.Euler:
+                    eulerAnglesPort.Show();
+                    axisPort.HideAndDisconnect();
+                    anglePort.HideAndDisconnect();
+                    break;
+                case RotationType.X_Axis:
+                case RotationType.Y_Axis:
+                case RotationType.Z_Axis:
+                    anglePort.Show();
+                    axisPort.HideAndDisconnect();
+                    eulerAnglesPort.HideAndDisconnect();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(rotationType), rotationType, null);
+            }
+        }
+
+        public override JObject GetNodeData() {
+            var root = base.GetNodeData();
+
+            root["t"] = (int)rotationType;
+            root["v"] = JsonConvert.SerializeObject(vector, float3Converter.Converter);
+            root["c"] = JsonConvert.SerializeObject(center, float3Converter.Converter);
+            root["x"] = JsonConvert.SerializeObject(axis, float3Converter.Converter);
+            root["e"] = JsonConvert.SerializeObject(eulerAngles, float3Converter.Converter);
+            root["a"] = angle;
+            
+            return root;
+        }
+        
+        public override void SetNodeData(JObject jsonData) {
+            rotationType = (RotationType)jsonData.Value<int>("t");
+            vector = JsonConvert.DeserializeObject<float3>(jsonData.Value<string>("v"), float3Converter.Converter);
+            center = JsonConvert.DeserializeObject<float3>(jsonData.Value<string>("c"), float3Converter.Converter);
+            axis = JsonConvert.DeserializeObject<float3>(jsonData.Value<string>("x"), float3Converter.Converter);
+            eulerAngles = JsonConvert.DeserializeObject<float3>(jsonData.Value<string>("e"), float3Converter.Converter);
+            angle = jsonData.Value<float>("a");
+            
+            rotationTypeButton.SetValueWithoutNotify(rotationType, 1);
+            vectorField.SetValueWithoutNotify(vector);
+            centerField.SetValueWithoutNotify(center);
+            axisField.SetValueWithoutNotify(axis);
+            eulerAnglesField.SetValueWithoutNotify(eulerAngles);
+            angleField.SetValueWithoutNotify(angle);
+            
+            RuntimeNode.UpdateType(rotationType);
+            RuntimeNode.UpdateValue(vector, Which.Vector);
+            RuntimeNode.UpdateValue(center, Which.Center);
+            RuntimeNode.UpdateValue(axis, Which.Axis);
+            RuntimeNode.UpdateValue(eulerAngles, Which.Euler);
+            RuntimeNode.UpdateValue(angle, Which.Angle);
+            
+            OnRotationTypeChanged();
+            
+            base.SetNodeData(jsonData);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Vector/RotateVectorNode.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace GeometryGraph.Runtime.Graph {
+    public class RotateVectorNode : RuntimeNode {
+        private float3 vector;
+        private float3 center;
+        private float3 axis;
+        private float3 eulerAngles;
+        private float angle;
+
+        private RotateVectorNode_Type type;
+
+        public RuntimePort VectorPort { get; private set; }
+        public RuntimePort CenterPort { get; private set; }
+        public RuntimePort AxisPort { get; private set; }
+        public RuntimePort EulerAnglesPort { get; private set; }
+        public RuntimePort AnglePort { get; private set; }
+        public RuntimePort ResultPort { get; private set; }
+
+        public RotateVectorNode(string guid) : base(guid) {
+            VectorPort = RuntimePort.Create(PortType.Vector, PortDirection.Input, this);
+            CenterPort = RuntimePort.Create(PortType.Vector, PortDirection.Input, this);
+            AxisPort = RuntimePort.Create(PortType.Vector, PortDirection.Input, this);
+            EulerAnglesPort = RuntimePort.Create(PortType.Vector, PortDirection.Output, this);
+            AnglePort = RuntimePort.Create(PortType.Float, PortDirection.Output, this);
+            ResultPort = RuntimePort.Create(PortType.Vector, PortDirection.Output, this);
+        }
+
+        public void UpdateType(RotateVectorNode_Type newType) {
+            if (newType == type) return;
+            type = newType;
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateValue(object value, RotateVectorNode_Which which) {
+            switch (which) {
+                case RotateVectorNode_Which.Vector: vector = (float3)value; break;
+                case RotateVectorNode_Which.Center: center = (float3)value; break;
+                case RotateVectorNode_Which.Axis: axis = (float3)value; break;
+                case RotateVectorNode_Which.Euler: eulerAngles = (float3)value; break;
+                case RotateVectorNode_Which.Angle: angle = (float)value; break;
+                default: throw new ArgumentOutOfRangeException(nameof(which), which, null);
+            }
+
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public override object GetValueForPort(RuntimePort port) {
+            if (port != ResultPort) return null;
+            
+            return Calculate();
+        }
+
+        private float3 Calculate() {
+            return type switch {
+                RotateVectorNode_Type.AxisAngle => math.rotate(quaternion.AxisAngle(axis, angle), vector - center) + center,
+                RotateVectorNode_Type.Euler => math.rotate(quaternion.Euler(eulerAngles), vector - center) + center,
+                RotateVectorNode_Type.X_Axis => math.rotate(quaternion.AxisAngle(float3_util.right, angle), vector - center) + center,
+                RotateVectorNode_Type.Y_Axis => math.rotate(quaternion.AxisAngle(float3_util.up, angle), vector - center) + center,
+                RotateVectorNode_Type.Z_Axis => math.rotate(quaternion.AxisAngle(float3_util.forward, angle), vector - center) + center,
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
+        }
+
+        protected override void OnPortValueChanged(Connection connection, RuntimePort port) {
+            if(port == ResultPort) return;
+            if (port == VectorPort) {
+                vector = GetValue(VectorPort, vector);
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == CenterPort) {
+                center = GetValue(CenterPort, center);
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == AxisPort) {
+                axis = GetValue(AxisPort, axis);
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == EulerAnglesPort) {
+                eulerAngles = GetValue(EulerAnglesPort, eulerAngles);
+                NotifyPortValueChanged(EulerAnglesPort);
+            } else if (port == AnglePort) {
+                var newAngle = GetValue(AnglePort, angle);
+                if (Math.Abs(newAngle - angle) > 0.000001f) {
+                    angle = newAngle;
+                    NotifyPortValueChanged(ResultPort);
+                }
+            }
+        }
+        
+        public override void RebindPorts() {
+            VectorPort = Ports[0];
+            CenterPort = Ports[1];
+            AxisPort = Ports[2];
+            EulerAnglesPort = Ports[3];
+            AnglePort = Ports[4];
+            ResultPort = Ports[5];
+        }
+
+        public override string GetCustomData() {
+            var data = new JObject {
+                ["t"] = (int)type,
+                ["v"] = JsonConvert.SerializeObject(vector, float3Converter.Converter),
+                ["c"] = JsonConvert.SerializeObject(center, float3Converter.Converter),
+                ["x"] = JsonConvert.SerializeObject(axis, float3Converter.Converter),
+                ["e"] = JsonConvert.SerializeObject(eulerAngles, float3Converter.Converter),
+                ["a"] = angle,
+            };
+            return data.ToString(Formatting.None);
+        }
+
+        public override void SetCustomData(string json) {
+            if(string.IsNullOrEmpty(json)) return;
+            
+            var data = JObject.Parse(json);
+            type = (RotateVectorNode_Type)data.Value<int>("t");
+            vector = JsonConvert.DeserializeObject<float3>(data.Value<string>("v"), float3Converter.Converter);
+            center = JsonConvert.DeserializeObject<float3>(data.Value<string>("c"), float3Converter.Converter);
+            axis = JsonConvert.DeserializeObject<float3>(data.Value<string>("x"), float3Converter.Converter);
+            eulerAngles = JsonConvert.DeserializeObject<float3>(data.Value<string>("e"), float3Converter.Converter);
+            angle = data.Value<float>("a");
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public enum RotateVectorNode_Type {AxisAngle = 0, Euler = 1, X_Axis = 2, Y_Axis = 3, Z_Axis = 4}
+        public enum RotateVectorNode_Which {Vector = 0, Center = 1, Axis = 2, Euler = 3, Angle = 4}
+    }
+}
