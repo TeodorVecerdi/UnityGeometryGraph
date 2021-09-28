5b5c03467c79b1c1f0fc325513cc0cfe6ed72151
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-28 11:27:06 +0300

    Add Modulo math function for negative numbers

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Misc/MathUtilities.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Misc/MathUtilities.cs
@@ -28,6 +28,8 @@ namespace GeometryGraph.Runtime {
             return ((x - min) % (max - min) + (max - min)) % (max - min) + min;
         }
 
+        public static int Mod(this int a, int n) => a % n < 0 ? a + n : a;
+
         public static Vector3 WrapPI(Vector3 a) {
             if (a.x < -180.0f) a.x += 360.0f;
             else if (a.x > 180.0f) a.x -= 360.0f;
