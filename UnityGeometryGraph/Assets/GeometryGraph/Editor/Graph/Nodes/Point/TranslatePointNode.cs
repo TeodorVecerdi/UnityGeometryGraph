efd2b0804332e368383e54927407707723e6e953
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-30 13:40:22 +0300

    Add Translate Point node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Point/TranslatePointNode.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.Graph;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Which = GeometryGraph.Runtime.Graph.TranslatePointNode.TranslatePointNode_Which;
+using Mode = GeometryGraph.Runtime.Graph.TranslatePointNode.TranslatePointNode_Mode;
+
+namespace GeometryGraph.Editor {
+    [Title("Point", "Translate Points")]
+    public class TranslatePointNode : AbstractNode<GeometryGraph.Runtime.Graph.TranslatePointNode> {
+        
+        private GraphFrameworkPort inputPort;
+        private GraphFrameworkPort translationPort;
+        private GraphFrameworkPort attributePort;
+        private GraphFrameworkPort resultPort;
+
+        private EnumSelectionButton<Mode> modeButton;
+        private Vector3Field translationField;
+        private TextField attributeNameField;
+
+        private float3 translation;
+        private string attributeName;
+        private Mode mode;
+
+        private static readonly SelectionTree tree = new SelectionTree(new List<object>(Enum.GetValues(typeof(Mode)).Convert(o => o))) {
+            new SelectionCategory("Type", false, SelectionCategory.CategorySize.Normal) {
+                new SelectionEntry("Translate every point using a vector", 0, false),
+                new SelectionEntry("Translate each point using an attribute", 1, false)
+            }
+        };
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Translate Points", EditorView.DefaultNodePosition);
+
+            inputPort = GraphFrameworkPort.Create("Geometry", Orientation.Horizontal, Direction.Input, Port.Capacity.Single, PortType.Geometry, edgeConnectorListener, this);
+            (translationPort, translationField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("Translation", Orientation.Horizontal, PortType.Vector, edgeConnectorListener, this, showLabelOnField: false);
+            (attributePort, attributeNameField) = GraphFrameworkPort.CreateWithBackingField<TextField, string>("Attribute", Orientation.Horizontal, PortType.String, edgeConnectorListener, this);
+            resultPort = GraphFrameworkPort.Create("Result", Orientation.Horizontal, Direction.Output, Port.Capacity.Multi, PortType.Geometry, edgeConnectorListener, this);
+
+            modeButton = new EnumSelectionButton<Mode>(mode, tree);
+            modeButton.RegisterCallback<ChangeEvent<Mode>>(evt => {
+                if (evt.newValue == mode) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change translation type");
+                mode = evt.newValue;
+                RuntimeNode.UpdateMode(mode);
+                OnModeChanged();
+            });
+            
+            translationField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change translation");
+                translation = evt.newValue;
+                RuntimeNode.UpdateValue(translation, Which.Translation);
+            });
+            
+            attributeNameField.RegisterValueChangedCallback(evt => {
+                if (string.Equals(evt.newValue, attributeName, StringComparison.InvariantCulture)) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change attribute name");
+                attributeName = evt.newValue;
+                RuntimeNode.UpdateValue(attributeName, Which.AttributeName);
+            });
+
+            attributePort.Add(attributeNameField);
+            
+            inputContainer.Add(modeButton);
+            AddPort(inputPort);
+            AddPort(translationPort);
+            inputContainer.Add(translationField);
+            AddPort(attributePort);
+            AddPort(resultPort);
+
+            OnModeChanged();
+            
+            Refresh();
+        }
+
+        private void OnModeChanged() {
+            if (mode == Mode.Attribute) {
+                attributePort.Show();
+                translationPort.HideAndDisconnect();
+            } else {
+                attributePort.HideAndDisconnect();
+                translationPort.Show();
+            }
+        }
+        
+        public override void BindPorts() {
+            BindPort(inputPort, RuntimeNode.InputPort);
+            BindPort(translationPort, RuntimeNode.TranslationPort);
+            BindPort(attributePort, RuntimeNode.AttributePort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+
+        public override JObject GetNodeData() {
+            var root = base.GetNodeData();
+
+            root["t"] = JsonConvert.SerializeObject(translation, Formatting.None, float3Converter.Converter);
+            root["a"] = attributeName;
+            root["m"] = (int)mode;
+            
+            return root;
+        }
+
+        public override void SetNodeData(JObject jsonData) {
+            translation = JsonConvert.DeserializeObject<float3>(jsonData.Value<string>("t"), float3Converter.Converter);
+            attributeName = jsonData.Value<string>("a");
+            mode = (Mode) jsonData.Value<int>("m");
+            
+            translationField.SetValueWithoutNotify(translation);
+            attributeNameField.SetValueWithoutNotify(attributeName);
+            modeButton.SetValueWithoutNotify(mode);
+            
+            RuntimeNode.UpdateValue(translation, Which.Translation);
+            RuntimeNode.UpdateValue(attributeName, Which.AttributeName);
+            RuntimeNode.UpdateMode(mode);
+
+            OnModeChanged();
+
+            base.SetNodeData(jsonData);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Point/TranslatePointNode.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using GeometryGraph.Runtime.Attribute;
+using GeometryGraph.Runtime.Geometry;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityCommons;
+using UnityEngine;
+
+namespace GeometryGraph.Runtime.Graph {
+    public class TranslatePointNode : RuntimeNode {
+        private GeometryData geometry;
+        private float3 translation;
+        private string attributeName;
+        private TranslatePointNode_Mode mode;
+        
+        private GeometryData result;
+        
+        public RuntimePort InputPort { get; private set; }
+        public RuntimePort TranslationPort { get; private set; }
+        public RuntimePort AttributePort { get; private set; }
+        public RuntimePort ResultPort { get; private set; }
+
+        public TranslatePointNode(string guid) : base(guid) {
+            InputPort = RuntimePort.Create(PortType.Geometry, PortDirection.Input, this);
+            TranslationPort = RuntimePort.Create(PortType.Vector, PortDirection.Input, this);
+            AttributePort = RuntimePort.Create(PortType.String, PortDirection.Input, this);
+            ResultPort = RuntimePort.Create(PortType.Geometry, PortDirection.Input, this);
+        }
+
+        public void UpdateMode(TranslatePointNode_Mode newMode) {
+            if(newMode == mode) return;
+
+            mode = newMode;
+            Calculate();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateValue(object value, TranslatePointNode_Which which) {
+            switch (which) {
+                case TranslatePointNode_Which.Translation: translation = (float3)value; break;
+                case TranslatePointNode_Which.AttributeName: attributeName = (string)value; break;
+                default: throw new ArgumentOutOfRangeException(nameof(which), which, null);
+            }
+
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public override object GetValueForPort(RuntimePort port) {
+            if (port != ResultPort) return null;
+            return result;
+        }
+
+        protected override void OnPortValueChanged(Connection connection, RuntimePort port) {
+            if(port == ResultPort) return;
+            if (port == InputPort) {
+                geometry = GetValue(connection, geometry);
+                Calculate();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == TranslationPort) {
+                translation = GetValue(connection, translation);
+                Calculate();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == AttributePort) {
+                var newValue = GetValue(connection, attributeName);
+                if (!string.Equals(newValue, attributeName, StringComparison.InvariantCulture)) {
+                    attributeName = newValue;
+                    Calculate();
+                    NotifyPortValueChanged(ResultPort);
+                } 
+            }
+        }
+        
+        public override void RebindPorts() {
+            InputPort = Ports[0];
+            TranslationPort = Ports[1];
+            AttributePort = Ports[2];
+            ResultPort = Ports[3];
+        }
+
+        public void Calculate() {
+            if (geometry == null) return;
+            result = (GeometryData) geometry.Clone();
+            var positionAttr = result.GetAttribute<Vector3Attribute>("position", AttributeDomain.Vertex);
+            if (mode == TranslatePointNode_Mode.Vector) {
+                positionAttr.Yield(position => position + translation).Into(positionAttr);
+                
+                /*
+                Storing even though I'm writing to the same attribute (with the .Into call) because AttributeManager
+                makes no guarantee that it will return the original attribute and not a clone of the attribute.
+                
+                Cloning happens when there is a domain or type mismatch and the original attribute gets converted.
+                
+                Also, storing the same attribute twice has no side effects and it's safe. The StoreAttribute method 
+                returns a boolean indicating whether an attribute was overwritten by the Store operation in case you 
+                care about that.
+                */
+                result.StoreAttribute(positionAttr);
+            } else {
+                if (!result.HasAttribute(attributeName)) {
+                    Debug.LogWarning($"Couldn't find attribute [{attributeName}]");
+                    return;
+                }
+                
+                var otherAttribute = result.GetAttribute<Vector3Attribute>(attributeName, AttributeDomain.Vertex);
+                positionAttr.YieldWithAttribute(otherAttribute, (position, translation) => position + translation).Into(positionAttr);
+                result.StoreAttribute(positionAttr);
+            }
+        }
+
+        public override string GetCustomData() {
+            var data = new JObject {
+                ["t"] = JsonConvert.SerializeObject(translation, Formatting.None, float3Converter.Converter),
+                ["a"] = attributeName,
+            };
+            return data.ToString(Formatting.None);
+        }
+
+        public override void SetCustomData(string json) {
+            if(string.IsNullOrEmpty(json)) return;
+            
+            var data = JObject.Parse(json);
+            translation = JsonConvert.DeserializeObject<float3>(data.Value<string>("t"), float3Converter.Converter);
+            attributeName = data.Value<string>("a");
+            Calculate();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public enum TranslatePointNode_Which {Translation = 0, AttributeName = 1}
+        public enum TranslatePointNode_Mode {Vector = 0, Attribute = 1}
+    }
+}
