180407fd47aaf8ffe45d341c7b60ddde7ebc2f32
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-30 12:50:33 +0300

    Add utility for converting between attribute types and system types

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Attribute/Utils/AttributeUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeometryGraph.Runtime.Attribute {
+    public static class AttributeUtility {
+        public static AttributeType SystemTypeToAttributeType(Type type) {
+            return attributeTypeDictionary[type];
+        }
+        
+        public static Type AttributeTypeToSystemType(AttributeType type) {
+            return systemTypeDictionary[type];
+        }
+
+        private static readonly Dictionary<Type, AttributeType> attributeTypeDictionary = new Dictionary<Type, AttributeType> {
+            { typeof(BoolAttribute), AttributeType.Boolean },
+            { typeof(IntAttribute), AttributeType.Integer },
+            { typeof(FloatAttribute), AttributeType.Float },
+            { typeof(ClampedFloatAttribute), AttributeType.ClampedFloat },
+            { typeof(Vector2Attribute), AttributeType.Vector2 },
+            { typeof(Vector3Attribute), AttributeType.Vector3 }
+        };
+
+        private static readonly Dictionary<AttributeType, Type> systemTypeDictionary = attributeTypeDictionary.ToDictionary(pair => pair.Value, pair => pair.Key);
+    }
+}
