5ede043c0eb02dddd00a387a91d0603e87388c70
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-09-30 14:12:32 +0300

    Add Scale point node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Point/ScalePointNode.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.Graph;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Which = GeometryGraph.Runtime.Graph.ScalePointNode.ScalePointNode_Which;
+using Mode = GeometryGraph.Runtime.Graph.ScalePointNode.ScalePointNode_Mode;
+
+namespace GeometryGraph.Editor {
+    [Title("Point", "Scale Points")]
+    public class ScalePointNode : AbstractNode<GeometryGraph.Runtime.Graph.ScalePointNode> {
+        
+        private GraphFrameworkPort inputPort;
+        private GraphFrameworkPort vectorPort;
+        private GraphFrameworkPort scalarPort;
+        private GraphFrameworkPort attributePort;
+        private GraphFrameworkPort resultPort;
+
+        private EnumSelectionButton<Mode> modeButton;
+        private Vector3Field vectorField;
+        private FloatField scalarField;
+        private TextField attributeNameField;
+
+        private float3 vector;
+        private float scalar;
+        private string attributeName;
+        private Mode mode;
+
+        private static readonly SelectionTree tree = new SelectionTree(new List<object>(Enum.GetValues(typeof(Mode)).Convert(o => o))) {
+            new SelectionCategory("Type", false, SelectionCategory.CategorySize.Normal) {
+                new SelectionEntry("Scale every point using a vector", 0, false),
+                new SelectionEntry("Scale every point using a float value", 1, false),
+                new SelectionEntry("Translate each point using an attribute", 2, false)
+            }
+        };
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Scale Points", EditorView.DefaultNodePosition);
+
+            inputPort = GraphFrameworkPort.Create("Geometry", Orientation.Horizontal, Direction.Input, Port.Capacity.Single, PortType.Geometry, edgeConnectorListener, this);
+            (vectorPort, vectorField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("Factor", Orientation.Horizontal, PortType.Vector, edgeConnectorListener, this, showLabelOnField: false);
+            (scalarPort, scalarField) = GraphFrameworkPort.CreateWithBackingField<FloatField, float>("Factor", Orientation.Horizontal, PortType.Float, edgeConnectorListener, this);
+            (attributePort, attributeNameField) = GraphFrameworkPort.CreateWithBackingField<TextField, string>("Attribute", Orientation.Horizontal, PortType.String, edgeConnectorListener, this);
+            resultPort = GraphFrameworkPort.Create("Result", Orientation.Horizontal, Direction.Output, Port.Capacity.Multi, PortType.Geometry, edgeConnectorListener, this);
+
+            modeButton = new EnumSelectionButton<Mode>(mode, tree);
+            modeButton.RegisterCallback<ChangeEvent<Mode>>(evt => {
+                if (evt.newValue == mode) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change scale type");
+                mode = evt.newValue;
+                RuntimeNode.UpdateMode(mode);
+                OnModeChanged();
+            });
+            
+            vectorField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change vector scale");
+                vector = evt.newValue;
+                RuntimeNode.UpdateValue(vector, Which.Vector);
+            });
+            
+            scalarField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change scalar scale");
+                scalar = evt.newValue;
+                RuntimeNode.UpdateValue(vector, Which.Scalar);
+            });
+            
+            attributeNameField.RegisterValueChangedCallback(evt => {
+                if (string.Equals(evt.newValue, attributeName, StringComparison.InvariantCulture)) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change attribute name");
+                attributeName = evt.newValue;
+                RuntimeNode.UpdateValue(attributeName, Which.AttributeName);
+            });
+
+            scalarPort.Add(scalarField);
+            attributePort.Add(attributeNameField);
+
+            inputContainer.Add(modeButton);
+            AddPort(inputPort);
+            AddPort(vectorPort);
+            inputContainer.Add(vectorField);
+            AddPort(scalarPort);
+            AddPort(attributePort);
+            AddPort(resultPort);
+
+            OnModeChanged();
+            
+            Refresh();
+        }
+
+        private void OnModeChanged() {
+            switch (mode) {
+                case Mode.Vector:
+                    vectorPort.Show();
+                    scalarPort.HideAndDisconnect();
+                    attributePort.HideAndDisconnect();
+                    break;
+                case Mode.Float:
+                    scalarPort.Show();
+                    vectorPort.HideAndDisconnect();
+                    attributePort.HideAndDisconnect();
+                    break;
+                case Mode.Attribute:
+                    attributePort.Show();
+                    scalarPort.HideAndDisconnect();
+                    vectorPort.HideAndDisconnect();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(mode), mode, null);
+            }
+        }
+        
+        public override void BindPorts() {
+            BindPort(inputPort, RuntimeNode.InputPort);
+            BindPort(vectorPort, RuntimeNode.VectorPort);
+            BindPort(scalarPort, RuntimeNode.ScalarPort);
+            BindPort(attributePort, RuntimeNode.AttributePort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+
+        public override JObject GetNodeData() {
+            var root = base.GetNodeData();
+
+            root["v"] = JsonConvert.SerializeObject(vector, Formatting.None, float3Converter.Converter);
+            root["s"] = scalar;
+            root["a"] = attributeName;
+            root["m"] = (int)mode;
+            
+            return root;
+        }
+
+        public override void SetNodeData(JObject jsonData) {
+            vector = JsonConvert.DeserializeObject<float3>(jsonData.Value<string>("v"), float3Converter.Converter);
+            scalar = jsonData.Value<float>("s");
+            attributeName = jsonData.Value<string>("a");
+            mode = (Mode) jsonData.Value<int>("m");
+            
+            vectorField.SetValueWithoutNotify(vector);
+            scalarField.SetValueWithoutNotify(scalar);
+            attributeNameField.SetValueWithoutNotify(attributeName);
+            modeButton.SetValueWithoutNotify(mode);
+            
+            RuntimeNode.UpdateValue(vector, Which.Vector);
+            RuntimeNode.UpdateValue(scalar, Which.Scalar);
+            RuntimeNode.UpdateValue(attributeName, Which.AttributeName);
+            RuntimeNode.UpdateMode(mode);
+
+            OnModeChanged();
+
+            base.SetNodeData(jsonData);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Point/ScalePointNode.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+using GeometryGraph.Runtime.Attribute;
+using GeometryGraph.Runtime.Geometry;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityCommons;
+using UnityEngine;
+
+namespace GeometryGraph.Runtime.Graph {
+    public class ScalePointNode : RuntimeNode {
+        private GeometryData geometry;
+        private float3 vector;
+        private float scalar;
+        private string attributeName;
+        private ScalePointNode_Mode mode;
+        
+        private GeometryData result;
+        
+        public RuntimePort InputPort { get; private set; }
+        public RuntimePort VectorPort { get; private set; }
+        public RuntimePort ScalarPort { get; private set; }
+        public RuntimePort AttributePort { get; private set; }
+        public RuntimePort ResultPort { get; private set; }
+
+        public ScalePointNode(string guid) : base(guid) {
+            InputPort = RuntimePort.Create(PortType.Geometry, PortDirection.Input, this);
+            VectorPort = RuntimePort.Create(PortType.Vector, PortDirection.Input, this);
+            ScalarPort = RuntimePort.Create(PortType.Float, PortDirection.Input, this);
+            AttributePort = RuntimePort.Create(PortType.String, PortDirection.Input, this);
+            ResultPort = RuntimePort.Create(PortType.Geometry, PortDirection.Input, this);
+        }
+
+        public void UpdateMode(ScalePointNode_Mode newMode) {
+            if(newMode == mode) return;
+
+            mode = newMode;
+            Calculate();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateValue(object value, ScalePointNode_Which which) {
+            switch (which) {
+                case ScalePointNode_Which.Vector: vector = (float3)value; break;
+                case ScalePointNode_Which.Scalar: scalar = (float)value; break;
+                case ScalePointNode_Which.AttributeName: attributeName = (string)value; break;
+                default: throw new ArgumentOutOfRangeException(nameof(which), which, null);
+            }
+
+            Calculate();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public override object GetValueForPort(RuntimePort port) {
+            if (port != ResultPort) return null;
+            return result;
+        }
+
+        protected override void OnPortValueChanged(Connection connection, RuntimePort port) {
+            if(port == ResultPort) return;
+            if (port == InputPort) {
+                geometry = GetValue(connection, geometry);
+                Calculate();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == VectorPort) {
+                vector = GetValue(connection, vector);
+                Calculate();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == ScalarPort) {
+                var newValue = GetValue(connection, scalar);
+                if (Math.Abs(newValue - scalar) > 0.000001f) {
+                    scalar = newValue;
+                    Calculate();
+                    NotifyPortValueChanged(ResultPort);
+                }
+            }else if (port == AttributePort) {
+                var newValue = GetValue(connection, attributeName);
+                if (!string.Equals(newValue, attributeName, StringComparison.InvariantCulture)) {
+                    attributeName = newValue;
+                    Calculate();
+                    NotifyPortValueChanged(ResultPort);
+                } 
+            }
+        }
+        
+        public override void RebindPorts() {
+            InputPort = Ports[0];
+            VectorPort = Ports[1];
+            ScalarPort = Ports[2];
+            AttributePort = Ports[3];
+            ResultPort = Ports[4];
+        }
+
+        public void Calculate() {
+            if (geometry == null) return;
+            result = (GeometryData) geometry.Clone();
+            var scaleAttr = result.GetAttribute<Vector3Attribute>("scale", AttributeDomain.Vertex);
+            scaleAttr ??= Enumerable.Repeat(float3_util.one, result.Vertices.Count).Into<Vector3Attribute>("scale", AttributeDomain.Vertex);
+            
+            if (mode == ScalePointNode_Mode.Vector || mode == ScalePointNode_Mode.Float) {
+                var multiplier = mode == ScalePointNode_Mode.Vector ? vector : new float3(scalar);
+                scaleAttr.Yield(scale => scale * multiplier).Into(scaleAttr);
+                result.StoreAttribute(scaleAttr);
+            } else {
+                if (!result.HasAttribute(attributeName)) {
+                    Debug.LogWarning($"Couldn't find attribute [{attributeName}]");
+                    return;
+                }
+                
+                var otherAttribute = result.GetAttribute<Vector3Attribute>(attributeName, AttributeDomain.Vertex);
+                scaleAttr.YieldWithAttribute(otherAttribute, (scale, multiplier) => scale * multiplier).Into(scaleAttr);
+                result.StoreAttribute(scaleAttr);
+            }
+        }
+
+        public override string GetCustomData() {
+            var data = new JObject {
+                ["v"] = JsonConvert.SerializeObject(vector, Formatting.None, float3Converter.Converter),
+                ["s"] = scalar,
+                ["a"] = attributeName,
+            };
+            return data.ToString(Formatting.None);
+        }
+
+        public override void SetCustomData(string json) {
+            if(string.IsNullOrEmpty(json)) return;
+            
+            var data = JObject.Parse(json);
+            vector = JsonConvert.DeserializeObject<float3>(data.Value<string>("v"), float3Converter.Converter);
+            scalar = data.Value<float>("s");
+            attributeName = data.Value<string>("a");
+           
+            Calculate();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public enum ScalePointNode_Which {Vector = 0, Scalar = 1, AttributeName = 2}
+        public enum ScalePointNode_Mode {Vector = 0, Float = 1, Attribute = 2}
+    }
+}
