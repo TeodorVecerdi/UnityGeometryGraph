83886a4ac0318d43b848d65ce1d9da4f3b589298
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-10-21 15:42:46 +0300

    Add interface for generating curves using Burst/Jobs system
    Add reference to UnityEngine.Burst

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Curve/Primitive/ICurveJob.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Jobs;
+using Unity.Mathematics;
+
+namespace GeometryGraph.Runtime.Curve.Primitive {
+    public interface ICurveJob : IJobParallelFor {
+        float3 Position(float t);
+        float3 Tangent(float t);
+        float3 Normal(float t);
+        float3 Binormal(float t);
+    }
+}
--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Misc/Constants.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Misc/Constants.cs
@@ -1,7 +1,12 @@
-﻿namespace GeometryGraph.Runtime {
+namespace GeometryGraph.Runtime {
     public static class Constants {
+        // Primitive Geometry constraints
         public const int MAX_ICOSPHERE_SUBDIVISIONS = 5;
         public const int MIN_CIRCULAR_GEOMETRY_POINTS = 3;
         public const int MAX_CIRCULAR_GEOMETRY_POINTS = 128;
+        
+        // Primitive Curves constraints
+        public const int MAX_CURVE_RESOLUTION = 1024;
+        public const int MIN_LINE_CURVE_RESOLUTION = 1;
     }
 }
