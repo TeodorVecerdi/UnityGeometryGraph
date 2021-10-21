33e3aea703156e04f2f9d615c93a4fab7bd6184a
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-10-21 15:47:27 +0300

    Add API for generating primitive curves

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Curve/CurvePrimitive.cs
@@ -0,0 +1,48 @@
+﻿using GeometryGraph.Runtime.Curve.Primitive;
+using Unity.Mathematics;
+using UnityCommons;
+
+namespace GeometryGraph.Runtime.Curve {
+    public static class CurvePrimitive {
+        public static CurveData Line(int resolution, float3 start, float3 end) {
+            resolution = resolution.Clamped(Constants.MIN_LINE_CURVE_RESOLUTION, Constants.MAX_CURVE_RESOLUTION);
+            
+            var lineCurve = new LineCurve(resolution, start, end);
+            lineCurve.Generate();
+            return lineCurve.ToCurveData();
+        }
+
+        public static CurveData Circle(int resolution, float radius) {
+            resolution = resolution.Clamped(Constants.MIN_CIRCLE_CURVE_RESOLUTION, Constants.MAX_CURVE_RESOLUTION);
+            radius = radius.Min(Constants.MIN_CIRCULAR_CURVE_RADIUS);
+
+            var circleCurve = new CircleCurve(resolution, radius);
+            circleCurve.Generate();
+            return circleCurve.ToCurveData();
+        }
+
+        public static CurveData QuadraticBezier(int resolution, bool isClosed, float3 start, float3 control, float3 end) {
+            resolution = resolution.Clamped(Constants.MIN_BEZIER_CURVE_RESOLUTION, Constants.MAX_CURVE_RESOLUTION);
+
+            var quadraticBezierCurve = new QuadraticBezierCurve(resolution, isClosed, start, control, end);
+            quadraticBezierCurve.Generate();
+            return quadraticBezierCurve.ToCurveData();
+        }
+        
+        public static CurveData CubicBezier(int resolution, bool isClosed, float3 start, float3 controlA, float3 controlB, float3 end) {
+            resolution = resolution.Clamped(Constants.MIN_BEZIER_CURVE_RESOLUTION, Constants.MAX_CURVE_RESOLUTION);
+
+            var cubicBezierCurve = new CubicBezierCurve(resolution, isClosed, start, controlA, controlB, end);
+            cubicBezierCurve.Generate();
+            return cubicBezierCurve.ToCurveData();
+        }
+        
+        public static CurveData Helix(int resolution, float rotations, float pitch, float topRadius, float bottomRadius) {
+            resolution = resolution.Clamped(Constants.MIN_BEZIER_CURVE_RESOLUTION, Constants.MAX_CURVE_RESOLUTION);
+
+            var helixCurve = new HelixCurve(resolution, rotations, pitch, topRadius, bottomRadius);
+            helixCurve.Generate();
+            return helixCurve.ToCurveData();
+        }
+    }
+}
