6682697a779994ab4964db9dc5035bf896689340
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-10-22 12:15:49 +0300

    Implement LinePrimitiveCurve node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Curve/Primitive/LinePrimitiveCurveNode.cs
@@ -0,0 +1,120 @@
+﻿using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.Graph;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityCommons;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace GeometryGraph.Editor {
+    [Title("Curve", "Primitive", "Line")]
+    public class LinePrimitiveCurveNode : AbstractNode<GeometryGraph.Runtime.Graph.LinePrimitiveCurveNode> {
+        private GraphFrameworkPort pointsPort;
+        private GraphFrameworkPort startPort;
+        private GraphFrameworkPort endPort;
+        private GraphFrameworkPort resultPort;
+
+        private ClampedIntegerField pointsField;
+        private Vector3Field startField;
+        private Vector3Field endField;
+
+        private int points = 2;
+        private float3 start = float3.zero;
+        private float3 end = float3_util.right;
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Line Primitive Curve");
+
+            (pointsPort, pointsField) = GraphFrameworkPort.CreateWithBackingField<ClampedIntegerField, int>("Points", Orientation.Horizontal, PortType.Integer, edgeConnectorListener, this, onDisconnect: (_, _) => RuntimeNode.UpdatePoints(points));
+            (startPort, startField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("Start", Orientation.Horizontal, PortType.Vector, edgeConnectorListener, this, showLabelOnField: false, onDisconnect: (_, _) => RuntimeNode.UpdateStart(start));
+            (endPort, endField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("End", Orientation.Horizontal, PortType.Vector, edgeConnectorListener, this, showLabelOnField: false, onDisconnect: (_, _) => RuntimeNode.UpdateEnd(end));
+            resultPort = GraphFrameworkPort.Create("Line Curve", Orientation.Horizontal, Direction.Output, Port.Capacity.Multi, PortType.Curve, edgeConnectorListener, this);
+
+            pointsField.Min = Constants.MIN_LINE_CURVE_RESOLUTION + 1;
+            pointsField.Max = Constants.MAX_CURVE_RESOLUTION + 1;
+            pointsField.RegisterValueChangedCallback(evt => {
+                var newValue = evt.newValue.Clamped(Constants.MIN_LINE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1);
+                if (newValue == points) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change line curve points");
+                points = newValue;
+                RuntimeNode.UpdatePoints(points);
+            });
+            
+            pointsField.RegisterCallback<BlurEvent>(_ => {
+                if (pointsField.value.Between(Constants.MIN_LINE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1)) return;
+                pointsField.SetValueWithoutNotify(pointsField.value.Clamped(Constants.MIN_LINE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1));
+            });
+
+            startField.RegisterValueChangedCallback(evt => {
+                var newValue = (float3)evt.newValue;
+                if (newValue.Equals(start)) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change line curve start position");
+                start = newValue;
+                RuntimeNode.UpdateStart(start);
+            });
+            
+            endField.RegisterValueChangedCallback(evt => {
+                var newValue = (float3)evt.newValue;
+                if (newValue.Equals(end)) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change line curve end position");
+                end = newValue;
+                RuntimeNode.UpdateEnd(end);
+            });
+            
+            pointsField.SetValueWithoutNotify(points);
+            startField.SetValueWithoutNotify(start);
+            endField.SetValueWithoutNotify(end);
+            
+            pointsPort.Add(pointsField);
+            AddPort(pointsPort);
+            AddPort(startPort);
+            inputContainer.Add(startField);
+            AddPort(endPort);
+            inputContainer.Add(endField);
+            AddPort(resultPort);
+            
+            Refresh();
+        }
+
+        public override void BindPorts() {
+            BindPort(pointsPort, RuntimeNode.PointsPort);
+            BindPort(startPort, RuntimeNode.StartPort);
+            BindPort(endPort, RuntimeNode.EndPort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+
+        public override JObject GetNodeData() {
+            var root =  base.GetNodeData();
+            var array = new JArray {
+                points,
+                JsonConvert.SerializeObject(start, float3Converter.Converter),
+                JsonConvert.SerializeObject(end, float3Converter.Converter),
+            };
+            root["d"] = array;
+            return root;
+        }
+
+        public override void SetNodeData(JObject jsonData) {
+            var array = jsonData["d"] as JArray;
+
+            points = array!.Value<int>(0);
+            start = JsonConvert.DeserializeObject<float3>(array!.Value<string>(1)!, float3Converter.Converter);
+            end = JsonConvert.DeserializeObject<float3>(array!.Value<string>(2)!, float3Converter.Converter);
+            
+            pointsField.SetValueWithoutNotify(points);
+            startField.SetValueWithoutNotify(start);
+            endField.SetValueWithoutNotify(end);
+            
+            RuntimeNode.UpdatePoints(points);
+            RuntimeNode.UpdateStart(start);
+            RuntimeNode.UpdateEnd(end);
+            
+            base.SetNodeData(jsonData);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Curve/Primitives/LinePrimitiveCurveNode.cs
@@ -0,0 +1,106 @@
+﻿using GeometryGraph.Runtime.Curve;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityCommons;
+
+namespace GeometryGraph.Runtime.Graph {
+    public class LinePrimitiveCurveNode : RuntimeNode {
+        private int points = 2;
+        private float3 start = float3.zero;
+        private float3 end = float3_util.right;
+        private CurveData lineCurve;
+
+        public RuntimePort PointsPort { get; private set; }
+        public RuntimePort StartPort { get; private set; }
+        public RuntimePort EndPort { get; private set; }
+        public RuntimePort ResultPort { get; private set; }
+
+        public LinePrimitiveCurveNode(string guid) : base(guid) {
+            PointsPort = RuntimePort.Create(PortType.Integer, PortDirection.Input, this);
+            StartPort = RuntimePort.Create(PortType.Vector, PortDirection.Input, this);
+            EndPort = RuntimePort.Create(PortType.Vector, PortDirection.Input, this);
+            ResultPort = RuntimePort.Create(PortType.Curve, PortDirection.Output, this);
+        }
+
+        private void CalculateResult() {
+            lineCurve = CurvePrimitive.Line(points - 1, start, end);
+        }
+
+        protected override void OnPortValueChanged(Connection connection, RuntimePort port) {
+            if (port == ResultPort) return;
+            if (port == PointsPort) {
+                var newValue = GetValue(connection, points);
+                if (newValue == points) return;
+                points = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == StartPort) {
+                var newValue = GetValue(connection, start);
+                if (newValue.Equals(start)) return;
+                start = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == EndPort) {
+                var newValue = GetValue(connection, end);
+                if (newValue.Equals(end)) return;
+                end = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            }
+        }
+
+        protected override object GetValueForPort(RuntimePort port) {
+            if (port != ResultPort) return null;
+            if (lineCurve == null) CalculateResult();
+            return lineCurve.Clone();
+        }
+
+        public override string GetCustomData() {
+            var array = new JArray {
+                points,
+                JsonConvert.SerializeObject(start, float3Converter.Converter),
+                JsonConvert.SerializeObject(end, float3Converter.Converter),
+            };
+            return array.ToString(Formatting.None);
+        }
+
+        public override void SetCustomData(string json) {
+            if (string.IsNullOrEmpty(json)) return;
+
+            var data = JArray.Parse(json);
+            points = data.Value<int>(0);
+            start = JsonConvert.DeserializeObject<float3>(data.Value<string>(1)!, float3Converter.Converter);
+            end = JsonConvert.DeserializeObject<float3>(data.Value<string>(2)!, float3Converter.Converter);
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdatePoints(int newPoints) {
+            newPoints = newPoints.Clamped(Constants.MIN_LINE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1);
+            if (newPoints == points) return;
+            
+            points = newPoints;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateStart(float3 newStart) {
+            if (start.Equals(newStart)) return;
+            start = newStart;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateEnd(float3 newEnd) {
+            if (end.Equals(newEnd)) return;
+            end = newEnd;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public override void RebindPorts() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
