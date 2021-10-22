ce50a1bc534b1366736beccca2b3958afa424f64
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-10-22 12:49:19 +0300

    Implement QuadraticBezierPrimitiveCurve node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Curve/Primitive/QuadraticBezierPrimitiveCurveNode.cs
@@ -0,0 +1,149 @@
+﻿using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.Graph;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityCommons;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace GeometryGraph.Editor {
+    [Title("Curve", "Primitive", "Quadratic Bezier")]
+    public class QuadraticBezierPrimitiveCurveNode : AbstractNode<GeometryGraph.Runtime.Graph.QuadraticBezierPrimitiveCurveNode> {
+        private GraphFrameworkPort pointsPort;
+        private GraphFrameworkPort closedPort;
+        private GraphFrameworkPort startPort;
+        private GraphFrameworkPort controlPort;
+        private GraphFrameworkPort endPort;
+        private GraphFrameworkPort resultPort;
+
+        private ClampedIntegerField pointsField;
+        private Toggle closedToggle;
+        private Vector3Field startField;
+        private Vector3Field controlField;
+        private Vector3Field endField;
+
+        private int points = 2;
+        private bool closed = false;
+        private float3 start = float3.zero;
+        private float3 control = float3_util.right;
+        private float3 end = float3_util.forward;
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Quadratic Bezier Primitive Curve");
+
+            (pointsPort, pointsField) = GraphFrameworkPort.CreateWithBackingField<ClampedIntegerField, int>("Points", Orientation.Horizontal, PortType.Integer, edgeConnectorListener, this, onDisconnect: (_, _) => RuntimeNode.UpdatePoints(points));
+            (closedPort, closedToggle) = GraphFrameworkPort.CreateWithBackingField<Toggle, bool>("Closed", Orientation.Horizontal, PortType.Boolean, edgeConnectorListener, this, onDisconnect: (_, _) => RuntimeNode.UpdateClosed(closed));
+            (startPort, startField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("Start", Orientation.Horizontal, PortType.Vector, edgeConnectorListener, this, showLabelOnField: false, onDisconnect: (_, _) => RuntimeNode.UpdateStart(start));
+            (controlPort, controlField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("Control", Orientation.Horizontal, PortType.Vector, edgeConnectorListener, this, showLabelOnField: false, onDisconnect: (_, _) => RuntimeNode.UpdateControl(control));
+            (endPort, endField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("End", Orientation.Horizontal, PortType.Vector, edgeConnectorListener, this, showLabelOnField: false, onDisconnect: (_, _) => RuntimeNode.UpdateEnd(end));
+            resultPort = GraphFrameworkPort.Create("Line Curve", Orientation.Horizontal, Direction.Output, Port.Capacity.Multi, PortType.Curve, edgeConnectorListener, this);
+
+            pointsField.Min = Constants.MIN_LINE_CURVE_RESOLUTION + 1;
+            pointsField.Max = Constants.MAX_CURVE_RESOLUTION + 1;
+            pointsField.RegisterValueChangedCallback(evt => {
+                var newValue = evt.newValue.Clamped(Constants.MIN_LINE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1);
+                if (newValue == points) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change quadratic bezier curve points");
+                points = newValue;
+                RuntimeNode.UpdatePoints(points);
+            });
+
+            closedToggle.RegisterValueChangedCallback(evt => {
+                if (evt.newValue == closed) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change quadratic bezier curve closed");
+                closed = evt.newValue;
+                RuntimeNode.UpdateClosed(closed);
+            });
+
+            startField.RegisterValueChangedCallback(evt => {
+                var newValue = (float3)evt.newValue;
+                if (newValue.Equals(start)) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change quadratic bezier curve start position");
+                start = newValue;
+                RuntimeNode.UpdateStart(start);
+            });
+            
+            controlField.RegisterValueChangedCallback(evt => {
+                var newValue = (float3)evt.newValue;
+                if (newValue.Equals(control)) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change quadratic bezier curve control position");
+                control = newValue;
+                RuntimeNode.UpdateControl(control);
+            });
+            
+            endField.RegisterValueChangedCallback(evt => {
+                var newValue = (float3)evt.newValue;
+                if (newValue.Equals(end)) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change quadratic bezier curve end position");
+                end = newValue;
+                RuntimeNode.UpdateEnd(end);
+            });
+            
+            pointsField.SetValueWithoutNotify(points);
+            startField.SetValueWithoutNotify(start);
+            controlField.SetValueWithoutNotify(control);
+            endField.SetValueWithoutNotify(end);
+            
+            pointsPort.Add(pointsField);
+            AddPort(pointsPort);
+            AddPort(startPort);
+            inputContainer.Add(startField);
+            AddPort(endPort);
+            inputContainer.Add(endField);
+            AddPort(resultPort);
+            
+            Refresh();
+        }
+
+        public override void BindPorts() {
+            BindPort(pointsPort, RuntimeNode.PointsPort);
+            BindPort(closedPort, RuntimeNode.ClosedPort);
+            BindPort(startPort, RuntimeNode.StartPort);
+            BindPort(controlPort, RuntimeNode.ControlPort);
+            BindPort(endPort, RuntimeNode.EndPort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+
+        public override JObject GetNodeData() {
+            var root =  base.GetNodeData();
+            var array = new JArray {
+                points,
+                closed ? 1 : 0,
+                JsonConvert.SerializeObject(start, float3Converter.Converter),
+                JsonConvert.SerializeObject(control, float3Converter.Converter),
+                JsonConvert.SerializeObject(end, float3Converter.Converter),
+            };
+            root["d"] = array;
+            return root;
+        }
+
+        public override void SetNodeData(JObject jsonData) {
+            var array = jsonData["d"] as JArray;
+
+            points = array!.Value<int>(0);
+            closed = array!.Value<int>(1) == 1;
+            start = JsonConvert.DeserializeObject<float3>(array!.Value<string>(2)!, float3Converter.Converter);
+            control = JsonConvert.DeserializeObject<float3>(array!.Value<string>(3)!, float3Converter.Converter);
+            end = JsonConvert.DeserializeObject<float3>(array!.Value<string>(4)!, float3Converter.Converter);
+            
+            pointsField.SetValueWithoutNotify(points);
+            closedToggle.SetValueWithoutNotify(closed);
+            startField.SetValueWithoutNotify(start);
+            controlField.SetValueWithoutNotify(control);
+            endField.SetValueWithoutNotify(end);
+            
+            RuntimeNode.UpdatePoints(points);
+            RuntimeNode.UpdateClosed(closed);
+            RuntimeNode.UpdateStart(start);
+            RuntimeNode.UpdateControl(control);
+            RuntimeNode.UpdateEnd(end);
+            
+            base.SetNodeData(jsonData);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Curve/Primitives/QuadraticBezierPrimitiveCurveNode.cs
@@ -0,0 +1,144 @@
+﻿using GeometryGraph.Runtime.Curve;
+using GeometryGraph.Runtime.Data;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityCommons;
+
+namespace GeometryGraph.Runtime.Graph {
+    public class QuadraticBezierPrimitiveCurveNode : RuntimeNode {
+        private MinMaxInt points = new MinMaxInt(2, Constants.MIN_LINE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1);
+        private bool isClosed;
+        private float3 start = float3.zero;
+        private float3 control = float3_util.right;
+        private float3 end = float3_util.forward;
+        
+        private CurveData curve;
+
+        public RuntimePort PointsPort { get; private set; }
+        public RuntimePort ClosedPort { get; private set; }
+        public RuntimePort StartPort { get; private set; }
+        public RuntimePort ControlPort { get; private set; }
+        public RuntimePort EndPort { get; private set; }
+        public RuntimePort ResultPort { get; private set; }
+
+        public QuadraticBezierPrimitiveCurveNode(string guid) : base(guid) {
+            PointsPort = RuntimePort.Create(PortType.Integer, PortDirection.Input, this);
+            ClosedPort = RuntimePort.Create(PortType.Boolean, PortDirection.Input, this);
+            StartPort = RuntimePort.Create(PortType.Vector, PortDirection.Input, this);
+            ControlPort = RuntimePort.Create(PortType.Vector, PortDirection.Input, this);
+            EndPort = RuntimePort.Create(PortType.Vector, PortDirection.Input, this);
+            ResultPort = RuntimePort.Create(PortType.Curve, PortDirection.Output, this);
+        }
+
+        private void CalculateResult() {
+            curve = CurvePrimitive.QuadraticBezier(points - 1, isClosed, start, control, end);
+        }
+
+        protected override void OnPortValueChanged(Connection connection, RuntimePort port) {
+            if (port == ResultPort) return;
+            if (port == PointsPort) {
+                var newValue = GetValue(connection, (int)points);
+                if (newValue == points) return;
+                points.Value = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == ClosedPort) {
+                var newValue = GetValue(connection, isClosed);
+                if (newValue == isClosed) return;
+                isClosed = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == StartPort) {
+                var newValue = GetValue(connection, start);
+                if (newValue.Equals(start)) return;
+                start = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == ControlPort) {
+                var newValue = GetValue(connection, control);
+                if (newValue.Equals(control)) return;
+                control = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == EndPort) {
+                var newValue = GetValue(connection, end);
+                if (newValue.Equals(end)) return;
+                end = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            }
+        }
+
+        protected override object GetValueForPort(RuntimePort port) {
+            if (port != ResultPort) return null;
+            if (curve == null) CalculateResult();
+            return curve.Clone();
+        }
+
+        public override string GetCustomData() {
+            var array = new JArray {
+                (int)points,
+                isClosed ? 1 : 0,
+                JsonConvert.SerializeObject(start, float3Converter.Converter),
+                JsonConvert.SerializeObject(control, float3Converter.Converter),
+                JsonConvert.SerializeObject(end, float3Converter.Converter),
+            };
+            return array.ToString(Formatting.None);
+        }
+
+        public override void SetCustomData(string json) {
+            if (string.IsNullOrEmpty(json)) return;
+
+            var data = JArray.Parse(json);
+            points = new MinMaxInt(data.Value<int>(0), Constants.MIN_LINE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1);
+            isClosed = data.Value<int>(1) == 1;
+            start = JsonConvert.DeserializeObject<float3>(data.Value<string>(2)!, float3Converter.Converter);
+            start = JsonConvert.DeserializeObject<float3>(data.Value<string>(3)!, float3Converter.Converter);
+            end = JsonConvert.DeserializeObject<float3>(data.Value<string>(4)!, float3Converter.Converter);
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdatePoints(int newValue) {
+            newValue = newValue.Clamped(Constants.MIN_LINE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1);
+            if (newValue == points) return;
+            
+            points.Value = newValue;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateClosed(bool newValue) {
+            if (newValue == isClosed) return;
+            isClosed = newValue;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateStart(float3 newValue) {
+            if (start.Equals(newValue)) return;
+            start = newValue;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+        
+        public void UpdateControl(float3 newValue) {
+            if (control.Equals(newValue)) return;
+            control = newValue;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateEnd(float3 newValue) {
+            if (end.Equals(newValue)) return;
+            end = newValue;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public override void RebindPorts() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
