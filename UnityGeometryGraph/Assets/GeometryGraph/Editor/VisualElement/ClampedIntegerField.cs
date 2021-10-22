7026d3926a7ee9d566c835d9f442356a78d0c3ac
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-10-22 11:30:48 +0300

    Add ClampedIntegerField visual element

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/VisualElement/ClampedIntegerField.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Globalization;
+using GeometryGraph.Editor.Utils;
+using UnityCommons;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace GeometryGraph.Editor {
+    public class ClampedIntegerField : TextValueField<int> {
+        private int? min;
+        private int? max;
+
+        public int? Min {
+            get => min;
+            set => min = value;
+        }
+
+        public int? Max {
+            get => max;
+            set => max = value;
+        }
+
+        public ClampedIntegerField() : this(null) {
+        }
+
+        public ClampedIntegerField(string label, int? min = null, int? max = null, int maxLength = -1)
+            : base(label, maxLength, new IntegerInput()) {
+            this.min = min;
+            this.max = max;
+            
+            AddToClassList(IntegerField.ussClassName);
+            labelElement.AddToClassList(IntegerField.labelUssClassName);
+            integerInput.AddToClassList(IntegerField.inputUssClassName);
+            AddLabelDragger<int>();
+        }
+
+        private IntegerInput integerInput => (IntegerInput)textInputBase;
+        
+        protected override string ValueToString(int v) {
+            return v.ToString(formatString, CultureInfo.InvariantCulture.NumberFormat);
+        }
+        
+        protected override int StringToValue(string str) {
+            if (long.TryParse(str, out var num)) {
+                return (int) num.Clamped(int.MinValue, int.MaxValue);
+            }
+
+            return default;
+        }
+
+        public override void ApplyInputDeviceDelta(Vector3 delta, DeltaSpeed speed, int startValue) {
+            integerInput.ApplyInputDeviceDelta(delta, speed, startValue);
+        }
+
+
+        public new class UxmlFactory : UxmlFactory<ClampedIntegerField, ClampedIntegerField.UxmlTraits> {
+        }
+        
+        public new class UxmlTraits : TextValueFieldTraits<int, UxmlIntAttributeDescription> {
+        }
+
+        private class IntegerInput : TextValueInput {
+            internal IntegerInput() {
+                formatString = "#######0";
+            }
+
+            private ClampedIntegerField parentIntegerField => (ClampedIntegerField)parent;
+
+            protected override string allowedCharacters => "0123456789-*/+%^()cosintaqrtelfundxvRL,=pPI#";
+
+            public override void ApplyInputDeviceDelta(Vector3 delta, DeltaSpeed speed, int startValue) {
+                double intDragSensitivity = NumericFieldDraggerUtility.CalculateIntDragSensitivity(startValue);
+                var acceleration = NumericFieldDraggerUtility.Acceleration(speed == DeltaSpeed.Fast, speed == DeltaSpeed.Slow);
+                var num = StringToValue(text) + (long)Math.Round(NumericFieldDraggerUtility.NiceDelta(delta, acceleration) * intDragSensitivity);
+
+                if (parentIntegerField.min != null) {
+                    num = num.Min((long)parentIntegerField.min);
+                }
+
+                if (parentIntegerField.max != null) {
+                    num = num.Max((long)parentIntegerField.max);
+                }
+                
+                if (parentIntegerField.isDelayed) text = ValueToString((int) num.Clamped(int.MinValue, int.MaxValue));
+                else parentIntegerField.value = (int) num.Clamped(int.MinValue, int.MaxValue);
+            }
+
+            protected override string ValueToString(int v) {
+                return v.ToString(formatString);
+            }
+
+            protected override int StringToValue(string str) {
+                if (long.TryParse(str, out var num)) {
+                    return (int) num.Clamped(int.MinValue, int.MaxValue);
+                }
+
+                return default;
+            }
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/VisualElement/Utils/NumericFieldDraggerUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace GeometryGraph.Editor.Utils {
+    internal class NumericFieldDraggerUtility {
+        private static bool s_UseYSign;
+
+        internal static float Acceleration(bool shiftPressed, bool altPressed) {
+            return (float)((shiftPressed ? 4.0 : 1.0) * (altPressed ? 0.25 : 1.0));
+        }
+
+        internal static float NiceDelta(Vector2 deviceDelta, float acceleration) {
+            deviceDelta.y = -deviceDelta.y;
+            if (Mathf.Abs(Mathf.Abs(deviceDelta.x) - Mathf.Abs(deviceDelta.y)) / (double)Mathf.Max(Mathf.Abs(deviceDelta.x), Mathf.Abs(deviceDelta.y)) > 0.100000001490116)
+                s_UseYSign = Mathf.Abs(deviceDelta.x) <= (double)Mathf.Abs(deviceDelta.y);
+            return s_UseYSign
+                ? Mathf.Sign(deviceDelta.y) * deviceDelta.magnitude * acceleration
+                : Mathf.Sign(deviceDelta.x) * deviceDelta.magnitude * acceleration;
+        }
+
+        internal static double CalculateFloatDragSensitivity(double value) {
+            return double.IsInfinity(value) || double.IsNaN(value) ? 0.0 : Math.Max(1.0, Math.Pow(Math.Abs(value), 0.5)) * 0.0299999993294477;
+        }
+
+        internal static long CalculateIntDragSensitivity(long value) {
+            return (long)Math.Max(1.0, Math.Pow(Math.Abs((double)value), 0.5) * 0.0299999993294477);
+        }
+    }
+}
