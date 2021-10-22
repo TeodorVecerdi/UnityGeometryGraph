13c771ca84add67d1f74eb6b703ac708e9885578
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-10-22 12:37:15 +0300

    Implement CirclePrimitiveCurve node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Curve/Primitive/CirclePrimitiveCurveNode.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.Graph;
+using Newtonsoft.Json.Linq;
+using UnityCommons;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
+namespace GeometryGraph.Editor {
+    [Title("Curve", "Primitive", "Circle")]
+    public class CirclePrimitiveCurveNode : AbstractNode<GeometryGraph.Runtime.Graph.CirclePrimitiveCurveNode> {
+        private GraphFrameworkPort pointsPort;
+        private GraphFrameworkPort radiusPort;
+        private GraphFrameworkPort resultPort;
+
+        private ClampedIntegerField pointsField;
+        private ClampedFloatField radiusField;
+
+        private int points = 8;
+        private float radius = 1.0f;
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Circle Primitive Curve");
+
+            (pointsPort, pointsField) = GraphFrameworkPort.CreateWithBackingField<ClampedIntegerField, int>("Points", Orientation.Horizontal, PortType.Integer, edgeConnectorListener, this, onDisconnect: (_, _) => RuntimeNode.UpdatePoints(points));
+            (radiusPort, radiusField) = GraphFrameworkPort.CreateWithBackingField<ClampedFloatField, float>("Radius", Orientation.Horizontal, PortType.Float, edgeConnectorListener, this, onDisconnect: (_, _) => RuntimeNode.UpdateRadius(radius));
+            resultPort = GraphFrameworkPort.Create("Circle Curve", Orientation.Horizontal, Direction.Output, Port.Capacity.Multi, PortType.Curve, edgeConnectorListener, this);
+
+            pointsField.Min = Constants.MIN_LINE_CURVE_RESOLUTION + 1;
+            pointsField.Max = Constants.MAX_CURVE_RESOLUTION + 1;
+            pointsField.RegisterValueChangedCallback(evt => {
+                var newValue = evt.newValue.Clamped(Constants.MIN_LINE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1);
+                if (newValue == points) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change circle curve points");
+                points = newValue;
+                RuntimeNode.UpdatePoints(points);
+            });
+
+            radiusField.Min = Constants.MIN_CIRCULAR_CURVE_RADIUS;
+            radiusField.RegisterValueChangedCallback(evt => {
+                var newValue = evt.newValue.Min(Constants.MIN_CIRCULAR_CURVE_RADIUS);
+                if (Math.Abs(newValue - radius) < Constants.FLOAT_TOLERANCE) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change circle curve radius");
+                radius = newValue;
+                RuntimeNode.UpdateRadius(radius);
+            });
+            
+            
+            pointsField.SetValueWithoutNotify(points);
+            radiusField.SetValueWithoutNotify(radius);
+            
+            pointsPort.Add(pointsField);
+            radiusPort.Add(radiusField);
+            AddPort(pointsPort);
+            AddPort(radiusPort);
+            AddPort(resultPort);
+            
+            Refresh();
+        }
+
+        public override void BindPorts() {
+            BindPort(pointsPort, RuntimeNode.PointsPort);
+            BindPort(radiusPort, RuntimeNode.RadiusPort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+
+        public override JObject GetNodeData() {
+            var root =  base.GetNodeData();
+            var array = new JArray {
+                points,
+                radius
+            };
+            root["d"] = array;
+            return root;
+        }
+
+        public override void SetNodeData(JObject jsonData) {
+            var array = jsonData["d"] as JArray;
+
+            points = array!.Value<int>(0);
+            radius = array!.Value<float>(1);
+            
+            pointsField.SetValueWithoutNotify(points);
+            radiusField.SetValueWithoutNotify(radius);
+            
+            RuntimeNode.UpdatePoints(points);
+            RuntimeNode.UpdateRadius(radius);
+            
+            base.SetNodeData(jsonData);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Curve/Primitives/CirclePrimitiveCurveNode.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using GeometryGraph.Runtime.Curve;
+using GeometryGraph.Runtime.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityCommons;
+
+namespace GeometryGraph.Runtime.Graph {
+    public class CirclePrimitiveCurveNode : RuntimeNode {
+        private MinMaxInt points = new (8, Constants.MIN_CIRCLE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1);
+        private MinMaxFloat radius = new (1.0f, Constants.MIN_CIRCULAR_CURVE_RADIUS);
+        private CurveData circleCurve;
+
+        public RuntimePort PointsPort { get; private set; }
+        public RuntimePort RadiusPort { get; private set; }
+        public RuntimePort ResultPort { get; private set; }
+
+        public CirclePrimitiveCurveNode(string guid) : base(guid) {
+            PointsPort = RuntimePort.Create(PortType.Integer, PortDirection.Input, this);
+            RadiusPort = RuntimePort.Create(PortType.Float, PortDirection.Input, this);
+            ResultPort = RuntimePort.Create(PortType.Curve, PortDirection.Output, this);
+        }
+
+        private void CalculateResult() {
+            circleCurve = CurvePrimitive.Circle(points - 1, radius);
+        }
+
+        protected override void OnPortValueChanged(Connection connection, RuntimePort port) {
+            if (port == ResultPort) return;
+            if (port == PointsPort) {
+                var newValue = GetValue(connection, (int)points);
+                if (newValue == points) return;
+                points.Value = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == RadiusPort) {
+                var newValue = GetValue(connection, (float)radius);
+                if (Math.Abs(newValue - radius) < Constants.FLOAT_TOLERANCE) return;
+                radius.Value = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            }
+        }
+
+        protected override object GetValueForPort(RuntimePort port) {
+            if (port != ResultPort) return null;
+            if (circleCurve == null) CalculateResult();
+            return circleCurve.Clone();
+        }
+
+        public override string GetCustomData() {
+            var array = new JArray {
+                (int)points,
+                (float)radius,
+            };
+            return array.ToString(Formatting.None);
+        }
+
+        public override void SetCustomData(string json) {
+            if (string.IsNullOrEmpty(json)) return;
+
+            var data = JArray.Parse(json);
+            points = new MinMaxInt(data.Value<int>(0), Constants.MIN_LINE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1);
+            radius = new MinMaxFloat(data.Value<float>(1), Constants.MIN_CIRCULAR_CURVE_RADIUS);
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdatePoints(int newPoints) {
+            newPoints = newPoints.Clamped(Constants.MIN_LINE_CURVE_RESOLUTION + 1, Constants.MAX_CURVE_RESOLUTION + 1);
+            if (newPoints == points) return;
+            
+            points.Value = newPoints;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateRadius(float newRadius) {
+            newRadius = newRadius.Min(Constants.MIN_CIRCULAR_CURVE_RADIUS);
+            if (Math.Abs(newRadius - radius) < Constants.FLOAT_TOLERANCE) return;
+            radius.Value = newRadius;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+        
+        public override void RebindPorts() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
