92814b9072746e374f60c12d7ca9e9c7e62dfebc
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-10-22 10:09:45 +0300

    Cleanup node styling code

--- UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Port/PortHelper.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Port/PortHelper.cs
@@ -12,38 +12,7 @@ namespace GeometryGraph.Editor {
                 return true;
             return port.Type == other.Type || compatiblePortTypes.Contains((port.Type, other.Type));
         }
-
-        public static Color PortColor(GraphFrameworkPort port) {
-            return Color.red;
-            // Note: Original PortColor implementation left as reference 
-            /*switch (port.Type) {
-                case PortType.Check:
-                    if (port.direction == Direction.Input)
-                        return new Color(0.2f, 0.73f, 1f);
-                    return new Color(0.5f, 0.98f, 1f);
-                case PortType.Trigger:
-                    if (port.direction == Direction.Input)
-                        return new Color(1f, 0.15f, 0.26f);
-                    return new Color(0.84f, 0.26f, 0.16f);
-                case PortType.Actor:
-                    if (port.direction == Direction.Input)
-                        return new Color(0.55f, 1f, 0.3f);
-                    return new Color(0.75f, 1f, 0.36f);
-                case PortType.Branch:
-                    if (port.direction == Direction.Input)
-                        return new Color(0.9f, 1f, 0.99f);
-                    return new Color(0.91f, 0.93f, 1f);
-                case PortType.Combiner:
-                    if (port.direction == Direction.Input)
-                        return new Color(0.45f, 0.25f, 1f);
-                    return new Color(0.45f, 0.25f, 1f);
-                case PortType.Fake:
-                    return Color.clear;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(port), port, "Undefined color for port type.");
-            }*/
-        }
-
+        
         // !! Update Runtime.PortValueConverter.Convert when adding stuff to this!
         private static readonly HashSet<(PortType, PortType)> compatiblePortTypes = new HashSet<(PortType, PortType)>(new PortTypeEqualityComparer()) {
             (PortType.Float, PortType.Integer), 
