82035d41394f7538e15c3c4e31e0655ced0928ba
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-10-24 16:53:46 +0300

    Add .InRange extension methods for integers and Index

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Misc/MiscExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace GeometryGraph.Runtime {
+    internal static class MiscExtensions {
+        internal static bool InRange(this Index index, Range range) {
+            if (range.Start.IsFromEnd || range.End.IsFromEnd) return false;
+            return index.Value >= range.Start.Value && index.Value < range.End.Value;
+        }
+        
+        internal static bool InRange(this int index, Range range) {
+            return index >= range.Start.Value && index < range.End.Value;
+        }
+        
+        internal static bool InRange(this Index index, ICollection collection) {
+            var actualIndex = index.IsFromEnd ? collection.Count - index.Value : index.Value; 
+            return actualIndex >= 0 && actualIndex < collection.Count;
+        }
+        internal static bool InRange(this int index, ICollection collection) {
+            return index >= 0 && index < collection.Count;
+        }
+    }
+}
