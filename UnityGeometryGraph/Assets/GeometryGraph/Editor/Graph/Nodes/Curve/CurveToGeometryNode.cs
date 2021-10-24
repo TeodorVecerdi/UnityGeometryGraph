1da2b1f6a31a8faccd2fb1e0467d1ece07ca2a1e
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-10-24 16:58:57 +0300

    Implement CurveToGeometry node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Curve/CurveToGeometryNode.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.Graph;
+using Newtonsoft.Json.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace GeometryGraph.Editor.Curve {
+    [Title("Curve", "Curve To Geometry")]
+    public class CurveToGeometryNode : AbstractNode<GeometryGraph.Runtime.Graph.CurveToGeometryNode> {
+        private GraphFrameworkPort inputCurvePort;
+        private GraphFrameworkPort profileCurvePort;
+        private GraphFrameworkPort rotationOffsetPort;
+        private GraphFrameworkPort closeCapsPort;
+        private GraphFrameworkPort resultPort;
+
+        private FloatField rotationOffsetField;
+        private Toggle closeCapsToggle;
+
+        private bool closeCaps;
+        private float rotationOffset;
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Curve To Geometry");
+            
+            inputCurvePort = GraphFrameworkPort.Create("Curve", Orientation.Horizontal, Direction.Input, Port.Capacity.Single, PortType.Curve, edgeConnectorListener, this);
+            profileCurvePort = GraphFrameworkPort.Create("Profile", Orientation.Horizontal, Direction.Input, Port.Capacity.Single, PortType.Curve, edgeConnectorListener, this, onConnect: OnProfileCurveConnected, onDisconnect: OnProfileCurveDisconnected);
+            (rotationOffsetPort, rotationOffsetField) = GraphFrameworkPort.CreateWithBackingField<FloatField, float>("Profile Rotation", Orientation.Horizontal, PortType.Float, edgeConnectorListener, this, onDisconnect: (_, _) => RuntimeNode.UpdateRotationOffset(rotationOffset));
+            (closeCapsPort, closeCapsToggle) = GraphFrameworkPort.CreateWithBackingField<Toggle, bool>("Close Caps", Orientation.Horizontal, PortType.Boolean, edgeConnectorListener, this, onDisconnect: (_, _) => RuntimeNode.UpdateCloseCaps(closeCaps));
+            resultPort = GraphFrameworkPort.Create("Geometry", Orientation.Horizontal, Direction.Output, Port.Capacity.Multi, PortType.Geometry, edgeConnectorListener, this);
+
+            rotationOffsetField.RegisterValueChangedCallback(evt => {
+                if (Math.Abs(evt.newValue - rotationOffset) < Constants.FLOAT_TOLERANCE) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change profile rotation");
+                rotationOffset = evt.newValue;
+                RuntimeNode.UpdateRotationOffset(rotationOffset);
+            });
+            
+            closeCapsToggle.RegisterValueChangedCallback(evt => {
+                if (evt.newValue == closeCaps) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Toggle close caps");
+                closeCaps = evt.newValue;
+                RuntimeNode.UpdateCloseCaps(closeCaps);
+            });
+            
+            rotationOffsetPort.Add(rotationOffsetField);
+            closeCapsPort.Add(closeCapsToggle);
+            
+            OnProfileCurveDisconnected(null, null);
+            
+            AddPort(inputCurvePort);
+            AddPort(profileCurvePort);
+            AddPort(rotationOffsetPort);
+            AddPort(closeCapsPort);
+            AddPort(resultPort);
+        }
+
+        private void OnProfileCurveConnected(Edge _1, GraphFrameworkPort _2) {
+            rotationOffsetPort.Show();
+            closeCapsPort.Show();
+        }
+
+        private void OnProfileCurveDisconnected(Edge _1, GraphFrameworkPort _2) {
+            rotationOffsetPort.HideAndDisconnect();
+            closeCapsPort.HideAndDisconnect();
+        }
+
+        public override void BindPorts() {
+            BindPort(inputCurvePort, RuntimeNode.InputCurvePort);
+            BindPort(profileCurvePort, RuntimeNode.ProfileCurvePort);
+            BindPort(rotationOffsetPort, RuntimeNode.RotationOffsetPort);
+            BindPort(closeCapsPort, RuntimeNode.CloseCapsPort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+
+        public override JObject GetNodeData() {
+            var root = base.GetNodeData();
+            var array = new JArray {
+                rotationOffset,
+                closeCaps ? 1 : 0,
+            };
+            root["d"] = array;
+            return root;
+        }
+
+        public override void SetNodeData(JObject jsonData) {
+            var array = jsonData["d"] as JArray;
+            
+            rotationOffset = array!.Value<float>(0);
+            closeCaps = array!.Value<int>(1) == 1;
+            
+            rotationOffsetField.SetValueWithoutNotify(rotationOffset);
+            closeCapsToggle.SetValueWithoutNotify(closeCaps);
+            
+            RuntimeNode.UpdateRotationOffset(rotationOffset);
+            RuntimeNode.UpdateCloseCaps(closeCaps);
+            
+            base.SetNodeData(jsonData);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Curve/CurveToGeometryNode.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using GeometryGraph.Runtime.Curve;
+using GeometryGraph.Runtime.Geometry;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GeometryGraph.Runtime.Graph {
+    public class CurveToGeometryNode : RuntimeNode {
+        private CurveData source;
+        private CurveData profile;
+        private float rotationOffset;
+        private bool closeCaps;
+        private GeometryData result;
+
+        public RuntimePort InputCurvePort { get; private set; }
+        public RuntimePort ProfileCurvePort { get; private set; }
+        public RuntimePort RotationOffsetPort { get; private set; }
+        public RuntimePort CloseCapsPort { get; private set; }
+        public RuntimePort ResultPort { get; private set; }
+
+        public CurveToGeometryNode(string guid) : base(guid) {
+            InputCurvePort = RuntimePort.Create(PortType.Curve, PortDirection.Input, this);
+            ProfileCurvePort = RuntimePort.Create(PortType.Curve, PortDirection.Input, this);
+            RotationOffsetPort = RuntimePort.Create(PortType.Float, PortDirection.Input, this);
+            CloseCapsPort = RuntimePort.Create(PortType.Boolean, PortDirection.Input, this);
+            ResultPort = RuntimePort.Create(PortType.Geometry, PortDirection.Output, this);
+        }
+
+        public void UpdateRotationOffset(float newValue) {
+            if (Math.Abs(newValue - rotationOffset) < Constants.FLOAT_TOLERANCE) return;
+            rotationOffset = newValue;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateCloseCaps(bool newValue) {
+            if (newValue == closeCaps) return;
+            closeCaps = newValue;
+            CalculateResult();
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        protected override void OnConnectionRemoved(Connection connection, RuntimePort port) {
+            if (port == InputCurvePort) {
+                source = null;
+                result = GeometryData.Empty;
+            } else if (port == ProfileCurvePort) {
+                profile = null;
+            }
+        }
+
+        protected override object GetValueForPort(RuntimePort port) {
+            if (port != ResultPort) return null;
+            if (result == null) {
+                CalculateResult();
+            }
+            
+            return result;
+        }
+
+        protected override void OnPortValueChanged(Connection connection, RuntimePort port) {
+            if (port == ResultPort) return;
+            if (port == InputCurvePort) {
+                source = GetValue(connection, CurveData.Empty).Clone();
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == ProfileCurvePort) {
+                profile = GetValue(connection, CurveData.Empty).Clone();
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == CloseCapsPort) {
+                var newValue = GetValue(connection, closeCaps);
+                if (newValue == closeCaps) return;
+                closeCaps = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            } else if (port == RotationOffsetPort) {
+                var newValue = GetValue(connection, rotationOffset);
+                if (Math.Abs(newValue - rotationOffset) < Constants.FLOAT_TOLERANCE) return;
+                rotationOffset = newValue;
+                CalculateResult();
+                NotifyPortValueChanged(ResultPort);
+            }
+        }
+
+        public override void RebindPorts() {
+            throw new NotImplementedException();
+        }
+
+        private void CalculateResult() {
+            if (source is not { Type: not CurveType.None }) {
+                DebugUtility.Log("Source curve was null");
+                result = GeometryData.Empty;
+                return;
+            }
+
+            if (profile is not { Type: not CurveType.None }) {
+                DebugUtility.Log("Profile curve was null");
+                result = CurveToGeometry.WithoutProfile(source);
+                return;
+            }
+
+            
+            if (RuntimeGraphObjectData.IsDuringSerialization) {
+                DebugUtility.Log("Attempting to generate geometry from curve during serialization. Aborting.");
+                result = null;
+                return;
+            }
+            DebugUtility.Log("Generated mesh with profile");
+            result = CurveToGeometry.WithProfile(source, profile, closeCaps, rotationOffset);
+        }
+
+        public override string GetCustomData() {
+            return new JArray {
+                rotationOffset,
+                closeCaps ? 1 : 0,
+            }.ToString(Formatting.None);
+        }
+
+        public override void SetCustomData(string json) {
+            var array = JArray.Parse(json);
+            rotationOffset = array.Value<float>(0);
+            closeCaps = array.Value<int>(1) == 1;
+            NotifyPortValueChanged(ResultPort);
+        }
+    }
+}
