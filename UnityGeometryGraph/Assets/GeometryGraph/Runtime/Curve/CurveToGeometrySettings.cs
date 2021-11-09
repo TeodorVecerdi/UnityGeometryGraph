83603cffc1ba384b5abdfcef2c86e50e9f64d303
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-09 16:50:37 +0200

    Add CapUVType setting to CurveToGeometrySettings

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Curve/CurveToGeometrySettings.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Curve/CurveToGeometrySettings.cs
@@ -1,6 +1,7 @@
 ﻿namespace GeometryGraph.Runtime.Curve {
     public readonly struct CurveToGeometrySettings {
         public bool CloseCaps { get; }
+        public CapUVType CapUvType { get; }
         public bool SeparateMaterialForCaps { get; }
         
         public bool ShadeSmoothCurve { get; }
@@ -9,13 +10,25 @@
         public float RotationOffset { get; }
         public float IncrementalRotationOffset { get; }
 
-        public CurveToGeometrySettings(bool closeCaps, bool separateMaterialForCaps, bool shadeSmoothCurve, bool shadeSmoothCaps, float rotationOffset, float incrementalRotationOffset) {
+        public CurveToGeometrySettings(bool closeCaps, bool separateMaterialForCaps, bool shadeSmoothCurve, bool shadeSmoothCaps, float rotationOffset, float incrementalRotationOffset, CapUVType capUVType) {
             CloseCaps = closeCaps;
             SeparateMaterialForCaps = separateMaterialForCaps;
             ShadeSmoothCurve = shadeSmoothCurve;
             ShadeSmoothCaps = shadeSmoothCaps;
             RotationOffset = rotationOffset;
             IncrementalRotationOffset = incrementalRotationOffset;
+            CapUvType = capUVType;
+        }
+        
+        public enum CapUVType {
+            /// <summary>Generate cap UVs in local space (relative to the cap)</summary>
+            LocalSpace = 0,
+            
+            /// <summary>Generate cap UVs in world space</summary>
+            WorldSpace = 1,
+            
+            /// <summary>Same as <see cref="WorldSpace"/> but offset to start at 0,0</summary> 
+            WorldSpaceAligned = 2,
         }
     }
 }
