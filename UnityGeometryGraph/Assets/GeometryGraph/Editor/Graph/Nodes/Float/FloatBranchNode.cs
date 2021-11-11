696ab2685dbdef65d88f4a6cdc0f06ac8a0c0df7
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-11 15:40:08 +0200

    Add Float Branch node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Float/FloatBranchNode.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.Graph;
+using Newtonsoft.Json.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+using Which = GeometryGraph.Runtime.Graph.ClampFloatNode.ClampFloatNode_Which;
+
+namespace GeometryGraph.Editor {
+    [Title("Float", "Branch")]
+    public class FloatBranchNode : AbstractNode<GeometryGraph.Runtime.Graph.FloatBranchNode> {
+        
+        private GraphFrameworkPort conditionPort;
+        private GraphFrameworkPort ifTruePort;
+        private GraphFrameworkPort ifFalsePort;
+        private GraphFrameworkPort resultPort;
+
+        private Toggle conditionToggle;
+        private FloatField ifTrueField;
+        private FloatField ifFalseField;
+
+        private bool condition;
+        private float ifTrue;
+        private float ifFalse;
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Branch (Float)");
+
+            (conditionPort, conditionToggle) = GraphFrameworkPort.CreateWithBackingField<Toggle, bool>("Condition", Orientation.Horizontal, PortType.Float, edgeConnectorListener, this, onDisconnect: (_, _) => RuntimeNode.UpdateCondition(condition));
+            (ifTruePort, ifTrueField) = GraphFrameworkPort.CreateWithBackingField<FloatField, float>("If True", Orientation.Horizontal, PortType.Float, edgeConnectorListener, this, onDisconnect: (_, _) => RuntimeNode.UpdateIfTrue(ifTrue));
+            (ifFalsePort, ifFalseField) = GraphFrameworkPort.CreateWithBackingField<FloatField, float>("If False", Orientation.Horizontal, PortType.Float, edgeConnectorListener, this, onDisconnect: (_, _) => RuntimeNode.UpdateIfFalse(ifFalse));
+            resultPort = GraphFrameworkPort.Create("Result", Orientation.Horizontal, Direction.Output, Port.Capacity.Multi, PortType.Float, edgeConnectorListener, this);
+
+            conditionToggle.RegisterValueChangedCallback(evt => {
+                if (evt.newValue == condition) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change branch node condition");
+                condition = evt.newValue;
+                RuntimeNode.UpdateCondition(condition);
+            });
+            
+            ifTrueField.RegisterValueChangedCallback(evt => {
+                if (Math.Abs(evt.newValue - ifTrue) < Constants.FLOAT_TOLERANCE) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change branch node if true value");
+                ifTrue = evt.newValue;
+                RuntimeNode.UpdateIfTrue(ifTrue);
+            });
+            
+            ifFalseField.RegisterValueChangedCallback(evt => {
+                if (Math.Abs(evt.newValue - ifFalse) < Constants.FLOAT_TOLERANCE) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change branch node if false value");
+                ifFalse = evt.newValue;
+                RuntimeNode.UpdateIfFalse(ifFalse);
+            });
+            
+
+            conditionPort.Add(conditionToggle);
+            ifTruePort.Add(ifTrueField);
+            ifFalsePort.Add(ifFalseField);
+            
+            AddPort(conditionPort);
+            AddPort(ifTruePort);
+            AddPort(ifFalsePort);
+            AddPort(resultPort);
+            
+            Refresh();
+        }
+        
+        public override void BindPorts() {
+            BindPort(conditionPort, RuntimeNode.ConditionPort);
+            BindPort(ifTruePort, RuntimeNode.IfTruePort);
+            BindPort(ifFalsePort, RuntimeNode.IfFalsePort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+
+        public override JObject GetNodeData() {
+            var root = base.GetNodeData();
+            var array = new JArray {
+                condition ? 1 : 0,
+                ifTrue,
+                ifFalse,
+            };
+            root["d"] = array;
+            return root;
+        }
+
+        public override void SetNodeData(JObject jsonData) {
+            var array = jsonData["d"] as JArray;
+            
+            condition = array!.Value<int>(0) == 1;
+            ifTrue = array.Value<float>(1);
+            ifFalse = array.Value<float>(2);
+            
+            conditionToggle.SetValueWithoutNotify(condition);
+            ifTrueField.SetValueWithoutNotify(ifTrue);
+            ifFalseField.SetValueWithoutNotify(ifFalse);
+            
+            RuntimeNode.UpdateCondition(condition);
+            RuntimeNode.UpdateIfTrue(ifTrue);
+            RuntimeNode.UpdateIfFalse(ifFalse);
+            
+            base.SetNodeData(jsonData);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Float/FloatBranchNode.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GeometryGraph.Runtime.Graph {
+    public class FloatBranchNode : RuntimeNode {
+        private bool condition;
+        private float ifTrue;
+        private float ifFalse;
+
+        public RuntimePort ConditionPort { get; private set; }
+        public RuntimePort IfTruePort { get; private set; }
+        public RuntimePort IfFalsePort { get; private set; }
+        public RuntimePort ResultPort { get; private set; }
+
+        public FloatBranchNode(string guid) : base(guid) {
+            ConditionPort = RuntimePort.Create(PortType.Boolean, PortDirection.Input, this);
+            IfTruePort = RuntimePort.Create(PortType.Float, PortDirection.Input, this);
+            IfFalsePort = RuntimePort.Create(PortType.Float, PortDirection.Input, this);
+            ResultPort = RuntimePort.Create(PortType.Float, PortDirection.Output, this);
+        }
+
+        public void UpdateCondition(bool newValue) {
+            if (newValue == condition) return;
+            condition = newValue;
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateIfTrue(float newValue) {
+            if (Math.Abs(newValue - ifTrue) < Constants.FLOAT_TOLERANCE) return;
+            ifTrue = newValue;
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        public void UpdateIfFalse(float newValue) {
+            if (Math.Abs(newValue - ifFalse) < Constants.FLOAT_TOLERANCE) return;
+            ifFalse = newValue;
+            NotifyPortValueChanged(ResultPort);
+        }
+
+        protected override object GetValueForPort(RuntimePort port) {
+            if (port != ResultPort) return null;
+            return condition ? ifTrue : ifFalse;
+        }
+
+        protected override void OnPortValueChanged(Connection connection, RuntimePort port) {
+            if (port == ResultPort) return;
+            if (port == ConditionPort) {
+                var newValue = GetValue(ConditionPort, condition);
+                if (newValue != condition) {
+                    condition = newValue;
+                    NotifyPortValueChanged(ResultPort);
+                }
+            } else if (port == IfTruePort) {
+                var newValue = GetValue(IfTruePort, ifTrue);
+                if (Math.Abs(newValue - ifTrue) > 0.000001f) {
+                    ifTrue = newValue;
+                    NotifyPortValueChanged(ResultPort);
+                }
+            } else if (port == IfFalsePort) {
+                var newValue = GetValue(IfFalsePort, ifFalse);
+                if (Math.Abs(newValue - ifFalse) > 0.000001f) {
+                    ifFalse = newValue;
+                    NotifyPortValueChanged(ResultPort);
+                }
+            }
+        }
+
+        public override void RebindPorts() {
+            throw new InvalidOperationException();
+        }
+
+        public override string GetCustomData() {
+            return new JArray {
+                condition ? 1 : 0,
+                ifTrue,
+                ifFalse
+            }.ToString(Formatting.None);
+        }
+
+        public override void SetCustomData(string json) {
+            JArray array = JArray.Parse(json);
+            condition = array.Value<int>(0) == 1;
+            ifTrue = array.Value<float>(1);
+            ifFalse = array.Value<float>(2);
+
+            NotifyPortValueChanged(ResultPort);
+        }
+    }
+}
