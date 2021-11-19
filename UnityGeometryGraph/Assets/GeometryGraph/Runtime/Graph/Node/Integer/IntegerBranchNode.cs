53146112a35fc559df9b3a6d4bdcc51cf740b778
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-19 10:45:10 +0200

    Cleanup Code Generator templates

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/IntegerBranchNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/IntegerBranchNode.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using GeometryGraph.Runtime.Attributes;
+﻿using GeometryGraph.Runtime.Attributes;
+using JetBrains.Annotations;
 
 namespace GeometryGraph.Runtime.Graph {
     [GenerateRuntimeNode]
     public partial class IntegerBranchNode {
-        [In]  public bool Condition { get; private set; }
-        [In]  public int IfTrue { get; private set; }
-        [In]  public int IfFalse { get; private set; }
+        [In] public bool Condition { get; private set; }
+        [In] public int IfTrue { get; private set; }
+        [In] public int IfFalse { get; private set; }
         [Out] public int Result { get; private set; }
         
-        }
+        [GetterMethod(nameof(Result), Inline = true), UsedImplicitly]
+        public int GetResult() => Condition ? IfTrue : IfFalse;
+    }
 }
