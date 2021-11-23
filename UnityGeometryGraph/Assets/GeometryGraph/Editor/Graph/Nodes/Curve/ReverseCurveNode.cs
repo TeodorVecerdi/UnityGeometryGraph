2044b6232d729ffa088504bdf039261bbb30866f
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-23 11:18:44 +0200

    Add ReverseCurve node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Curve/ReverseCurveNode.cs
@@ -0,0 +1,26 @@
+﻿using GeometryGraph.Runtime.Graph;
+using UnityEditor.Experimental.GraphView;
+
+namespace GeometryGraph.Editor {
+    [Title("Curve", "Reverse Curve")]
+    public class ReverseCurveNode : AbstractNode<GeometryGraph.Runtime.Graph.ReverseCurveNode> {
+        private GraphFrameworkPort inputPort;
+        private GraphFrameworkPort resultPort;
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Reverse Curve");
+            
+            inputPort = GraphFrameworkPort.Create("Curve", Direction.Input, Port.Capacity.Single, PortType.Curve, this);
+            resultPort = GraphFrameworkPort.Create("Result", Direction.Output, Port.Capacity.Multi, PortType.Curve, this);
+            
+            AddPort(inputPort);
+            AddPort(resultPort);
+        }
+
+        public override void BindPorts() {
+            BindPort(inputPort, RuntimeNode.InputPort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Curve/ReverseCurveNode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using GeometryGraph.Runtime.Attributes;
+using GeometryGraph.Runtime.Curve;
+using Unity.Mathematics;
+
+namespace GeometryGraph.Runtime.Graph {
+    [GenerateRuntimeNode]
+    public partial class ReverseCurveNode {
+        [In] public CurveData Input { get; private set; }
+        [Out] public CurveData Result { get; private set; }
+
+        protected override void OnConnectionRemoved(Connection connection, RuntimePort port) {
+            if (port == InputPort) {
+                Input = CurveData.Empty;
+                Result = CurveData.Empty;
+                NotifyPortValueChanged(ResultPort);
+            }
+        }
+
+        [CalculatesProperty(nameof(Result))]
+        private void Calculate() {
+            if (Input == null || Input.Points == 0) {
+                Result = CurveData.Empty;
+                return;
+            }
+
+            List<float3> newPosition = new List<float3>();
+            List<float3> newTangent = new List<float3>();
+            List<float3> newNormal = new List<float3>();
+            
+            for (int i = Input.Points - 1; i >= 0; --i) {
+                newPosition.Add(Input.Position[i]);
+                newTangent.Add(-Input.Tangent[i]);
+                newNormal.Add(-Input.Normal[i]);
+            }
+            
+            Result = new CurveData(Input.Type, Input.Points, Input.IsClosed, newPosition, newTangent, newNormal, new List<float3>(Input.Binormal));
+        }
+    }
+}
