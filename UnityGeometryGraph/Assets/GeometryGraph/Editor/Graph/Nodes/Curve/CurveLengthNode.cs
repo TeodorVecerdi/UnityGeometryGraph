0a534269fb1bd19967a5bc72303bc163099d873e
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-23 11:07:55 +0200

    Add CurveLength node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Curve/CurveLengthNode.cs
@@ -0,0 +1,26 @@
+﻿using GeometryGraph.Runtime.Graph;
+using UnityEditor.Experimental.GraphView;
+
+namespace GeometryGraph.Editor {
+    [Title("Curve", "Curve Length")]
+    public class CurveLengthNode : AbstractNode<GeometryGraph.Runtime.Graph.CurveLengthNode> {
+        private GraphFrameworkPort curvePort;
+        private GraphFrameworkPort lengthPort;
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Curve Length");
+            
+            curvePort = GraphFrameworkPort.Create("Curve", Direction.Input, Port.Capacity.Single, PortType.Curve, this);
+            lengthPort = GraphFrameworkPort.Create("Length", Direction.Output, Port.Capacity.Multi, PortType.Float, this);
+            
+            AddPort(curvePort);
+            AddPort(lengthPort);
+        }
+
+        public override void BindPorts() {
+            BindPort(curvePort, RuntimeNode.CurvePort);
+            BindPort(lengthPort, RuntimeNode.LengthPort);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Curve/CurveLengthNode.cs
@@ -0,0 +1,25 @@
+﻿using GeometryGraph.Runtime.Attributes;
+using GeometryGraph.Runtime.Curve;
+using Unity.Mathematics;
+
+namespace GeometryGraph.Runtime.Graph {
+    [GenerateRuntimeNode]
+    public partial class CurveLengthNode {
+        [In] public CurveData Curve { get; private set; }
+        [Out] public float Length { get; private set; }
+
+        [CalculatesProperty(nameof(Length))]
+        private void Calculate() {
+            if (Curve == null || Curve.Points == 0) {
+                Length = 0;
+                return;
+            }
+            
+            float length = 0f;
+            for (int i = 0; i < Curve.Points - 1; i++) {
+                length += math.distance(Curve.Position[i], Curve.Position[i + 1]);
+            }
+            Length = length;
+        }
+    }
+}
