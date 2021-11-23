d0f77eba0780740264e2a4e88a3800cf793a7759
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-23 10:20:42 +0200

    Add min and max range to RandomFloatNode

--- UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Input/RandomFloatNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Input/RandomFloatNode.cs
@@ -1,4 +1,6 @@
-﻿using GeometryGraph.Runtime.Graph;
+﻿using System;
+using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.Graph;
 using Newtonsoft.Json.Linq;
 using UnityEditor.Experimental.GraphView;
 using UnityEditor.UIElements;
@@ -30,12 +32,36 @@ namespace GeometryGraph.Editor {
             (maxPort, maxField) = GraphFrameworkPort.CreateWithBackingField<FloatField, float>("Max", PortType.Float, this, onDisconnect: (_, _) => RuntimeNode.UpdateMax(max));
 
             seedField.RegisterValueChangedCallback(evt => {
-                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Changed seed");
+                if (seed == evt.newValue) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change seed value");
                 seed = evt.newValue;
                 RuntimeNode.UpdateSeed(seed);
             });
-            AddPort(seedPort);
+            
+            minField.RegisterValueChangedCallback(evt => {
+                if (Math.Abs(min - evt.newValue) < Constants.FLOAT_TOLERANCE) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change min value");
+                min = evt.newValue;
+                RuntimeNode.UpdateMin(min);
+            });
+            
+            maxField.RegisterValueChangedCallback(evt => {
+                if (Math.Abs(max - evt.newValue) < Constants.FLOAT_TOLERANCE) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change max value");
+                max = evt.newValue;
+                RuntimeNode.UpdateMax(max);
+            });
+            
+            minField.SetValueWithoutNotify(0.0f);
+            maxField.SetValueWithoutNotify(1.0f);
+            
             seedPort.Add(seedField);
+            minPort.Add(minField);
+            maxPort.Add(maxField);
+            
+            AddPort(seedPort);
+            AddPort(minPort);
+            AddPort(maxPort);
             AddPort(valuePort);
             
             Refresh();
@@ -43,19 +69,34 @@ namespace GeometryGraph.Editor {
         
         public override void BindPorts() {
             BindPort(seedPort, RuntimeNode.SeedPort);
+            BindPort(minPort, RuntimeNode.MinPort);
+            BindPort(maxPort, RuntimeNode.MaxPort);
             BindPort(valuePort, RuntimeNode.ValuePort);
         }
 
         public override JObject GetNodeData() {
-            var root =  base.GetNodeData();
-            root["s"] = seed;
+            JObject root =  base.GetNodeData();
+            root["d"] = new JArray {
+                seed,
+                min,
+                max
+            };
             return root;
         }
 
         public override void SetNodeData(JObject jsonData) {
-            seed = jsonData.Value<int>("s");
+            JArray array = jsonData["d"] as JArray;
+            seed = array.Value<int>(0);
+            min = array.Value<float>(1);
+            max = array.Value<float>(2);
+            
             seedField.SetValueWithoutNotify(seed);
+            minField.SetValueWithoutNotify(min);
+            maxField.SetValueWithoutNotify(max);
+            
             RuntimeNode.UpdateSeed(seed);
+            RuntimeNode.UpdateMin(min);
+            RuntimeNode.UpdateMax(max);
             
             base.SetNodeData(jsonData);
         }
--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Input/RandomFloatNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Input/RandomFloatNode.cs
@@ -7,17 +7,19 @@ namespace GeometryGraph.Runtime.Graph {
     [GenerateRuntimeNode]
     public partial class RandomFloatNode {
         [In] public int Seed { get; private set; }
+        [In] public float Min { get; private set; } = 0.0f;
+        [In] public float Max { get; private set; } = 1.0f;
         [Out] public float Value { get; private set; }
         
         [GetterMethod(nameof(Value), Inline = true), UsedImplicitly] 
-        private float GetValue() => Rand.FloatSeeded(Seed);
+        private float GetValue() => Rand.RangeSeeded(Min, Max, Seed);
 
         public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
             if (port != ValuePort) yield break;
             if (count < 0) yield break;
             Rand.PushState(Seed);
             for (var i = 0; i < count; i++) {
-                yield return Rand.Float;
+                yield return Rand.Range(Min, Max);
             }
             Rand.PopState();
         }
