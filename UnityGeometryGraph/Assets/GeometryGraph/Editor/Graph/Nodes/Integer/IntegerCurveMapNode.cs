1cd7e746ecaa971dae5042c1bfd28035ed3a377d
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-23 16:26:02 +0200

    Add IntegerCurveMap node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Integer/IntegerCurveMapNode.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Reflection;
+using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.Graph;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using AnimationCurve = GeometryGraph.Runtime.Data.AnimationCurve;
+
+namespace GeometryGraph.Editor {
+    [Title("Integer", "Curve Map")]
+    public class IntegerCurveMapNode : AbstractNode<GeometryGraph.Runtime.Graph.IntegerCurveMapNode> {
+        private int value;
+        private int min = 0;
+        private int max = 100;
+
+        private IntegerField valueField;
+        private IntegerField minField;
+        private IntegerField maxField;
+        private CurveField curveField;
+        
+        private GraphFrameworkPort valuePort;
+        private GraphFrameworkPort minPort;
+        private GraphFrameworkPort maxPort;
+        private GraphFrameworkPort resultPort;
+
+        private AnimationCurve curve = new AnimationCurve(UnityEngine.AnimationCurve.Linear(0.0f, 0.0f, 1.0f, 1.0f));
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Curve Map (Integer)");
+
+            (valuePort, valueField) = GraphFrameworkPort.CreateWithBackingField<IntegerField, int>("Value", PortType.Integer, this, onDisconnect: (_, _) => RuntimeNode.UpdateValue(value));
+            (minPort, minField) = GraphFrameworkPort.CreateWithBackingField<IntegerField, int>("Min", PortType.Integer, this, onDisconnect: (_, _) => RuntimeNode.UpdateMin(min));
+            (maxPort, maxField) = GraphFrameworkPort.CreateWithBackingField<IntegerField, int>("Max", PortType.Integer, this, onDisconnect: (_, _) => RuntimeNode.UpdateMax(max));
+            resultPort = GraphFrameworkPort.Create("Result", Direction.Output, Port.Capacity.Multi, PortType.Integer, this);
+
+            valueField.RegisterValueChangedCallback(evt => {
+                if (evt.newValue == value) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change value");
+                value = evt.newValue;
+                RuntimeNode.UpdateValue(value);
+            });
+            
+            minField.RegisterValueChangedCallback(evt => {
+                if (evt.newValue == min) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change min");
+                min = evt.newValue;
+                RuntimeNode.UpdateMin(min);
+            });
+
+            maxField.RegisterValueChangedCallback(evt => {
+                if (evt.newValue == max) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change max");
+                max = evt.newValue;
+                RuntimeNode.UpdateMax(max);
+            });
+            
+            curveField = new CurveField { renderMode = CurveField.RenderMode.Mesh };
+            InjectStyle();
+
+            curveField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change curve");
+                curve = (AnimationCurve) evt.newValue;
+                RuntimeNode.UpdateCurve(curve);
+            });
+
+            curveField.SetValueWithoutNotify(UnityEngine.AnimationCurve.Linear(0.0f, 0.0f, 1.0f, 1.0f));
+            valueField.SetValueWithoutNotify(value);
+            minField.SetValueWithoutNotify(min);
+            maxField.SetValueWithoutNotify(max);
+
+            inputContainer.Add(curveField);
+            valuePort.Add(valueField);
+            minPort.Add(minField);
+            maxPort.Add(maxField);
+            
+            AddPort(valuePort);
+            AddPort(minPort);
+            AddPort(maxPort);
+            AddPort(resultPort);
+            
+            Refresh();
+        }
+
+        private void InjectStyle() {
+            // I honestly don't know why unity wouldn't make this accessible.
+            // They even have a property for it but decided to make it private: `private Color curveColor => this.m_CurveColor;`
+            // Honestly, the class is a big mess in general.
+            typeof(CurveField).GetField("m_CurveColor", BindingFlags.Instance | BindingFlags.NonPublic)!
+                              .SetValue(curveField, new Color(0.8f, 0.8f, 0.8f));
+            
+            // The curve field has a weird 1px high element with a different background color.
+            StyleColor backgroundColor = curveField[0][0][0].style.backgroundColor;
+            backgroundColor.value = new Color(0x2E / (float)0xFF, 0x2E / (float)0xFF, 0x2E / (float)0xFF);
+            curveField[0][0][0].style.backgroundColor = backgroundColor;
+        }
+
+        public override void BindPorts() {
+            BindPort(valuePort, RuntimeNode.ValuePort);
+            BindPort(minPort, RuntimeNode.MinPort);
+            BindPort(maxPort, RuntimeNode.MaxPort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+
+        public override JObject GetNodeData() {
+            JObject root = base.GetNodeData();
+            root["d"] = new JArray {
+                value,
+                min,
+                max,
+                Serializer.AnimationCurve(curve),
+            };
+            return root;
+        }
+
+        public override void SetNodeData(JObject jsonData) {
+            JArray array = jsonData["d"] as JArray;
+            value = array.Value<int>(0);
+            min = array.Value<int>(1);
+            max = array.Value<int>(2);
+            curve = Deserializer.AnimationCurve(array[3] as JObject);
+            
+            valueField.SetValueWithoutNotify(value);
+            minField.SetValueWithoutNotify(min);
+            maxField.SetValueWithoutNotify(max);
+            curveField.SetValueWithoutNotify(curve.UnityCurve);
+            
+            RuntimeNode.UpdateValue(value);
+            RuntimeNode.UpdateMin(min);
+            RuntimeNode.UpdateMax(max);
+            RuntimeNode.UpdateCurve(curve);
+
+            base.SetNodeData(jsonData);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/IntegerCurveMapNode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using GeometryGraph.Runtime.Attributes;
+using UnityCommons;
+using UnityEngine;
+using AnimationCurve = GeometryGraph.Runtime.Data.AnimationCurve;
+
+namespace GeometryGraph.Runtime.Graph {
+    [GenerateRuntimeNode]
+    [AdditionalUsingStatements("GeometryGraph.Runtime.Serialization")]
+    public partial class IntegerCurveMapNode {
+        [In] public int Value { get; private set; }
+        [In] public int Min { get; private set; } = 0;
+        [In] public int Max { get; private set; } = 100;
+
+        [CustomSerialization("Serializer.AnimationCurve({self})", "{self} = Deserializer.AnimationCurve({storage}[{index}] as JObject)")]
+        [Setting(GenerateEquality = false)]
+        public AnimationCurve Curve { get; private set; } = (AnimationCurve)UnityEngine.AnimationCurve.Linear(0.0f, 0.0f, 1.0f, 1.0f);
+
+        [Out] public int Result { get; private set; }
+
+        [CalculatesProperty(nameof(Result))]
+        private void Calculate() {
+            Result = Mathf.FloorToInt(
+                Curve.Evaluate(
+                    ((float)Value).Map(Min, Max, 0.0f, 1.0f)
+                ).Map(0.0f, 1.0f, Min, Max)
+            );
+        }
+
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if (port != ResultPort && count <= 0) yield break;
+            IEnumerable<int> values = GetValues(ValuePort, count, Value);
+            foreach (int value in values) {
+                yield return Curve.Evaluate(((float)value).Map(Min, Max, 0.0f, 1.0f));
+            }
+        }
+    }
+}
