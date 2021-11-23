b901b9b657e9434c4310b576d18680f21b292b8b
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-23 16:02:58 +0200

    Add IntegerGradientMap node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Integer/IntegerGradientMapNode.cs
@@ -0,0 +1,149 @@
+﻿using GeometryGraph.Runtime.Graph;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Gradient = GeometryGraph.Runtime.Data.Gradient;
+
+namespace GeometryGraph.Editor {
+    [Title("Integer", "Gradient Map")]
+    public class IntegerGradientMapNode : AbstractNode<GeometryGraph.Runtime.Graph.IntegerGradientMapNode> {
+        private int value;
+        private int min = 0;
+        private int max = 100;
+        
+        private IntegerField valueField;
+        private IntegerField minField;
+        private IntegerField maxField;
+        
+        private GraphFrameworkPort valuePort;
+        private GraphFrameworkPort minPort;
+        private GraphFrameworkPort maxPort;
+        private GraphFrameworkPort resultRGBPort;
+        private GraphFrameworkPort resultAlphaPort;
+
+        private GradientField gradientField;
+        private GradientField colorOnlyField;
+
+        private Gradient gradient = (Gradient) GeometryGraph.Runtime.Graph.FloatGradientMapNode.Default;
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Gradient Map (Integer)");
+
+            (valuePort, valueField) = GraphFrameworkPort.CreateWithBackingField<IntegerField, int>("Value", PortType.Integer, this, onDisconnect: (_, _) => RuntimeNode.UpdateValue(value));
+            (minPort, minField) = GraphFrameworkPort.CreateWithBackingField<IntegerField, int>("Min", PortType.Integer, this, onDisconnect: (_, _) => RuntimeNode.UpdateMin(min));
+            (maxPort, maxField) = GraphFrameworkPort.CreateWithBackingField<IntegerField, int>("Max", PortType.Integer, this, onDisconnect: (_, _) => RuntimeNode.UpdateMax(max));
+            
+            resultRGBPort = GraphFrameworkPort.Create("Color", Direction.Output, Port.Capacity.Multi, PortType.Vector, this);
+            resultAlphaPort = GraphFrameworkPort.Create("Alpha", Direction.Output, Port.Capacity.Multi, PortType.Float, this);
+
+            valueField.RegisterValueChangedCallback(evt => {
+                if (evt.newValue == value) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change value");
+                value = evt.newValue;
+                RuntimeNode.UpdateValue(value);
+            });
+            
+            minField.RegisterValueChangedCallback(evt => {
+                if (evt.newValue == min) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change min");
+                min = evt.newValue;
+                RuntimeNode.UpdateMin(min);
+            });
+            
+            maxField.RegisterValueChangedCallback(evt => {
+                if (evt.newValue == max) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change max");
+                max = evt.newValue;
+                RuntimeNode.UpdateMax(max);
+            });
+
+            gradientField = new GradientField();
+            gradientField.AddToClassList("gradient-field-main");
+            colorOnlyField = new GradientField();
+            colorOnlyField.AddToClassList("gradient-field-color");
+            colorOnlyField.SetEnabled(false);
+
+            gradientField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change gradient");
+                gradient = (Gradient) evt.newValue;
+                SetColorField(evt.newValue);
+                RuntimeNode.UpdateGradient(gradient);
+            });
+
+            gradientField.SetValueWithoutNotify(gradient.UnityGradient);
+            SetColorField(gradient.UnityGradient);
+            valueField.SetValueWithoutNotify(value);
+            minField.SetValueWithoutNotify(min);
+            maxField.SetValueWithoutNotify(max);
+
+            VisualElement gradientContainer = new VisualElement();
+            gradientContainer.AddToClassList("gradient-container");
+            gradientContainer.Add(gradientField);
+            gradientContainer.Add(colorOnlyField);
+            
+            inputContainer.Add(gradientContainer);
+            valuePort.Add(valueField);
+            minPort.Add(minField);
+            maxPort.Add(maxField);
+            
+            AddPort(valuePort);
+            AddPort(minPort);
+            AddPort(maxPort);
+            AddPort(resultRGBPort);
+            AddPort(resultAlphaPort);
+
+            Refresh();
+        }
+
+        private void SetColorField(UnityEngine.Gradient gradient) {
+            colorOnlyField.value = new UnityEngine.Gradient {colorKeys = gradient.colorKeys, alphaKeys = new []{new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f)}};
+        }
+
+        public override void BindPorts() {
+            BindPort(valuePort, RuntimeNode.ValuePort);
+            BindPort(minPort, RuntimeNode.MinPort);
+            BindPort(maxPort, RuntimeNode.MaxPort);
+            BindPort(resultRGBPort, RuntimeNode.ResultRGBPort);
+            BindPort(resultAlphaPort, RuntimeNode.ResultAlphaPort);
+        }
+
+        public override JObject GetNodeData() {
+            JObject root = base.GetNodeData();
+            root["d"] = new JArray {
+                value,
+                min,
+                max,
+                Serializer.Gradient(gradient),
+            };
+            return root;
+        }
+
+        public override void SetNodeData(JObject jsonData) {
+            JArray array = jsonData["d"] as JArray;
+            value = array.Value<int>(0);
+            min = array.Value<int>(1);
+            max = array.Value<int>(2);
+            gradient = Deserializer.Gradient(array[3] as JObject);
+            
+            valueField.SetValueWithoutNotify(value);
+            minField.SetValueWithoutNotify(min);
+            maxField.SetValueWithoutNotify(max);
+            gradientField.SetValueWithoutNotify(gradient.UnityGradient);
+            SetColorField(gradient.UnityGradient);
+            
+            RuntimeNode.UpdateValue(value);
+            RuntimeNode.UpdateMin(min);
+            RuntimeNode.UpdateMax(max);
+            RuntimeNode.UpdateGradient(gradient);
+
+            base.SetNodeData(jsonData);
+        }
+    }
+}
--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/IntegerGradientMapNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/IntegerGradientMapNode.cs
@@ -2,24 +2,27 @@
 using GeometryGraph.Runtime.Attributes;
 using GeometryGraph.Runtime.Data;
 using Unity.Mathematics;
+using UnityCommons;
 using UnityEngine;
 using Gradient = GeometryGraph.Runtime.Data.Gradient;
 
 namespace GeometryGraph.Runtime.Graph {
     [GenerateRuntimeNode]
     [AdditionalUsingStatements("GeometryGraph.Runtime.Serialization")]
-    public partial class FloatGradientMapNode {
-        [In] public float Value { get; private set; }
+    public partial class IntegerGradientMapNode {
+        [In] public int Value { get; private set; }
+        [In] public int Min { get; private set; } = 0;
+        [In] public int Max { get; private set; } = 100;
 
         [CustomSerialization("Serializer.Gradient({self})", "{self} = Deserializer.Gradient({storage}[{index}] as JObject)")]
         [Setting(GenerateEquality = false)]
         public Gradient Gradient { get; private set; } = (Gradient)Default;
-
+        
         [Out] public float3 ResultRGB { get; private set; }
         [Out] public float ResultAlpha { get; private set; }
 
         private RGBAlphaPair result;
-        
+
         [GetterMethod(nameof(ResultRGB), Inline = true)]
         private float3 GetResultRGB() => result.RGB;
 
@@ -28,14 +31,15 @@ namespace GeometryGraph.Runtime.Graph {
 
         [CalculatesAllProperties]
         private void Calculate() {
-            result = Gradient.Evaluate(Value);
+            if (Min > Max) (Min, Max) = (Max, Min);
+            result = Gradient.Evaluate(((float)Value).Map(Min, Max, 0.0f, 1.0f));
         }
-
+        
         public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
             if (count <= 0) yield break;
-            IEnumerable<float> values = GetValues(ValuePort, count, Value);
-            foreach (float value in values) {
-                RGBAlphaPair evaluated = Gradient.Evaluate(value);
+            IEnumerable<int> values = GetValues(ValuePort, count, Value);
+            foreach (int value in values) {
+                RGBAlphaPair evaluated = Gradient.Evaluate(((float)value).Map(Min, Max, 0.0f, 1.0f));
                 if (port == ResultRGBPort) {
                     yield return evaluated.RGB;
                 } else if (port == ResultAlphaPort) {
