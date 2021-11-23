7f36c8a9d8957a7b8f3d543277afa1fb1c8822c8
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-23 10:30:54 +0200

    Add RandomInteger node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Input/RandomIntegerNode.cs
@@ -0,0 +1,102 @@
+﻿using GeometryGraph.Runtime.Graph;
+using Newtonsoft.Json.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace GeometryGraph.Editor {
+    [Title("Input", "Random Float")]
+    public class RandomIntegerNode : AbstractNode<GeometryGraph.Runtime.Graph.RandomIntegerNode> {
+        private int seed;
+        private int min = 0;
+        private int max = 100;
+        
+        private IntegerField seedField;
+        private IntegerField minField;
+        private IntegerField maxField;
+        
+        private GraphFrameworkPort seedPort;
+        private GraphFrameworkPort minPort;
+        private GraphFrameworkPort maxPort;
+        private GraphFrameworkPort valuePort;
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Random Integer");
+
+            valuePort = GraphFrameworkPort.Create("Value", Direction.Output, Port.Capacity.Multi, PortType.Integer, this);
+            (seedPort, seedField) = GraphFrameworkPort.CreateWithBackingField<IntegerField, int>("Seed", PortType.Integer, this, onDisconnect: (_, _) => RuntimeNode.UpdateSeed(seed));
+            (minPort, minField) = GraphFrameworkPort.CreateWithBackingField<IntegerField, int>("Min", PortType.Integer, this, onDisconnect: (_, _) => RuntimeNode.UpdateMin(min));
+            (maxPort, maxField) = GraphFrameworkPort.CreateWithBackingField<IntegerField, int>("Max", PortType.Integer, this, onDisconnect: (_, _) => RuntimeNode.UpdateMax(max));
+
+            seedField.RegisterValueChangedCallback(evt => {
+                if (seed == evt.newValue) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change seed value");
+                seed = evt.newValue;
+                RuntimeNode.UpdateSeed(seed);
+            });
+            
+            minField.RegisterValueChangedCallback(evt => {
+                if (min == evt.newValue) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change min value");
+                min = evt.newValue;
+                RuntimeNode.UpdateMin(min);
+            });
+            
+            maxField.RegisterValueChangedCallback(evt => {
+                if (max == evt.newValue) return;
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change max value");
+                max = evt.newValue;
+                RuntimeNode.UpdateMax(max);
+            });
+            
+            minField.SetValueWithoutNotify(0);
+            maxField.SetValueWithoutNotify(100);
+            
+            seedPort.Add(seedField);
+            minPort.Add(minField);
+            maxPort.Add(maxField);
+            
+            AddPort(seedPort);
+            AddPort(minPort);
+            AddPort(maxPort);
+            AddPort(valuePort);
+            
+            Refresh();
+        }
+        
+        public override void BindPorts() {
+            BindPort(seedPort, RuntimeNode.SeedPort);
+            BindPort(minPort, RuntimeNode.MinPort);
+            BindPort(maxPort, RuntimeNode.MaxPort);
+            BindPort(valuePort, RuntimeNode.ValuePort);
+        }
+
+        public override JObject GetNodeData() {
+            JObject root =  base.GetNodeData();
+            root["d"] = new JArray {
+                seed,
+                min,
+                max
+            };
+            return root;
+        }
+
+        public override void SetNodeData(JObject jsonData) {
+            JArray array = jsonData["d"] as JArray;
+            seed = array.Value<int>(0);
+            min = array.Value<int>(1);
+            max = array.Value<int>(2);
+            
+            seedField.SetValueWithoutNotify(seed);
+            minField.SetValueWithoutNotify(min);
+            maxField.SetValueWithoutNotify(max);
+            
+            RuntimeNode.UpdateSeed(seed);
+            RuntimeNode.UpdateMin(min);
+            RuntimeNode.UpdateMax(max);
+            
+            base.SetNodeData(jsonData);
+        }
+    }
+}
--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Input/RandomIntegerNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Input/RandomIntegerNode.cs
@@ -4,20 +4,20 @@ using UnityCommons;
 
 namespace GeometryGraph.Runtime.Graph {
     [GenerateRuntimeNode]
-    public partial class RandomFloatNode {
+    public partial class RandomIntegerNode {
         [In] public int Seed { get; private set; }
-        [In] public float Min { get; private set; } = 0.0f;
-        [In] public float Max { get; private set; } = 1.0f;
-        [Out] public float Value { get; private set; }
-        
-        [GetterMethod(nameof(Value), Inline = true)] 
-        private float GetValue() => Rand.RangeSeeded(Min, Max, Seed);
+        [In] public int Min { get; private set; } = 0;
+        [In] public int Max { get; private set; } = 100;
+        [Out] public int Value { get; private set; }
+
+        [GetterMethod(nameof(Value), Inline = true)]
+        private int GetResult() => Rand.RangeSeeded(Min, Max, Seed);
 
         public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
             if (port != ValuePort) yield break;
             if (count <= 0) yield break;
             Rand.PushState(Seed);
-            for (var i = 0; i < count; i++) {
+            for (int i = 0; i < count; i++) {
                 yield return Rand.Range(Min, Max);
             }
             Rand.PopState();
