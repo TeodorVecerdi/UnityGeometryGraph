f97d2c139e5d5456f4b6b47af2d0175319636033
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-29 11:17:07 +0200

    Add support for multiple values in IntegerBranchNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/IntegerBranchNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/IntegerBranchNode.cs
@@ -1,5 +1,6 @@
-﻿using GeometryGraph.Runtime.Attributes;
-using JetBrains.Annotations;
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeometryGraph.Runtime.Attributes;
 
 namespace GeometryGraph.Runtime.Graph {
     [GenerateRuntimeNode]
@@ -9,7 +10,37 @@ namespace GeometryGraph.Runtime.Graph {
         [In] public int IfFalse { get; private set; }
         [Out] public int Result { get; private set; }
         
-        [GetterMethod(nameof(Result), Inline = true), UsedImplicitly]
+        private readonly List<int> results = new List<int>();
+        private bool resultsDirty = true;
+
+        [GetterMethod(nameof(Result), Inline = true), ]
         private int GetResult() => Condition ? IfTrue : IfFalse;
+        
+        [CalculatesAllProperties] private void MarkResultsDirty() => resultsDirty = true;
+        
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if (port != ResultPort || count <= 0) yield break;
+            if (!resultsDirty && results.Count == count) {
+                for (int i = 0; i < count; i++) {
+                    yield return results[i];
+                }
+             
+                yield break;
+            }
+
+            List<bool> conditions = GetValues(ConditionPort, count, Condition).ToList();
+            List<int> ifTrue = GetValues(IfTruePort, count, IfTrue).ToList();
+            List<int> ifFalse = GetValues(IfFalsePort, count, IfFalse).ToList();
+            results.Clear();
+            
+            for (int i = 0; i < count; i++) {
+                int result = conditions[i] ? ifTrue[i] : ifFalse[i];
+                results.Add(result);
+                yield return result;
+            }
+            
+            resultsDirty = false;
+        }
+
     }
 }
