bbbe4e8be73b36fa9adaa56902ab1ed7beeff940
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-29 10:33:29 +0200

    Add support for multiple values in FloatBranchNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Float/FloatBranchNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Float/FloatBranchNode.cs
@@ -1,4 +1,6 @@
-﻿using GeometryGraph.Runtime.Attributes;
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeometryGraph.Runtime.Attributes;
 using JetBrains.Annotations;
 
 
@@ -9,8 +11,38 @@ namespace GeometryGraph.Runtime.Graph {
         [In] public float IfTrue { get; private set; }
         [In] public float IfFalse { get; private set; }
         [Out] public float Result { get; private set; }
+        
+        private readonly List<float> results = new List<float>();
+        private bool resultsDirty = true;
 
         [GetterMethod(nameof(Result), Inline = true), UsedImplicitly]
         private float GetResult() => Condition ? IfTrue : IfFalse;
+        
+        [CalculatesProperty(nameof(Result))]
+        private void MarkResultsDirty() => resultsDirty = true;
+
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if (port != ResultPort || count <= 0) yield break;
+            if (!resultsDirty && results.Count == count) {
+                for (int i = 0; i < count; i++) {
+                    yield return results[i];
+                }
+             
+                yield break;
+            }
+
+            List<bool> conditions = GetValues(ConditionPort, count, Condition).ToList();
+            List<float> ifTrue = GetValues(IfTruePort, count, IfTrue).ToList();
+            List<float> ifFalse = GetValues(IfFalsePort, count, IfFalse).ToList();
+            results.Clear();
+            
+            for (int i = 0; i < count; i++) {
+                float result = conditions[i] ? ifTrue[i] : ifFalse[i];
+                results.Add(result);
+                yield return result;
+            }
+            
+            resultsDirty = false;
+        }
     }
 }
