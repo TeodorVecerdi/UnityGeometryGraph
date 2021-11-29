1f8b0e10661028f97afde24f3f349e44c748c985
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-29 10:23:40 +0200

    Add support for multiple values in ClampFloatNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Float/ClampFloatNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Float/ClampFloatNode.cs
@@ -1,4 +1,6 @@
-﻿using GeometryGraph.Runtime.Attributes;
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeometryGraph.Runtime.Attributes;
 using JetBrains.Annotations;
 using UnityCommons;
 
@@ -9,8 +11,38 @@ namespace GeometryGraph.Runtime.Graph {
         [In] public float Min { get; private set; } = 0.0f;
         [In] public float Max { get; private set; } = 1.0f;
         [Out] public float Result { get; private set; }
+        
+        private readonly List<float> results = new List<float>();
+        private bool resultsDirty = true;
 
         [GetterMethod(nameof(Result), Inline = true), UsedImplicitly]
         private float GetResult() => Input.Clamped(Min, Max);
+        
+        [CalculatesProperty(nameof(Result))]
+        private void MarkResultsDirty() => resultsDirty = true;
+
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if (port != ResultPort || count <= 0) yield break;
+            if (!resultsDirty && results.Count == count) {
+                foreach (float result in results) {
+                    yield return result;
+                }
+                
+                yield break;
+            }
+            
+            List<float> inputs = GetValues(InputPort, count, Input).ToList();
+            List<float> mins = GetValues(MinPort, count, Min).ToList();
+            List<float> maxs = GetValues(MaxPort, count, Max).ToList();
+            results.Clear();
+            
+            for (int i = 0; i < count; i++) {
+                float result = inputs[i].Clamped(mins[i], maxs[i]);
+                results.Add(result);
+                yield return result;
+            }
+            
+            resultsDirty = false;
+        }
     }
 }
