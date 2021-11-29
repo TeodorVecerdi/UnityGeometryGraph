52456c828d4bd2cbe8a88bf4b7a239489f27ab92
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-29 11:42:09 +0200

    Add support for multiple values in VectorBranchNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Vector/VectorBranchNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Vector/VectorBranchNode.cs
@@ -1,4 +1,6 @@
-﻿using GeometryGraph.Runtime.Attributes;
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeometryGraph.Runtime.Attributes;
 using JetBrains.Annotations;
 using Unity.Mathematics;
 
@@ -9,8 +11,36 @@ namespace GeometryGraph.Runtime.Graph {
         [In(GenerateEquality = false)] public float3 IfTrue { get; private set; }
         [In(GenerateEquality = false)] public float3 IfFalse { get; private set; }
         [Out] public float3 Result { get; private set; }
+
+        private readonly List<float3> results = new List<float3>();
+        private bool resultsDirty = true;
         
+        [CalculatesAllProperties] private void MarkResultsDirty() => resultsDirty = true;
+
         [GetterMethod(nameof(Result), Inline = true), UsedImplicitly]
         private float3 GetResult() => Condition ? IfTrue : IfFalse;
+
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if(port != ResultPort || count <= 0) yield break;
+            if (!resultsDirty && results.Count == count) {
+                for (int i = 0; i < count; i++) {
+                    yield return results[i];
+                }
+                
+                yield break;
+            }
+            
+            List<bool> conditions = GetValues(ConditionPort, count, Condition).ToList();
+            List<float3> ifTrue = GetValues(IfTruePort, count, IfTrue).ToList();
+            List<float3> ifFalse = GetValues(IfFalsePort, count, IfFalse).ToList();
+            results.Clear();
+            resultsDirty = false;
+            
+            for (int i = 0; i < count; i++) {
+                float3 result = conditions[i] ? ifTrue[i] : ifFalse[i];
+                results.Add(result);
+                yield return result;
+            }
+        }
     }
 }
