d0c15ecda9a8d5ee29a2ff02ed9fe8385d9caa37
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-29 11:12:26 +0200

    Add support for multiple values in ClampIntegerNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/ClampIntegerNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/ClampIntegerNode.cs
@@ -1,4 +1,6 @@
-﻿using GeometryGraph.Runtime.Attributes;
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeometryGraph.Runtime.Attributes;
 using JetBrains.Annotations;
 using UnityCommons;
 
@@ -9,8 +11,37 @@ namespace GeometryGraph.Runtime.Graph {
         [In] public int Min {get; private set; } = 0;
         [In] public int Max {get; private set; } = 1;
         [Out] public int Result {get; private set; }
+        
+        private readonly List<int> results = new List<int>();
+        private bool resultsDirty = true;
 
         [GetterMethod(nameof(Result), Inline = true), UsedImplicitly]
         private int GetResult() => Input.Clamped(Min, Max);
+        
+        [CalculatesAllProperties] private void MarkResultsDirty() => resultsDirty = true;
+
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if (port != ResultPort || count <= 0) yield break;
+            if (!resultsDirty && results.Count == count) {
+                for (int i = 0; i < count; i++) {
+                    yield return results[i];
+                }
+                
+                yield break;
+            }
+
+            List<int> inputs = GetValues(InputPort, count, Input).ToList();
+            List<int> mins = GetValues(MinPort, count, Min).ToList();
+            List<int> maxs = GetValues(MaxPort, count, Max).ToList();
+            results.Clear();
+            
+            for (int i = 0; i < count; i++) {
+                int result = inputs[i].Clamped(mins[i], maxs[i]);
+                results.Add(result);
+                yield return result;
+            }
+            
+            resultsDirty = false;
+        }
     }
 }
