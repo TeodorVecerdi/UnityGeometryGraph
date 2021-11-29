94955c20f4d3d91d50b6213ae95d1b72b160cd44
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-29 11:28:22 +0200

    Add support for multiple values in MapRangeIntegerNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/MapRangeIntegerNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/MapRangeIntegerNode.cs
@@ -1,4 +1,6 @@
-﻿using GeometryGraph.Runtime.Attributes;
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeometryGraph.Runtime.Attributes;
 using UnityCommons;
 
 namespace GeometryGraph.Runtime.Graph {
@@ -11,11 +13,47 @@ namespace GeometryGraph.Runtime.Graph {
         [In] public int ToMin { get; private set; } = 0;
         [In] public int ToMax { get; private set; } = 1;
         [Out] public int Result { get; private set; }
+        
+        private readonly List<int> results = new List<int>();
+        private bool resultsDirty = true;
+        
+        [CalculatesAllProperties] private void MarkResultsDirty() => resultsDirty = true;
 
         [GetterMethod(nameof(Result), Inline = true)]
         private int GetResult() {
-            int value = Value.Map(FromMin, FromMax, ToMin, ToMax);
-            return Clamp ? value.Clamped(ToMin, ToMax) : value;
+            return CalculateResult(Value, FromMin, FromMax, ToMin, ToMax, Clamp);
+        }
+        
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if (port != ResultPort || count <= 0) yield break;
+            if (!resultsDirty && results.Count == count) {
+                for (int i = 0; i < count; i++) {
+                    yield return results[i];
+                }
+                
+                yield break;
+            }
+
+            List<int> values = GetValues(ValuePort, count, Value).ToList();
+            List<int> fromMin = GetValues(FromMinPort, count, FromMin).ToList();
+            List<int> fromMax = GetValues(FromMaxPort, count, FromMax).ToList();
+            List<int> toMin = GetValues(ToMinPort, count, ToMin).ToList();
+            List<int> toMax = GetValues(ToMaxPort, count, ToMax).ToList();
+            results.Clear();
+
+            for (int i = 0; i < count; i++) {
+                int result = CalculateResult(values[i], fromMin[i], fromMax[i], toMin[i], toMax[i], Clamp);
+                results.Add(result);
+                yield return result;
+            }
+            
+            resultsDirty = false;
+        }
+
+        
+        private int CalculateResult(int value, int fromMin, int fromMax, int toMin, int toMax, bool clamp) {
+            int result = value.Map(fromMin, fromMax, toMin, toMax);
+            return clamp ? result.Clamped(toMin, toMax) : result;
         }
     }
 }
