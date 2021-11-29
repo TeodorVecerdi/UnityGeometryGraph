8e320d9e83f8a82e13d94f821ee5b6f9b73bd162
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-29 11:56:12 +0200

    Add support for multiple values in BooleanMathNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/BooleanMathNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/BooleanMathNode.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using GeometryGraph.Runtime.Attributes;
 using JetBrains.Annotations;
 
@@ -9,14 +11,43 @@ namespace GeometryGraph.Runtime.Graph {
         [In] public bool B { get; private set; }
         [Setting] public BooleanMathNode_Operation Operation { get; private set; }
         [Out] public bool Result { get; private set; }
+        
+        private readonly List<bool> results = new List<bool>();
+        private bool resultsDirty = true;
+        
+        [CalculatesAllProperties] private void MarkResultsDirty() => resultsDirty = true;
 
-        [GetterMethod(nameof(Result), Inline = true), UsedImplicitly]
+        [GetterMethod(nameof(Result), Inline = true)]
         private bool GetResult() {
+            return CalculateResult(A, B);
+        }
+
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if (port != ResultPort || count <= 0) yield break;
+            if (!resultsDirty && results.Count == count) {
+                for (int i = 0; i < count; i++) {
+                    yield return results[i];
+                }
+                yield break;
+            }
+            
+            resultsDirty = false;
+            results.Clear();
+            List<bool> a = GetValues(APort, count, A).ToList();
+            List<bool> b = GetValues(BPort, count, B).ToList();
+            for (int i = 0; i < count; i++) {
+                bool result = CalculateResult(a[i], b[i]);
+                results.Add(result);
+                yield return result;
+            }
+        }
+
+        private bool CalculateResult(bool a, bool b) {
             return Operation switch {
-                BooleanMathNode_Operation.AND => A && B,
-                BooleanMathNode_Operation.OR => A || B,
-                BooleanMathNode_Operation.XOR => A ^ B,
-                BooleanMathNode_Operation.NOT => !A,
+                BooleanMathNode_Operation.AND => a && b,
+                BooleanMathNode_Operation.OR => a || b,
+                BooleanMathNode_Operation.XOR => a ^ b,
+                BooleanMathNode_Operation.NOT => !a,
                 _ => throw new ArgumentOutOfRangeException(nameof(Operation), Operation, null)
             };
         }
