9824ab1dec47decb180c187a87df99474650b5f5
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-29 11:39:37 +0200

    Add support for multiple values in SplitVectorNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Vector/SplitVectorNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Vector/SplitVectorNode.cs
@@ -1,4 +1,5 @@
-﻿using GeometryGraph.Runtime.Attributes;
+﻿using System.Collections.Generic;
+using GeometryGraph.Runtime.Attributes;
 using JetBrains.Annotations;
 using Unity.Mathematics;
 
@@ -9,9 +10,29 @@ namespace GeometryGraph.Runtime.Graph {
         [Out] public float X { get; private set; }
         [Out] public float Y { get; private set; }
         [Out] public float Z { get; private set; }
-        
+
+        private readonly List<float3> vectors = new List<float3>();
+        private bool vectorsDirty = true;
+
+        [CalculatesAllProperties] private void MarkVectorsDirty() => vectorsDirty = true;
+
         [GetterMethod(nameof(X), Inline = true), UsedImplicitly] private float GetX() => Vector.x;
         [GetterMethod(nameof(Y), Inline = true), UsedImplicitly] private float GetY() => Vector.y;
         [GetterMethod(nameof(Z), Inline = true), UsedImplicitly] private float GetZ() => Vector.z;
+
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if (count <= 0) yield break;
+            if (vectorsDirty || vectors.Count != count) {
+                vectors.Clear();
+                vectors.AddRange(GetValues(VectorPort, count, Vector));
+                vectorsDirty = false;
+            }
+
+            for (int i = 0; i < count; i++) {
+                if (port == XPort) yield return vectors[i].x;
+                else if (port == YPort) yield return vectors[i].y;
+                else if (port == ZPort) yield return vectors[i].z;
+            }
+        }
     }
 }
