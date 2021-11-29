00f04763514eb0b31de27dab2455425d7b51aaba
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-29 11:20:21 +0200

    Add support for multiple values in IntegerCurveMapNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/IntegerCurveMapNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Integer/IntegerCurveMapNode.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using GeometryGraph.Runtime.Attributes;
 using UnityCommons;
 using UnityEngine;
@@ -17,22 +18,48 @@ namespace GeometryGraph.Runtime.Graph {
         public AnimationCurve Curve { get; private set; } = (AnimationCurve)UnityEngine.AnimationCurve.Linear(0.0f, 0.0f, 1.0f, 1.0f);
 
         [Out] public int Result { get; private set; }
+        
+        
+        private readonly List<int> results = new List<int>();
+        private bool resultsDirty = true;
+
+        [CalculatesAllProperties] private void MarkResultsDirty() => resultsDirty = true;
 
         [CalculatesProperty(nameof(Result))]
         private void Calculate() {
-            Result = Mathf.FloorToInt(
+            Result = Calculate(Value, Min, Max);
+        }
+
+        private int Calculate(int value, int min, int max) {
+            return Mathf.FloorToInt(
                 Curve.Evaluate(
-                    ((float)Value).Map(Min, Max, 0.0f, 1.0f)
-                ).Map(0.0f, 1.0f, Min, Max)
+                    ((float)value).Map(min, max, 0.0f, 1.0f)
+                ).Map(0.0f, 1.0f, min, max)
             );
         }
 
         public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
-            if (port != ResultPort && count <= 0) yield break;
-            IEnumerable<int> values = GetValues(ValuePort, count, Value);
-            foreach (int value in values) {
-                yield return Curve.Evaluate(((float)value).Map(Min, Max, 0.0f, 1.0f));
+            if (port != ResultPort || count <= 0) yield break;
+            if (!resultsDirty && results.Count == count) {
+                for (int i = 0; i < count; i++) {
+                    yield return results[i];
+                }
+                
+                yield break;
             }
+
+            List<int> values = GetValues(ValuePort, count, Value).ToList();
+            List<int> mins = GetValues(MinPort, count, Min).ToList();
+            List<int> maxs = GetValues(MaxPort, count, Max).ToList();
+            results.Clear();
+            
+            for (int i = 0; i < count; i++) {
+                int result = Calculate(values[i], mins[i], maxs[i]);
+                results.Add(result);
+                yield return result;
+            }
+            
+            resultsDirty = false;
         }
     }
 }
