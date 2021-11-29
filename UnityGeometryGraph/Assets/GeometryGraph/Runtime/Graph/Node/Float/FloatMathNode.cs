867546892dbdc60adad6d5b28125cd5caaef2e5c
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-29 11:00:21 +0200

    Add support for multiple values in FloatMathNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Float/FloatMathNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Float/FloatMathNode.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using GeometryGraph.Runtime.Attributes;
 using JetBrains.Annotations;
 using Unity.Mathematics;
@@ -12,49 +14,84 @@ namespace GeometryGraph.Runtime.Graph {
         [In] public float Extra { get; private set; }
         [Setting] public FloatMathNode_Operation Operation { get; private set; }
         [Out] public float Result { get; private set; }
+        
+        private readonly List<float> results = new List<float>();
+        private bool resultsDirty = true;
 
         [GetterMethod(nameof(Result), Inline = true), UsedImplicitly]
         private float GetResult() {
+            return CalculateResult(X, Y, Tolerance, Extra);
+        }
+
+        [CalculatesAllProperties]
+        private void MarkResultsDirty() => resultsDirty = true;
+
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if (port != ResultPort || count <= 0) yield break;
+            if (!resultsDirty && results.Count == count) {
+                for (int i = 0; i < count; i++) {
+                    yield return results[i];
+                }
+                
+                yield break;
+            }
+
+            List<float> xs = GetValues(XPort, count, X).ToList();
+            List<float> ys = GetValues(YPort, count, Y).ToList();
+            List<float> tolerances = GetValues(TolerancePort, count, Tolerance).ToList();
+            List<float> extras = GetValues(ExtraPort, count, Extra).ToList();
+            results.Clear();
+            
+            for (int i = 0; i < count; i++) {
+                float result = CalculateResult(xs[i], ys[i], tolerances[i], extras[i]);
+                results.Add(result);
+                yield return result;
+            }
+            
+            resultsDirty = false;
+        }
+
+        private float CalculateResult(float x, float y, float tolerance, float extra) {
             return Operation switch {
-                FloatMathNode_Operation.Add => X + Y,
-                FloatMathNode_Operation.Subtract => X - Y,
-                FloatMathNode_Operation.Multiply => X * Y,
-                FloatMathNode_Operation.Divide => X / Y,
-                FloatMathNode_Operation.Power => math.pow(X, Y),
-                FloatMathNode_Operation.Logarithm => MathF.Log(X, Y),
-                FloatMathNode_Operation.SquareRoot => math.sqrt(X),
-                FloatMathNode_Operation.InverseSquareRoot => math.rsqrt(X),
-                FloatMathNode_Operation.Absolute => math.abs(X),
-                FloatMathNode_Operation.Exponent => math.exp(X),
+                FloatMathNode_Operation.Add => x + y,
+                FloatMathNode_Operation.Subtract => x - y,
+                FloatMathNode_Operation.Multiply => x * y,
+                FloatMathNode_Operation.Divide => x / y,
+                FloatMathNode_Operation.Power => math.pow(x, y),
+                FloatMathNode_Operation.Logarithm => MathF.Log(x, y),
+                FloatMathNode_Operation.SquareRoot => math.sqrt(x),
+                FloatMathNode_Operation.InverseSquareRoot => math.rsqrt(x),
+                FloatMathNode_Operation.Absolute => math.abs(x),
+                FloatMathNode_Operation.Exponent => math.exp(x),
 
-                FloatMathNode_Operation.Minimum => math.min(X, Y),
-                FloatMathNode_Operation.Maximum => math.max(X, Y),
-                FloatMathNode_Operation.LessThan => X < Y ? 1.0f : 0.0f,
-                FloatMathNode_Operation.GreaterThan => X > Y ? 1.0f : 0.0f,
-                FloatMathNode_Operation.Sign => X < 0 ? -1.0f : X == 0.0f ? 0.0f : 1.0f,
-                FloatMathNode_Operation.Compare => math.abs(X - Y) < Tolerance ? 1.0f : 0.0f,
-                FloatMathNode_Operation.SmoothMinimum => math_ext.smooth_min(X, Y, Tolerance),
-                FloatMathNode_Operation.SmoothMaximum => math_ext.smooth_max(X, Y, Tolerance),
+                FloatMathNode_Operation.Minimum => math.min(x, y),
+                FloatMathNode_Operation.Maximum => math.max(x, y),
+                FloatMathNode_Operation.LessThan => x < y ? 1.0f : 0.0f,
+                FloatMathNode_Operation.GreaterThan => x > y ? 1.0f : 0.0f,
+                FloatMathNode_Operation.Sign => x < 0 ? -1.0f : x == 0.0f ? 0.0f : 1.0f,
+                FloatMathNode_Operation.Compare => math.abs(x - y) < tolerance ? 1.0f : 0.0f,
+                FloatMathNode_Operation.SmoothMinimum => math_ext.smooth_min(x, y, tolerance),
+                FloatMathNode_Operation.SmoothMaximum => math_ext.smooth_max(x, y, tolerance),
 
-                FloatMathNode_Operation.Round => math.round(X),
-                FloatMathNode_Operation.Floor => math.floor(X),
-                FloatMathNode_Operation.Ceil => math.ceil(X),
-                FloatMathNode_Operation.Truncate => (int)X,
-                FloatMathNode_Operation.Fraction => X - (int)X,
-                FloatMathNode_Operation.Modulo => MathF.IEEERemainder(X, Y),
-                FloatMathNode_Operation.Wrap => X = math_ext.wrap(X, Y, Extra),
-                FloatMathNode_Operation.Snap => math.round(X / Y) * Y,
+                FloatMathNode_Operation.Round => math.round(x),
+                FloatMathNode_Operation.Floor => math.floor(x),
+                FloatMathNode_Operation.Ceil => math.ceil(x),
+                FloatMathNode_Operation.Truncate => (int)x,
+                FloatMathNode_Operation.Fraction => x - (int)x,
+                FloatMathNode_Operation.Modulo => MathF.IEEERemainder(x, y),
+                FloatMathNode_Operation.Wrap => math_ext.wrap(x, y, extra),
+                FloatMathNode_Operation.Snap => math.round(x / y) * y,
 
-                FloatMathNode_Operation.Sine => math.sin(X),
-                FloatMathNode_Operation.Cosine => math.cos(X),
-                FloatMathNode_Operation.Tangent => math.tan(X),
-                FloatMathNode_Operation.Arcsine => math.asin(X),
-                FloatMathNode_Operation.Arccosine => math.acos(X),
-                FloatMathNode_Operation.Arctangent => math.atan(X),
-                FloatMathNode_Operation.Atan2 => math.atan2(X, Y),
-                FloatMathNode_Operation.ToRadians => math.radians(X),
-                FloatMathNode_Operation.ToDegrees => math.degrees(X),
-                FloatMathNode_Operation.Lerp => math.lerp(X, Y, Extra),
+                FloatMathNode_Operation.Sine => math.sin(x),
+                FloatMathNode_Operation.Cosine => math.cos(x),
+                FloatMathNode_Operation.Tangent => math.tan(x),
+                FloatMathNode_Operation.Arcsine => math.asin(x),
+                FloatMathNode_Operation.Arccosine => math.acos(x),
+                FloatMathNode_Operation.Arctangent => math.atan(x),
+                FloatMathNode_Operation.Atan2 => math.atan2(x, y),
+                FloatMathNode_Operation.ToRadians => math.radians(x),
+                FloatMathNode_Operation.ToDegrees => math.degrees(x),
+                FloatMathNode_Operation.Lerp => math.lerp(x, y, extra),
                 _ => throw new ArgumentOutOfRangeException()
             };
         }
