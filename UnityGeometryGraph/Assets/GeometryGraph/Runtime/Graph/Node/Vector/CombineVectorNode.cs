1bdc1c50e307649a1f5319bc0b39185a1db0f0b3
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-11-29 11:36:39 +0200

    Add support for multiple values in CombineVectorNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Vector/CombineVectorNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Vector/CombineVectorNode.cs
@@ -1,4 +1,6 @@
-﻿using GeometryGraph.Runtime.Attributes;
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeometryGraph.Runtime.Attributes;
 using Unity.Mathematics;
 
 namespace GeometryGraph.Runtime.Graph {
@@ -9,9 +11,38 @@ namespace GeometryGraph.Runtime.Graph {
         [In] public float Z { get; private set; }
         [Out] public float3 Vector { get; private set; }
 
+        private readonly List<float3> results = new List<float3>();
+        private bool resultsDirty = true;
+        
+        [CalculatesAllProperties] private void MarkResultsDirty() => resultsDirty = true;
+
         [CalculatesProperty(nameof(Vector))]
         private void CalculateVector() {
             Vector = new float3(X, Y, Z);
+        }
+
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if (port != VectorPort || count <= 0) yield break;
+            if (!resultsDirty && results.Count == count) {
+                for (int i = 0; i < count; i++) {
+                    yield return results[i];
+                }
+                
+                yield break;
+            }
+
+            List<float> xs = GetValues(XPort, count, X).ToList();
+            List<float> ys = GetValues(YPort, count, Y).ToList();
+            List<float> zs = GetValues(ZPort, count, Z).ToList();
+            results.Clear();
+            
+            for (int i = 0; i < count; i++) {
+                float3 result = new float3(xs[i], ys[i], zs[i]);
+                results.Add(result);
+                yield return result;
+            }
+            
+            resultsDirty = false;
         }
     }
 }
