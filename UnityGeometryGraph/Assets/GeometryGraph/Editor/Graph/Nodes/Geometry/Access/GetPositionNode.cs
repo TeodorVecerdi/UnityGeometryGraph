6ca63b9d692c23bb49e02e0c1ee6b5c4865658d5
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-12-01 15:46:50 +0200

    Add GetPosition node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Geometry/Access/GetPositionNode.cs
@@ -0,0 +1,26 @@
+﻿using GeometryGraph.Runtime.Graph;
+using UnityEditor.Experimental.GraphView;
+
+namespace GeometryGraph.Editor.Access {
+    [Title("Geometry", "Get Position")]
+    public class GetPositionNode : AbstractNode<GeometryGraph.Runtime.Graph.GetPositionNode> {
+        private GraphFrameworkPort geometryPort;
+        private GraphFrameworkPort positionPort;
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Get Position", NodeCategory.Geometry);
+            
+            geometryPort = GraphFrameworkPort.Create("Geometry", Direction.Input, Port.Capacity.Single, PortType.Geometry, this);
+            positionPort = GraphFrameworkPort.Create("Position", Direction.Output, Port.Capacity.Multi, PortType.Vector, this);
+            
+            AddPort(geometryPort);
+            AddPort(positionPort);
+        }
+
+        public override void BindPorts() {
+            BindPort(geometryPort, RuntimeNode.GeometryPort);
+            BindPort(positionPort, RuntimeNode.PositionPort);
+        }
+    }
+}
