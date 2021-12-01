71d36d9bdf67d768e95712233c29af3386620a3f
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-12-01 10:13:15 +0200

    Add MeshPool class

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Data/Pools/MeshPool.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Object = UnityEngine.Object;
+
+namespace GeometryGraph.Runtime.Data {
+    [Serializable]
+    public class MeshPool : ObjectPool<Mesh> {
+        [SerializeField] private IndexFormat meshIndexFormat;
+        
+        public MeshPool(IndexFormat meshIndexFormat, int initialPoolSize = 4, float growthFactor = 2.0f) : base(initialPoolSize, growthFactor) {
+            this.meshIndexFormat = meshIndexFormat;
+        }
+        
+        protected override Mesh CreatePooled() {
+            return new Mesh {
+                name = $"PooledMesh_{Guid.NewGuid():N}",
+                indexFormat = meshIndexFormat
+            };
+        }
+
+        protected override void OnReturn(Mesh pooled) {
+            pooled.Clear();
+        }
+
+        protected override void DestroyPooled(Mesh obj) {
+            if(Application.isPlaying) {
+                Object.Destroy(obj);
+            } else {
+                Object.DestroyImmediate(obj);
+            }
+        }
+    }
+}
