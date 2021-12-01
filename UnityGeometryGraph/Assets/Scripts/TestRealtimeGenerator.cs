44c470fc6b98d589798992d490b61413c713ccb6
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-12-01 15:49:14 +0200

    Add temporary script for evaluating in realtime in the editor

---
+++ UnityGeometryGraph/Assets/Scripts/TestRealtimeGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GeometryGraph.Runtime.Curve.TEMP {
+    [ExecuteAlways]
+    public class TestRealtimeGenerator : MonoBehaviour {
+        [Required] public GeometryGraph GeometryGraph;
+        public bool Enabled;
+
+        private void Update() {
+            if (!Enabled || GeometryGraph == null) return;
+            GeometryGraph.Evaluate();
+        }
+    }
+}
