041fc1b3490b5c91998df7eae80e087aebe05a90
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-12-14 09:26:11 +0200

    Add Grid Node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Geometry/Operations/GeometryGrid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
+
+namespace GeometryGraph.Runtime.Geometry {
+    public static class GeometryGrid {
+        public static GeometryData Make(float2 size, int pointsX, int pointsY) {
+            float2 cellSize = size / new float2(pointsX, pointsY);
+            List<float3> position = new();
+            for (int x = 0; x < pointsX; x++) {
+                for (int y = 0; y < pointsY; y++) {
+                    position.Add(new float3(x * cellSize.x, 0, y * cellSize.y));
+                }
+            }
+
+            return new GeometryData(
+                    Enumerable.Empty<GeometryData.Edge>(), Enumerable.Empty<GeometryData.Face>(), Enumerable.Empty<GeometryData.FaceCorner>(),
+                    1, position, Enumerable.Empty<float3>(), Enumerable.Empty<int>(),
+                    Enumerable.Empty<bool>(), Enumerable.Empty<float>(), Enumerable.Empty<float2>())
+                ;
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/WIP/GridNode.cs
@@ -0,0 +1,25 @@
+﻿using GeometryGraph.Runtime.Attributes;
+using GeometryGraph.Runtime.Geometry;
+using Unity.Mathematics;
+
+namespace GeometryGraph.Runtime.Graph {
+    [GenerateRuntimeNode]
+    public partial class GridNode {
+        [In] public float Width { get; private set; } = 1.0f;
+        [In] public float Height { get; private set; } = 1.0f;
+        [In] public int PointsX { get; private set; } = 4;
+        [In] public int PointsY { get; private set; } = 4;
+        [Out] public GeometryData Result { get; private set; }
+        
+        [GetterMethod(nameof(Result), Inline = true)]
+        private GeometryData GetResult() {
+            if (Result == null) CalculateResult();
+            return Result;
+        }
+        
+        [CalculatesProperty(nameof(Result))]
+        private void CalculateResult() {
+            Result = GeometryGrid.Make(new float2(Width, Height), PointsX, PointsY);
+        }
+    }
+}
