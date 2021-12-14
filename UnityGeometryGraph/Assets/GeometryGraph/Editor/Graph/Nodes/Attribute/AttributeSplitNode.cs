4358f64cd19cd5df866bc528a9f6ba0701a2e6d6
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-12-14 11:42:46 +0200

    Add AttributeSplit node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Attribute/AttributeSplitNode.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using GeometryGraph.Runtime;
+using GeometryGraph.Runtime.AttributeSystem;
+using GeometryGraph.Runtime.Graph;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using InputType = GeometryGraph.Runtime.Graph.AttributeSplitNode.AttributeSplitNode_InputType;
+
+namespace GeometryGraph.Editor {
+    [Title("Attribute", "Attribute Split")]
+    public class AttributeSplitNode : AbstractNode<GeometryGraph.Runtime.Graph.AttributeSplitNode> {
+        private GraphFrameworkPort geometryPort;
+        private GraphFrameworkPort vectorPort;
+        private GraphFrameworkPort attributePort;
+        private GraphFrameworkPort xResultPort;
+        private GraphFrameworkPort yResultPort;
+        private GraphFrameworkPort zResultPort;
+        private GraphFrameworkPort resultPort;
+        
+        private EnumSelectionDropdown<AttributeDomain> domainDropdown;
+        private EnumSelectionDropdown<InputType> typeDropdown;
+        private Vector3Field vectorField;
+        private TextField attributeField;
+        private TextField xResultField;
+        private TextField yResultField;
+        private TextField zResultField;
+
+        private float3 vector;
+        private string attribute;
+        private string xResult;
+        private string yResult;
+        private string zResult;
+        private AttributeDomain domain = AttributeDomain.Vertex;
+        private InputType type = InputType.Attribute;
+        
+        private static readonly SelectionTree typeTree = new (new List<object>(Enum.GetValues(typeof(InputType)).Convert(o => o))) {
+            new SelectionCategory("Type", false) {
+                new ("Vector", 0, false),
+                new ("Attribute", 1, false)
+            }
+        };
+        
+        private static readonly SelectionTree domainTree = new (new List<object>(Enum.GetValues(typeof(AttributeDomain)).Convert(o => o))) {
+            new SelectionCategory("Domain", false) {
+                new ("Vertex", 0, false),
+                new ("Edge", 1, false),
+                new ("Face", 2, false),
+                new ("FaceCorner", 3, false)
+            }
+        };
+
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("Attribute Split", NodeCategory.Attribute);
+            
+            geometryPort = GraphFrameworkPort.Create("Geometry", Direction.Input, Port.Capacity.Single, PortType.Geometry, this);
+            (vectorPort, vectorField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>("Vector", PortType.Vector, this, showLabelOnField: false, onDisconnect: (_, _) => RuntimeNode.UpdateVector(vector));
+            (attributePort, attributeField) = GraphFrameworkPort.CreateWithBackingField<TextField, string>("Vector", PortType.String, this, onDisconnect: (_, _) => RuntimeNode.UpdateAttribute(attribute));
+            (xResultPort, xResultField) = GraphFrameworkPort.CreateWithBackingField<TextField, string>("X Result", PortType.String, this, onDisconnect: (_, _) => RuntimeNode.UpdateXResult(xResult));
+            (yResultPort, yResultField) = GraphFrameworkPort.CreateWithBackingField<TextField, string>("Y Result", PortType.String, this, onDisconnect: (_, _) => RuntimeNode.UpdateYResult(yResult));
+            (zResultPort, zResultField) = GraphFrameworkPort.CreateWithBackingField<TextField, string>("Z Result", PortType.String, this, onDisconnect: (_, _) => RuntimeNode.UpdateZResult(zResult));
+            resultPort = GraphFrameworkPort.Create("Result", Direction.Output, Port.Capacity.Multi, PortType.Geometry, this);
+
+            domainDropdown = new EnumSelectionDropdown<AttributeDomain>(domain, domainTree, "Domain");
+            typeDropdown = new EnumSelectionDropdown<InputType>(type, typeTree, "Type");
+            
+            domainDropdown.RegisterValueChangedCallback(evt => {
+                if (evt.newValue == domain) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change domain");
+                domain = evt.newValue;
+                RuntimeNode.UpdateTargetDomain(domain);
+            });
+            
+            typeDropdown.RegisterValueChangedCallback(evt => {
+                if (evt.newValue == type) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change type");
+                type = evt.newValue;
+                RuntimeNode.UpdateInputType(type);
+                OnTypeChanged();
+            });
+            
+            vectorField.RegisterValueChangedCallback(evt => {
+                if (math.distancesq(vector, evt.newValue) < Constants.FLOAT_TOLERANCE * Constants.FLOAT_TOLERANCE) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change vector");
+                vector = evt.newValue;
+                RuntimeNode.UpdateVector(vector);
+            });
+            
+            attributeField.RegisterValueChangedCallback(evt => {
+                if (attribute == evt.newValue) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change source attribute name");
+                attribute = evt.newValue;
+                RuntimeNode.UpdateAttribute(attribute);
+            });
+            
+            xResultField.RegisterValueChangedCallback(evt => {
+                if (xResult == evt.newValue) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change X result attribute name");
+                xResult = evt.newValue;
+                RuntimeNode.UpdateXResult(xResult);
+            });
+            
+            yResultField.RegisterValueChangedCallback(evt => {
+                if (yResult == evt.newValue) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change Y result attribute name");
+                yResult = evt.newValue;
+                RuntimeNode.UpdateYResult(yResult);
+            });
+            
+            zResultField.RegisterValueChangedCallback(evt => {
+                if (zResult == evt.newValue) return;
+                
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change Z result attribute name");
+                zResult = evt.newValue;
+                RuntimeNode.UpdateZResult(zResult);
+            });
+            
+            attributePort.Add(attributeField);
+            xResultPort.Add(xResultField);
+            yResultPort.Add(yResultField);
+            zResultPort.Add(zResultField);
+            
+            inputContainer.Add(domainDropdown);
+            inputContainer.Add(typeDropdown);
+            
+            AddPort(geometryPort);
+            AddPort(vectorPort);
+            inputContainer.Add(vectorField);
+            AddPort(attributePort);
+            AddPort(xResultPort);
+            AddPort(yResultPort);
+            AddPort(zResultPort);
+            AddPort(resultPort);
+
+            OnTypeChanged();
+        }
+
+        private void OnTypeChanged() {
+            if (type is InputType.Vector) {
+                vectorPort.Show();
+                attributePort.HideAndDisconnect();
+            } else {
+                vectorPort.HideAndDisconnect();
+                attributePort.Show();
+            }
+        }
+        
+        public override void BindPorts() {
+            BindPort(geometryPort, RuntimeNode.GeometryPort);
+            BindPort(vectorPort, RuntimeNode.VectorPort);
+            BindPort(attributePort, RuntimeNode.AttributePort);
+            BindPort(xResultPort, RuntimeNode.XResultPort);
+            BindPort(yResultPort, RuntimeNode.YResultPort);
+            BindPort(zResultPort, RuntimeNode.ZResultPort);
+            BindPort(resultPort, RuntimeNode.ResultPort);
+        }
+
+        public override JObject GetNodeData() {
+            JObject root = base.GetNodeData();
+            JArray array = new() {
+                JsonConvert.SerializeObject(vector, Formatting.None, float3Converter.Converter),
+                attribute,
+                xResult,
+                yResult,
+                zResult,
+                (int)domain,
+                (int)type
+            };
+            root["d"] = array;
+            return root;
+        }
+        
+        public override void SetNodeData(JObject data) {
+            JArray array = data["d"] as JArray;
+            vector = JsonConvert.DeserializeObject<float3>(array!.Value<string>(0), float3Converter.Converter);
+            attribute = array.Value<string>(1);
+            xResult = array.Value<string>(2);
+            yResult = array.Value<string>(3);
+            zResult = array.Value<string>(4);
+            domain = (AttributeDomain)array.Value<int>(5);
+            type = (InputType)array.Value<int>(6);
+            
+            vectorField.SetValueWithoutNotify(vector);
+            attributeField.SetValueWithoutNotify(attribute);
+            xResultField.SetValueWithoutNotify(xResult);
+            yResultField.SetValueWithoutNotify(yResult);
+            zResultField.SetValueWithoutNotify(zResult);
+            domainDropdown.SetValueWithoutNotify(domain);
+            typeDropdown.SetValueWithoutNotify(type);
+            
+            RuntimeNode.UpdateVector(vector);
+            RuntimeNode.UpdateAttribute(attribute);
+            RuntimeNode.UpdateXResult(xResult);
+            RuntimeNode.UpdateYResult(yResult);
+            RuntimeNode.UpdateZResult(zResult);
+            RuntimeNode.UpdateTargetDomain(domain);
+            RuntimeNode.UpdateInputType(type);
+            
+            OnTypeChanged();
+            
+            base.SetNodeData(data);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Attribute/AttributeSplitNode.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GeometryGraph.Runtime.Attributes;
+using GeometryGraph.Runtime.AttributeSystem;
+using GeometryGraph.Runtime.Geometry;
+using Unity.Mathematics;
+
+namespace GeometryGraph.Runtime.Graph {
+    [GenerateRuntimeNode]
+    public partial class AttributeSplitNode {
+        [In] public GeometryData Geometry { get; private set; }
+        [In] public float3 Vector { get; private set; }
+        [In] public string Attribute { get; private set; }
+        [In] public string XResult { get; private set; }
+        [In] public string YResult { get; private set; }
+        [In] public string ZResult { get; private set; }
+        [Out] public GeometryData Result { get; private set; }
+
+        [Setting] public AttributeDomain TargetDomain { get; private set; } = AttributeDomain.Vertex;
+        [Setting] public AttributeSplitNode_InputType InputType { get; private set; } = AttributeSplitNode_InputType.Attribute;
+
+        [CalculatesProperty(nameof(Result))]
+        private void CalculateResult() {
+            if (Geometry == null || string.IsNullOrWhiteSpace(Attribute)) {
+                Result = GeometryData.Empty;
+                return;
+            }
+            Result = Geometry.Clone();
+            
+            int count = TargetDomain switch {
+                AttributeDomain.Vertex => Geometry.Vertices.Count,
+                AttributeDomain.Edge => Geometry.Edges.Count,
+                AttributeDomain.Face => Geometry.Faces.Count,
+                AttributeDomain.FaceCorner => Geometry.FaceCorners.Count,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            // check if at least 2 of the 3 results are not null or whitespace
+            int numResults = 0;
+            if (!string.IsNullOrWhiteSpace(XResult) is var xHasValue && xHasValue) numResults++;
+            if (!string.IsNullOrWhiteSpace(YResult) is var yHasValue && yHasValue) numResults++;
+            if (!string.IsNullOrWhiteSpace(ZResult) is var zHasValue && zHasValue) numResults++;
+            
+            if (numResults == 0) return;
+            
+            if (InputType is AttributeSplitNode_InputType.Vector) {
+                IEnumerable<float3> values = GetValues(VectorPort, count, Vector);
+                switch (numResults) {
+                    case 1: {
+                        if (xHasValue) {
+                            Result.StoreAttribute(new FloatAttribute(XResult, values.Select(v => v.x)), TargetDomain);
+                        } else if (yHasValue) {
+                            Result.StoreAttribute(new FloatAttribute(YResult, values.Select(v => v.y)), TargetDomain);
+                        } else if (zHasValue) {
+                            Result.StoreAttribute(new FloatAttribute(ZResult, values.Select(v => v.z)), TargetDomain);
+                        }
+
+                        break;
+                    }
+                    case > 1: {
+                        List<float3> valuesList = values.ToList();
+                        if (xHasValue) {
+                            Result.StoreAttribute(new FloatAttribute(XResult, valuesList.Select(v => v.x)), TargetDomain);
+                        }
+                        if (yHasValue) {
+                            Result.StoreAttribute(new FloatAttribute(YResult, valuesList.Select(v => v.y)), TargetDomain);
+                        }
+                        if (zHasValue) {
+                            Result.StoreAttribute(new FloatAttribute(ZResult, valuesList.Select(v => v.z)), TargetDomain);
+                        }
+
+                        break;
+                    }
+                }
+            } else {
+                Vector3Attribute attribute = Geometry.GetAttribute<Vector3Attribute>(Attribute, TargetDomain);
+                if (attribute == null) {
+                    if (xHasValue) {
+                        Result.StoreAttribute(new FloatAttribute(XResult, Enumerable.Repeat(0.0f, count)), TargetDomain);
+                    }
+                    if (yHasValue) {
+                        Result.StoreAttribute(new FloatAttribute(YResult, Enumerable.Repeat(0.0f, count)), TargetDomain);
+                    }
+                    if (zHasValue) {
+                        Result.StoreAttribute(new FloatAttribute(ZResult, Enumerable.Repeat(0.0f, count)), TargetDomain);
+                    }
+                } else {
+                    if (xHasValue) {
+                        Result.StoreAttribute(new FloatAttribute(XResult, attribute.Select(v => v.x)), TargetDomain);
+                    }
+                    if (yHasValue) {
+                        Result.StoreAttribute(new FloatAttribute(YResult, attribute.Select(v => v.y)), TargetDomain);
+                    }
+                    if (zHasValue) {
+                        Result.StoreAttribute(new FloatAttribute(ZResult, attribute.Select(v => v.z)), TargetDomain);
+                    }
+                }
+            }
+        }
+        
+        public enum AttributeSplitNode_InputType {
+            Vector,
+            Attribute
+        }
+    }
+}
