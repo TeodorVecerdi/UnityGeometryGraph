98acae7ad4a5e22328a9c8f21320ca2760f4f940
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-12-15 16:39:15 +0200

    Add HashSet containing builtin attributes in AttributeId

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Attribute/Utils/AttributeId.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Attribute/Utils/AttributeId.cs
@@ -1,4 +1,6 @@
-﻿namespace GeometryGraph.Runtime.AttributeSystem {
+﻿using System.Collections.Generic;
+
+namespace GeometryGraph.Runtime.AttributeSystem {
     public static class AttributeId {
         public const string Position = "position";
         public const string Normal = "normal";
@@ -6,5 +8,14 @@
         public const string ShadeSmooth = "shade_smooth";
         public const string Crease = "crease";
         public const string UV = "uv";
+        
+        public static readonly HashSet<string> BuiltinIds = new() {
+            Position,
+            Normal,
+            Material,
+            ShadeSmooth,
+            Crease,
+            UV
+        };
     }
 }
