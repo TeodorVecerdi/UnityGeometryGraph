edeeccce67a475f51d3d3edfc66f3fecbed3955e
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-12-16 13:52:11 +0200

    Add UITester node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/UITesterNode.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace GeometryGraph.Editor {
+    [Title("UI Tester")]
+    public class UITesterNode : AbstractNode<GeometryGraph.Runtime.Graph.UITesterNode> {
+        private float fromMin = 0.0f;
+        private float fromMax = 1.0f;
+        private float toMin = 0.0f;
+        private float toMax = 1.0f;
+
+        private FloatMapRangeField floatMapRangeField;
+        
+        public override void InitializeNode(EdgeConnectorListener edgeConnectorListener) {
+            base.InitializeNode(edgeConnectorListener);
+            Initialize("UI Tester", NodeCategory.None);
+            
+            floatMapRangeField = new FloatMapRangeField("Label", fromMin, fromMax, toMin, toMax);
+            floatMapRangeField.RegisterFromMinValueChanged(evt => {
+                Debug.Log("FromMinValueChanged: " + evt.newValue);
+                fromMin = evt.newValue;
+            });
+            floatMapRangeField.RegisterFromMaxValueChanged(evt => {
+                Debug.Log("FromMaxValueChanged: " + evt.newValue);
+                fromMax = evt.newValue;
+            });
+            floatMapRangeField.RegisterToMinValueChanged(evt => {
+                Debug.Log("ToMinValueChanged: " + evt.newValue);
+                toMin = evt.newValue;
+            });
+            floatMapRangeField.RegisterToMaxValueChanged(evt => {
+                Debug.Log("ToMaxValueChanged: " + evt.newValue);
+                toMax = evt.newValue;
+            });
+            
+            inputContainer.Add(floatMapRangeField);
+        }
+
+        public override void BindPorts() {}
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/UITesterNode.cs
@@ -0,0 +1,7 @@
+﻿using GeometryGraph.Runtime.Attributes;
+
+namespace GeometryGraph.Runtime.Graph {
+    [GenerateRuntimeNode]
+    public partial class UITesterNode {
+    }
+}
