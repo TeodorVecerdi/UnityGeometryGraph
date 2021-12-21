f63ed4d24e029697dd5d36ce2b06fec0b35e43cd
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-12-21 10:29:33 +0200

    Add editor implementation for StringValue node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Input/StringValueNode.cs
@@ -0,0 +1,48 @@
+﻿using GeometryGraph.Runtime.Graph;
+using Newtonsoft.Json.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
+namespace GeometryGraph.Editor {
+    [Title("Input", "String Value")]
+    public class StringValueNode : AbstractNode<GeometryGraph.Runtime.Graph.StringValueNode> {
+        protected override string Title => "String Value";
+        protected override NodeCategory Category => NodeCategory.Input;
+
+        private string value;
+        private TextField valueField;
+        private GraphFrameworkPort valuePort;
+
+        protected override void CreateNode() {
+            valuePort = GraphFrameworkPort.Create("Value", Direction.Output, Port.Capacity.Multi, PortType.String, this);
+            valueField = new TextField("Value");
+            valueField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Changed float value");
+                value = evt.newValue;
+                RuntimeNode.UpdateValue(value);
+            });
+            AddPort(valuePort);
+            extensionContainer.Add(valueField);
+            
+            Refresh();
+        }
+        
+        protected override void BindPorts() {
+            BindPort(valuePort, RuntimeNode.ResultPort);
+        }
+
+        protected internal override JObject Serialize() {
+            JObject root =  base.Serialize();
+            root["v"] = value;
+            return root;
+        }
+
+        protected internal override void Deserialize(JObject data) {
+            value = data.Value<string>("v");
+            valueField.SetValueWithoutNotify(value);
+            RuntimeNode.UpdateValue(value);
+            
+            base.Deserialize(data);
+        }
+    }
+}
