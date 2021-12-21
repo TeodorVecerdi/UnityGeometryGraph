aad1007bdb18098473e51fb92c99e447d79edbdd
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-12-21 10:24:33 +0200

    Add runtime implementation for StringValue node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Input/StringValueNode.cs
@@ -0,0 +1,10 @@
+﻿using GeometryGraph.Runtime.Attributes;
+
+namespace GeometryGraph.Runtime.Graph {
+    [GenerateRuntimeNode]
+    public partial class StringValueNode {
+        [Setting] public string Value { get; private set; }
+        [Out] public string Result { get; private set; }
+        [GetterMethod(nameof(Result), Inline = true)] private string GetResult() => Value;
+    }
+}
