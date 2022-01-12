c01f86a5914437883e77ec772a13be3fe1161307
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-01-12 13:49:09 +0200

    Add `CurveTypeUtilities` class containing `MinPoints(CurveType)`

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Curve/CurveType.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Curve/CurveType.cs
@@ -1,4 +1,6 @@
-﻿namespace GeometryGraph.Runtime.Curve {
+﻿using System;
+
+namespace GeometryGraph.Runtime.Curve {
     public enum CurveType {
         None = 0,
         Line,
@@ -7,5 +9,20 @@
         CubicBezier,
         Helix,
         Unknown,
+    }
+
+    public static class CurveTypeUtilities {
+        public static int MinPoints(CurveType curveType) {
+            return curveType switch {
+                CurveType.None => 0,
+                CurveType.Line => 2,
+                CurveType.Circle => 3,
+                CurveType.QuadraticBezier => 2,
+                CurveType.CubicBezier => 2,
+                CurveType.Helix => 2,
+                CurveType.Unknown => 0,
+                _ => throw new ArgumentOutOfRangeException(nameof(curveType), curveType, null)
+            };
+        }
     }
 }
