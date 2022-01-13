52949f1958cb938a22c8b934070c9ddd9e68a0c7
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-01-13 14:18:43 +0200

    Add EnumerateCurve node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Curve/EnumerateCurveNode.cs
@@ -0,0 +1,46 @@
+﻿using GeometryGraph.Runtime.Graph;
+using UnityEditor.Experimental.GraphView;
+
+namespace GeometryGraph.Editor {
+    [Title("Curve", "Enumerate Curve")]
+    public class EnumerateCurveNode : AbstractNode<GeometryGraph.Runtime.Graph.EnumerateCurveNode> {
+        protected override string Title => "Enumerate Curve";
+        protected override NodeCategory Category => NodeCategory.Curve;
+
+        private GraphFrameworkPort curvePort;
+        private GraphFrameworkPort countPort;
+        private GraphFrameworkPort indexPort;
+        private GraphFrameworkPort positionPort;
+        private GraphFrameworkPort tangentPort;
+        private GraphFrameworkPort normalPort;
+        private GraphFrameworkPort binormalPort;
+
+        protected override void CreateNode() {
+            curvePort = GraphFrameworkPort.Create("Curve", Direction.Input, Port.Capacity.Single, PortType.Curve, this);
+            countPort = GraphFrameworkPort.Create("Count", Direction.Output, Port.Capacity.Single, PortType.Integer, this);
+            indexPort = GraphFrameworkPort.Create("Index", Direction.Output, Port.Capacity.Multi, PortType.Integer, this);
+            positionPort = GraphFrameworkPort.Create("Position", Direction.Output, Port.Capacity.Multi, PortType.Vector, this);
+            tangentPort = GraphFrameworkPort.Create("Tangent", Direction.Output, Port.Capacity.Multi, PortType.Vector, this);
+            normalPort = GraphFrameworkPort.Create("Normal", Direction.Output, Port.Capacity.Multi, PortType.Vector, this);
+            binormalPort = GraphFrameworkPort.Create("Binormal", Direction.Output, Port.Capacity.Multi, PortType.Vector, this);
+
+            AddPort(curvePort);
+            AddPort(countPort);
+            AddPort(indexPort);
+            AddPort(positionPort);
+            AddPort(tangentPort);
+            AddPort(normalPort);
+            AddPort(binormalPort);
+        }
+
+        protected override void BindPorts() {
+            BindPort(curvePort, RuntimeNode.CurvePort);
+            BindPort(countPort, RuntimeNode.CountPort);
+            BindPort(indexPort, RuntimeNode.IndexPort);
+            BindPort(positionPort, RuntimeNode.PositionPort);
+            BindPort(tangentPort, RuntimeNode.TangentPort);
+            BindPort(normalPort, RuntimeNode.NormalPort);
+            BindPort(binormalPort, RuntimeNode.BinormalPort);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Curve/EnumerateCurveNode.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GeometryGraph.Runtime.Attributes;
+using GeometryGraph.Runtime.Curve;
+using Unity.Mathematics;
+
+namespace GeometryGraph.Runtime.Graph {
+    [GenerateRuntimeNode]
+    public partial class EnumerateCurveNode {
+        [In(
+            DefaultValue = "CurveData.Empty",
+            GetValueCode = "{self} = GetValue(connection, {default})",
+            UpdateValueCode = ""
+        )]
+        public CurveData Curve { get; private set; }
+
+        [Out] public int Count { get; private set; }
+        [Out, Getter("return 0;")] public int Index { get; private set; }
+        [Out, Getter("return float3.zero;")] public float3 Position { get; private set; }
+        [Out, Getter("return float3.zero;")] public float3 Tangent { get; private set; }
+        [Out, Getter("return float3.zero;")] public float3 Normal { get; private set; }
+        [Out, Getter("return float3.zero;")] public float3 Binormal { get; private set; }
+
+        private readonly List<int> indexResults = new();
+        private readonly List<float3> positionResults = new();
+        private readonly List<float3> tangentResults = new();
+        private readonly List<float3> normalResults = new();
+        private readonly List<float3> binormalResults = new();
+        private bool indexDirty = true;
+        private bool positionDirty = true;
+        private bool tangentDirty = true;
+        private bool normalDirty = true;
+        private bool binormalDirty = true;
+
+        [CalculatesProperty(nameof(Index))] private void MarkIndexDirty() => indexDirty = true;
+        [CalculatesProperty(nameof(Position))] private void MarkPositionDirty() => positionDirty = true;
+        [CalculatesProperty(nameof(Tangent))] private void MarkTangentDirty() => tangentDirty = true;
+        [CalculatesProperty(nameof(Normal))] private void MarkNormalDirty() => normalDirty = true;
+        [CalculatesProperty(nameof(Binormal))] private void MarkBinormalDirty() => binormalDirty = true;
+
+        [GetterMethod(nameof(Count), Inline = true)]
+        private int GetCount() => Curve?.Points ?? 0;
+
+        public override IEnumerable<object> GetValuesForPort(RuntimePort port, int count) {
+            if (Curve == null || count <= 0) {
+                yield break;
+            }
+
+            if (port == IndexPort) {
+                if (!indexDirty && indexResults.Count == count) {
+                    foreach (int indexResult in indexResults) {
+                        yield return indexResult;
+                    }
+                    yield break;
+                }
+
+                indexDirty = false;
+                indexResults.Clear();
+                indexResults.AddRange(Enumerable.Range(0, Curve.Points));
+
+                int yieldCount = Math.Min(count, indexResults.Count);
+                for (int i = 0; i < yieldCount; i++) {
+                    yield return indexResults[i];
+                }
+
+                if (yieldCount < count) {
+                    for(int i = 0; i < count - yieldCount; i++) {
+                        yield return 0;
+                    }
+                }
+            } else if (port == PositionPort) {
+                if (!positionDirty && positionResults.Count == count) {
+                    foreach (float3 positionResult in positionResults) {
+                        yield return positionResult;
+                    }
+                    yield break;
+                }
+
+                positionDirty = false;
+                positionResults.Clear();
+                positionResults.AddRange(Curve.Position);
+
+                int yieldCount = Math.Min(count, positionResults.Count);
+                for (int i = 0; i < yieldCount; i++) {
+                    yield return positionResults[i];
+                }
+
+                if (yieldCount < count) {
+                    for (int i = 0; i < count - yieldCount; i++) {
+                        yield return float3.zero;
+                    }
+                }
+            } else if (port == TangentPort) {
+                if (!tangentDirty && tangentResults.Count == count) {
+                    foreach (float3 tangentResult in tangentResults) {
+                        yield return tangentResult;
+                    }
+                    yield break;
+                }
+
+                tangentDirty = false;
+                tangentResults.Clear();
+                tangentResults.AddRange(Curve.Tangent);
+
+                int yieldCount = Math.Min(count, tangentResults.Count);
+                for (int i = 0; i < yieldCount; i++) {
+                    yield return tangentResults[i];
+                }
+
+                if (yieldCount < count) {
+                    for (int i = 0; i < count - yieldCount; i++) {
+                        yield return float3.zero;
+                    }
+                }
+            } else if (port == NormalPort) {
+                if (!normalDirty && normalResults.Count == count) {
+                    foreach (float3 normalResult in normalResults) {
+                        yield return normalResult;
+                    }
+                    yield break;
+                }
+
+                normalDirty = false;
+                normalResults.Clear();
+                normalResults.AddRange(Curve.Normal);
+
+                int yieldCount = Math.Min(count, normalResults.Count);
+                for (int i = 0; i < yieldCount; i++) {
+                    yield return normalResults[i];
+                }
+
+                if (yieldCount < count) {
+                    for (int i = 0; i < count - yieldCount; i++) {
+                        yield return float3.zero;
+                    }
+                }
+            } else if (port == BinormalPort) {
+                if (!binormalDirty && binormalResults.Count == count) {
+                    foreach (float3 binormalResult in binormalResults) {
+                        yield return binormalResult;
+                    }
+                    yield break;
+                }
+
+                binormalDirty = false;
+                binormalResults.Clear();
+                binormalResults.AddRange(Curve.Binormal);
+
+                int yieldCount = Math.Min(count, binormalResults.Count);
+                for (int i = 0; i < yieldCount; i++) {
+                    yield return binormalResults[i];
+                }
+
+                if (yieldCount < count) {
+                    for (int i = 0; i < count - yieldCount; i++) {
+                        yield return float3.zero;
+                    }
+                }
+            }
+        }
+    }
+}
