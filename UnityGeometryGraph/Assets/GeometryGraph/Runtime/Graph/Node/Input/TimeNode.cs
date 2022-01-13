daf9661f15d040de73d5af66b5f74755852d3305
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-01-13 13:14:41 +0200

    Prevent Time node evaluation during serialization

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Input/TimeNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Input/TimeNode.cs
@@ -14,34 +14,34 @@ namespace GeometryGraph.Runtime.Graph {
         [Out] public float DeltaTime { get; private set; }
 
         [GetterMethod(nameof(SmoothDeltaTime), Inline = true)]
-        private float GetSmoothDeltaTime() => UTime.smoothDeltaTime;
+        private float GetSmoothDeltaTime() => Utils.IfNotSerializing(() => UTime.smoothDeltaTime, "UTime.smoothDeltaTime", 0.016f);
 
         [GetterMethod(nameof(TimeScale), Inline = true)]
-        private float GetTimeScale() => UTime.timeScale;
+        private float GetTimeScale() => Utils.IfNotSerializing(() => UTime.timeScale, "UTime.timeScale", 1.0f);
 
         [GetterMethod(nameof(RealtimeSinceStartup), Inline = true)]
-        private float GetRealtimeSinceStartup() => UTime.realtimeSinceStartup;
+        private float GetRealtimeSinceStartup() => Utils.IfNotSerializing(() => UTime.realtimeSinceStartup, "UTime.realtimeSinceStartup");
 
         [GetterMethod(nameof(TimeSinceLevelLoad), Inline = true)]
-        private float GetTimeSinceLevelLoad() => UTime.timeSinceLevelLoad;
+        private float GetTimeSinceLevelLoad() => Utils.IfNotSerializing(() => UTime.timeSinceLevelLoad, "UTime.timeSinceLevelLoad");
 
         [GetterMethod(nameof(Time), Inline = true)]
         private float GetTime() {
             return IsFixed switch {
-                true when IsUnscaled => UTime.fixedUnscaledTime,
-                true => UTime.fixedTime,
-                false when IsUnscaled => UTime.unscaledTime,
-                false => UTime.time
+                true when IsUnscaled => Utils.IfNotSerializing(() => UTime.fixedUnscaledTime, "UTime.fixedUnscaledTime"),
+                true => Utils.IfNotSerializing(() => UTime.fixedTime, "UTime.fixedTime"),
+                false when IsUnscaled => Utils.IfNotSerializing(() => UTime.unscaledTime, "UTime.unscaledTime"),
+                false => Utils.IfNotSerializing(() => UTime.time, "UTime.time")
             };
         }
 
         [GetterMethod(nameof(DeltaTime), Inline = true)]
         private float GetDeltaTime() {
             return IsFixed switch {
-                true when IsUnscaled => UTime.fixedUnscaledDeltaTime,
-                true => UTime.fixedDeltaTime,
-                false when IsUnscaled => UTime.unscaledDeltaTime,
-                false => UTime.deltaTime
+                true when IsUnscaled => Utils.IfNotSerializing(() => UTime.fixedUnscaledDeltaTime, "UTime.fixedUnscaledDeltaTime", 0.016f),
+                true => Utils.IfNotSerializing(() => UTime.fixedDeltaTime, "UTime.fixedDeltaTime", 0.016f),
+                false when IsUnscaled => Utils.IfNotSerializing(() => UTime.unscaledDeltaTime, "UTime.unscaledDeltaTime", 0.016f),
+                false => Utils.IfNotSerializing(() => UTime.deltaTime, "UTime.deltaTime", 0.016f)
             };
         }
 
