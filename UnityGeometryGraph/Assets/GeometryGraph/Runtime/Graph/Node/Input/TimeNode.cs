a5cf5a4ea65358bb389d36a6b81b12a8c2af933c
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-01-18 12:16:10 +0200

    Optimize usage of `IfNotSerializing` in TimeNode

--- UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Input/TimeNode.cs
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Input/TimeNode.cs
@@ -4,8 +4,10 @@ using UTime = UnityEngine.Time;
 namespace GeometryGraph.Runtime.Graph {
     [GenerateRuntimeNode]
     public partial class TimeNode {
-        [Setting, UpdatesProperties(nameof(Time), nameof(DeltaTime))] public bool IsFixed { get; private set; }
-        [Setting, UpdatesProperties(nameof(Time), nameof(DeltaTime))] public bool IsUnscaled { get; private set; }
+        [Setting, UpdatesProperties(nameof(Time), nameof(DeltaTime))]
+        public bool IsFixed { get; private set; }
+        [Setting, UpdatesProperties(nameof(Time), nameof(DeltaTime))]
+        public bool IsUnscaled { get; private set; }
         [Out] public float SmoothDeltaTime { get; private set; }
         [Out] public float TimeScale { get; private set; }
         [Out] public float RealtimeSinceStartup { get; private set; }
@@ -14,35 +16,37 @@ namespace GeometryGraph.Runtime.Graph {
         [Out] public float DeltaTime { get; private set; }
 
         [GetterMethod(nameof(SmoothDeltaTime), Inline = true)]
-        private float GetSmoothDeltaTime() => Utils.IfNotSerializing(() => UTime.smoothDeltaTime, "UTime.smoothDeltaTime", 0.016f);
+        private float GetSmoothDeltaTime() => Utils.IfNotSerializing(() => UTime.smoothDeltaTime, "UnityEngine.Time.smoothDeltaTime", 0.016f);
 
         [GetterMethod(nameof(TimeScale), Inline = true)]
-        private float GetTimeScale() => Utils.IfNotSerializing(() => UTime.timeScale, "UTime.timeScale", 1.0f);
+        private float GetTimeScale() => Utils.IfNotSerializing(() => UTime.timeScale, "UnityEngine.timeScale", 1.0f);
 
         [GetterMethod(nameof(RealtimeSinceStartup), Inline = true)]
-        private float GetRealtimeSinceStartup() => Utils.IfNotSerializing(() => UTime.realtimeSinceStartup, "UTime.realtimeSinceStartup");
+        private float GetRealtimeSinceStartup() => Utils.IfNotSerializing(() => UTime.realtimeSinceStartup, "UnityEngine.realtimeSinceStartup");
 
         [GetterMethod(nameof(TimeSinceLevelLoad), Inline = true)]
-        private float GetTimeSinceLevelLoad() => Utils.IfNotSerializing(() => UTime.timeSinceLevelLoad, "UTime.timeSinceLevelLoad");
+        private float GetTimeSinceLevelLoad() => Utils.IfNotSerializing(() => UTime.timeSinceLevelLoad, "UnityEngine.timeSinceLevelLoad");
 
         [GetterMethod(nameof(Time), Inline = true)]
         private float GetTime() {
-            return IsFixed switch {
-                true when IsUnscaled => Utils.IfNotSerializing(() => UTime.fixedUnscaledTime, "UTime.fixedUnscaledTime"),
-                true => Utils.IfNotSerializing(() => UTime.fixedTime, "UTime.fixedTime"),
-                false when IsUnscaled => Utils.IfNotSerializing(() => UTime.unscaledTime, "UTime.unscaledTime"),
-                false => Utils.IfNotSerializing(() => UTime.time, "UTime.time")
-            };
+            return Utils.IfNotSerializing(
+                () => IsFixed switch {
+                    true when IsUnscaled => UTime.fixedUnscaledTime,
+                    true => UTime.fixedTime,
+                    false when IsUnscaled => UTime.unscaledTime,
+                    false => UTime.time
+                }, "UnityEngine.Time");
         }
 
         [GetterMethod(nameof(DeltaTime), Inline = true)]
         private float GetDeltaTime() {
-            return IsFixed switch {
-                true when IsUnscaled => Utils.IfNotSerializing(() => UTime.fixedUnscaledDeltaTime, "UTime.fixedUnscaledDeltaTime", 0.016f),
-                true => Utils.IfNotSerializing(() => UTime.fixedDeltaTime, "UTime.fixedDeltaTime", 0.016f),
-                false when IsUnscaled => Utils.IfNotSerializing(() => UTime.unscaledDeltaTime, "UTime.unscaledDeltaTime", 0.016f),
-                false => Utils.IfNotSerializing(() => UTime.deltaTime, "UTime.deltaTime", 0.016f)
-            };
+            return Utils.IfNotSerializing(
+                () => IsFixed switch {
+                true when IsUnscaled => UTime.fixedUnscaledDeltaTime,
+                true => UTime.fixedDeltaTime,
+                false when IsUnscaled => UTime.unscaledDeltaTime,
+                false => UTime.deltaTime
+            }, "UnityEngine.Time", 0.016f);
         }
 
         internal void NotifyAllPorts() {
