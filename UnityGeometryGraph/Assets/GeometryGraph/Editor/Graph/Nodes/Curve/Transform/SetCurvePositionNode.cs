663ed0cf5a57a2edf658bb41f1889111d4312c58
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-01-18 09:46:00 +0200

    Add SetCurvePosition node

---
+++ UnityGeometryGraph/Assets/GeometryGraph/Editor/Graph/Nodes/Curve/Transform/SetCurvePositionNode.cs
@@ -0,0 +1,72 @@
+﻿using GeometryGraph.Runtime.Graph;
+using GeometryGraph.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+using IsClosedMode = GeometryGraph.Runtime.Graph.TransformCurveNode.TransformCurveNode_IsClosedMode;
+
+namespace GeometryGraph.Editor {
+    [Title("Curve", "Set Curve Position")]
+    public class SetCurvePositionNode : AbstractNode<GeometryGraph.Runtime.Graph.SetCurvePositionNode> {
+        protected override string Title => "Set Curve Position";
+        protected override NodeCategory Category => NodeCategory.Curve;
+
+        private float3 position;
+        private Vector3Field positionField;
+
+        private GraphFrameworkPort inputCurvePort;
+        private GraphFrameworkPort positionPort;
+        private GraphFrameworkPort resultCurvePort;
+
+        protected override void CreateNode() {
+            inputCurvePort = GraphFrameworkPort.Create("Curve", Direction.Input, Port.Capacity.Single, PortType.Curve, this);
+            (positionPort, positionField) = GraphFrameworkPort.CreateWithBackingField<Vector3Field, Vector3>(
+                "Position", PortType.Vector, this, showLabelOnField: false,
+                onDisconnect: (_, _) => RuntimeNode.UpdatePosition(position)
+            );
+            resultCurvePort = GraphFrameworkPort.Create("Curve", Direction.Output, Port.Capacity.Multi, PortType.Curve, this);
+
+            positionField.RegisterValueChangedCallback(evt => {
+                Owner.EditorView.GraphObject.RegisterCompleteObjectUndo("Change curve position");
+                position = evt.newValue;
+                RuntimeNode.UpdatePosition(position);
+            });
+
+            AddPort(inputCurvePort);
+            AddPort(positionPort);
+            inputContainer.Add(positionField);
+            AddPort(resultCurvePort);
+        }
+
+
+        protected override void BindPorts() {
+            BindPort(inputCurvePort, RuntimeNode.InputPort);
+            BindPort(positionPort, RuntimeNode.PositionPort);
+            BindPort(resultCurvePort, RuntimeNode.ResultPort);
+        }
+
+        protected internal override JObject Serialize() {
+            JObject root = base.Serialize();
+            JArray array = new() {
+                JsonConvert.SerializeObject(position, float3Converter.Converter),
+            };
+            root["d"] = array;
+            return root;
+        }
+
+        protected internal override void Deserialize(JObject data) {
+            JArray array = data["d"] as JArray;
+
+            position = JsonConvert.DeserializeObject<float3>(array!.Value<string>(0), float3Converter.Converter);
+            positionField.SetValueWithoutNotify(position);
+            RuntimeNode.UpdatePosition(position);
+
+            base.Deserialize(data);
+        }
+    }
+}
---
+++ UnityGeometryGraph/Assets/GeometryGraph/Runtime/Graph/Node/Curve/Transform/SetCurvePositionNode.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeometryGraph.Runtime.Attributes;
+using GeometryGraph.Runtime.Curve;
+using Unity.Mathematics;
+
+namespace GeometryGraph.Runtime.Graph {
+    [GenerateRuntimeNode]
+    public partial class SetCurvePositionNode {
+        [In(
+            DefaultValue = "(CurveData)null",
+            GetValueCode = "{self} = GetValue(connection, {default})",
+            UpdateValueCode = ""
+        )]
+        public CurveData Input { get; private set; }
+
+        [In(IsSerialized = false)]
+        public float3 Position { get; private set; }
+
+        [Out] public CurveData Result { get; private set; }
+
+        [GetterMethod(nameof(Result), Inline = true)]
+        private CurveData GetResult() => Result ?? CurveData.Empty;
+
+        protected override void OnConnectionRemoved(Connection connection, RuntimePort port) {
+            if (port != InputPort) return;
+            Result = CurveData.Empty;
+        }
+
+        [CalculatesProperty(nameof(Result))]
+        private void CalculateResult() {
+            if (Input == null || Input.Points == 0 || Input.Type == CurveType.None) {
+                Result = CurveData.Empty;
+                return;
+            }
+
+            List<float3> position = GetValues(PositionPort, Input.Points, Position).ToList();
+            Result = new CurveData(Input.Type, Input.Points, Input.IsClosed, position, Input.Tangent.ToList(), Input.Normal.ToList(), Input.Binormal.ToList());
+        }
+    }
+}
